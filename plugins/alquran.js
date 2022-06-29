4928a9013f663bc97dacc61a4a71beaa5610458b
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-06-29 13:50:35 +0700

    Create alquran.js

---
+++ plugins/alquran.js
@@ -0,0 +1,20 @@
+import {alquran} from '@bochilteam/scraper'
+let handler = async (m, { args, usedPrefix, command }) => {
+    if (!(args[0] || args[1])) throw `contoh:\n${usedPrefix + command} 1 2\n\nmaka hasilnya adalah surah Al-Fatihah ayat 2 beserta audionya, & ayatnya 1 saja`
+    if (isNaN(args[0]) || isNaN(args[1])) throw `contoh:\n${usedPrefix + command} 1 2\n\nmaka hasilnya adalah surah Al-Fatihah ayat 2 beserta audionya, dan ayatnya 1 aja`
+    let api = await alquran()
+    let mes = `
+${api[args[0] - 1].ayahs[args[1] - 1].text.ar}
+    
+${api[args[0] - 1].ayahs[args[1] - 1].translation.id}
+( Q.S ${api[args[0] - 1 ].asma.id.short} : ${api[args[0] - 1].ayahs[args[1] - 1].number.insurah} )
+`.trim()
+    m.reply(mes)
+    conn.sendFile(m.chat, api[args[0] - 1].ayahs[args[1] - 1].audio.url, '', '', m)
+}
+
+handler.command = ['alquran']
+handler.help = ['alquran']
+handler.tags = ['quran']
+
+export default handler
