973698fe94d5b551bd3e12fba84c87b65cb0152c
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-06-30 23:36:12 +0700

    Update and rename plugins/downloader-tiktok.js to plugin/downloader-tiktok.js

--- plugin/downloader-tiktok.js
+++ plugin/downloader-tiktok.js
@@ -5,7 +5,17 @@ if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/
     const { author: { nickname }, video, description } = await tiktokdl(args[0])
     const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `*「🇹」 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ*
+    conn.sendFile(m.chat, url, 'tiktok.mp4', 
+`                 *「 🇹 ᴛɪᴋᴛᴏᴋ 」*
+                 ████████▀▀▀████
+                 ████████────▀██
+                 ████████──█▄──█
+                 ███▀▀▀██──█████
+                 █▀──▄▄██──█████
+                 █──█████──█████
+                 █▄──▀▀▀──▄█████
+                 ███▄▄▄▄▄███████
+────────────── ⇆ㅤ◁ㅤ ❚❚ㅤ ▷ㅤ↻ ──────────
 *Nickname:* ${nickname}
 *Description:* ${description}
 
