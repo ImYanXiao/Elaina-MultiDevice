b3d095f3b73b02fe8bcb1fa06c0220808887bdb6
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-04 13:48:22 +0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -1,70 +1,80 @@
-const { makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = (await import('@adiwajshing/baileys')).default
-import fs from 'fs'
 import fetch from 'node-fetch'
-import moment from 'moment-timezone'
+
 let handler = async (m) => {
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+    //let wibu = `https://api.zacros.my.id/randomimg/loli`
+    let res = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')
+    let txt = await res.text()
+    let arr = txt.split('\n')
+    let cita = arr[Math.floor(Math.random() * arr.length)]
+    let thumb = await(await fetch(cita)).buffer()
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
 let tqto = `*▸ - - - —「 BIG THANKS TO 」— - - - ◂*
-
 *❉ Adiwajshing:*
 https://github.com/adiwajshing
-
 *❉ Nurutomo:*
 https://github.com/Nurutomo
-
 *❉ Istikmal:* 
 https://github.com/BochilGaming
-
 *❉ Ariffb:*
 https://github.com/Ariffb25
-
 *❉ Ilman:*
 https://github.com/ilmanhdyt
-
 *❉ Amirul:*
 https://github.com/amiruldev20
-
 *❉ Rasel:*
 https://github.com/raselcomel
-
 *❉ Fatur:*
 https://github.com/Ftwrr
-
 *❉ Rominaru:*
 https://github.com/Rominaru
-
 *❉ Kannachann:*
 https://github.com/Kannachann
-
 *❉ The.sad.boy01:*
 https://github.com/kangsad01
-
 *❉ Ameliascrf:*
 https://github.com/Ameliascrf
-
 *❉ Fokus ID:*
 https://github.com/Fokusdotid
-
-*❉ Fahri Adison:*
+*❉ Johannes:*
+https://github.com/Johannes2803
+*❉ BrunoSobrino:*
+https://github.com/BrunoSobrino
+*❉ Krisna:*
+https://github.com/NevtBotz
+*❉ LitRHap:*
+https://github.com/LitRHap
+*❉ Rlxfly:*
+https://github.com/Rlxfly
+*❉ Aine:*
+https://github.com/Aiinne
+*❉ Papah-Chan:*
 https://github.com/FahriAdison
-
-*❉ Xiao Yan:*
+*▸ - - - —「 Penulis Ulang 」— - - - ◂*
+*❉ ImYanXiao:*
 https://github.com/ImYanXiao
+*▸ - - - —「 Thanks For Suhu 」— - - - ◂*
+*❉ Kannachan:*
+*❉ Papah-Chan:*
+*❉ Johannes:*
+*❉ Krisna:*
+*❉ Rlxfly:*
+
 `
-conn.reply(m.chat, tqto, m, { contextInfo: { externalAdReply: {
-            title: `${htjava} ${namebot}`,
-            body: titlebot,
-            description: titlebot,
-            mediaType: 2,
-          thumbnail: await(await fetch(thumb2)).buffer(),
-         mediaUrl: sgh
-        }
-     }
-    })
+conn.sendButtonDoc(m.chat, tqto, wm,'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/Xiao_yan_21",
+    mediaType: "VIDEO",
+    description: "https://Instagram.com/Xiao_yan_21", 
+    title: 'Kanna-MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
 }
+} })
+        }
 handler.help = ['tqto']
 handler.tags = ['main','info']
 handler.command = /^(credits|credit|thanks|thanksto|tqto)$/i
