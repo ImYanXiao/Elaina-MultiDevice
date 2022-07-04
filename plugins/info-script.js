8fb040fdc2a7c24c700e20b3657ae97b2c22b2d5
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-04 16:53:01 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -5,8 +5,19 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
 let str = `*https://github.com/ImYanXiao/Kanna-Multi-Device*`
-conn.sendButtonDoc(m.chat, str, wm, 'Thankyou,'Bilek', ftextt, m)
-}
+let wibu = `https://api.zacros.my.id/randomimg/waifu` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/Xiao_yan_21",
+    mediaType: "VIDEO",
+    description: "https://Instagram.com/Xiao_yan_21", 
+    title: 'Kanna-MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
 handler.help = ['source code']
 handler.tags = ['info']
 handler.command =  /^(script|sc)$/i
