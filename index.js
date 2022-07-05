1cdd1429e7abc5beb7e0334cb08ee56ad2e47a9b
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-06 00:44:40 +0700

    Fix

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
 console.log('🐾 Starting...')
 
-import { join, dirname } from 'path'
-import { createRequire } from "module";
+import yargs from 'yargs'
+import cfonts from 'cfonts'
 import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
+import { createRequire } from 'module'
+import { createInterface } from 'readline'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
-import { createInterface } from 'readline'
-import yargs from 'yargs'
 
 // https://stackoverflow.com/a/50052194
+const { say } = cfonts
+const rl = createInterface(process.stdin, process.stdout)
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
 const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
-const { say } = cfonts
-const rl = createInterface(process.stdin, process.stdout)
 
-say('KannaBotz\nMulti-Device', {
-  font: 'block',
-  align: 'center',
-  colors: ['blue']
-})
-say(`🐾 RPG BOT Multi-Device Whatsapp By ImYanXiao`, {
-  font: 'console',
-  align: 'center',
-  colors: ['green']
-})
+say('🐾RPG BOT Multi-Device\nWhatsApp', { font: 'chrome', align: 'center', gradient: ['blue', 'green'] })
+say(`'${name}' By @${author.name || author}`, { font: 'console', align: 'center', gradient: ['blue', 'green'] })
 
 var isRunning = false
 /**
@@ -36,33 +28,11 @@ function start(file) {
   if (isRunning) return
   isRunning = true
   let args = [join(__dirname, file), ...process.argv.slice(2)]
-  say([process.argv[0], ...args].join(' '), {
-    font: 'console',
-    align: 'center',
-    colors: ['magenta']
-  })
-  say('🌎 MEMUAT SOURCE...', {
-    font: 'console',
-    align: 'center',
-    colors: ['green']
-  })
-  say('📑 MEMUAT PLUGINS...', {
-    font: 'console',
-    align: 'center',
-    colors: ['green']
-  })
-  say('✅ DONE !', {
-    font: 'console',
-    align: 'center',
-    colors: ['green']
-  })
-  setupMaster({
-    exec: args[0],
-    args: args.slice(1),
-  })
+  say([process.argv[0], ...args].join(' '), { font: 'console', align: 'center', gradient: ['red', 'magenta'] })
+  setupMaster({ exec: args[0], args: args.slice(1) })
   let p = fork()
   p.on('message', data => {
-    console.log('[RECEIVED]', data)
+    console.log('[✅RECEIVED]', data)
     switch (data) {
       case 'reset':
         p.process.kill()
@@ -76,8 +46,8 @@ function start(file) {
   })
   p.on('exit', (_, code) => {
     isRunning = false
-    console.error('[❗] Exited with code:', code)
-    if (code === 0) return
+    console.error('[❗]Exited with code:', code)
+    if (code !== 0) return start(file)
     watchFile(args[0], () => {
       unwatchFile(args[0])
       start(file)
