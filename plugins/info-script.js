8e58653127d3bd6f6e3b3c901fd5f4b3f17948c7
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-11 11:01:43 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/ImYanXiao/Kanna-Multi-Device*`
+let str = `*https://github.com/ImYanXiao/Kanna-MultiDevice*`
 let wibu = `https://api.zacros.my.id/randomimg/waifu` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
