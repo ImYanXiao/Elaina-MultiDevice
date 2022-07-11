2b94924fdc6cc61efcda4d98a54dee6d2126c1a3
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-11 10:54:46 +0700

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -1,51 +1,31 @@
 import fs from 'fs'
+
 import fetch from 'node-fetch'
+
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `Kanna Disini (>ω<)`
+
+let info = `Kanna Disini (｡>_<｡)`
+
+
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-/*const message = {
-            document: { url: thumb },
-            jpegThumbnail: await (await fetch(thumb)).buffer(), fileName: global.wm, mimetype: td, fileLength: '9999999', pageCount: '999',
-            caption: info,
-            footer: wm,
-            templateButtons: [
-                {
-                    urlButton: {
-                        displayText: '🌎 OFFICIAL GROUP',
-                        url: sgc
-                    }
-                },
-                {
-                    callButton: {
-                        displayText: '📞 Add me',
-                        phoneNumber: nomorown
-                    }
-                },
-                {
-                    quickReplyButton: {
-                        displayText: 'MENU',
-                        id: '.menu'
-                    }
-                },
-                {
-                    quickReplyButton: {
-                        displayText: 'PING',
-                        id: '.ping'
-                    }
-                },
-                {
-                    quickReplyButton: {
-                        displayText: 'DONASI',
-                        id: '.donasi'
-                    }
-                },
-            ]
-        }
-        return await conn.sendMessage(m.chat, message)*/
-conn.reply(m.chat, info, m, { contextInfo: { externalAdReply: {title: global.wm, body: 'Yaw? ada apa kak?', sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg')  }}})
+
+
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+      mediaUrl: "https://Instagram.com/Xiao_yan_21",
+      mediaType: 2,
+      description: "https://Instagram.com/Xiao_yan_21", 
+      title: global.titlebot,
+      body: wm,
+      thumbnail: thumb,
+      sourceUrl: sig  }}})
+
 }
+
 handler.customPrefix = /^(tes|bot|kanna)$/i
+
 handler.command = new RegExp
+
+
 
 export default handler
