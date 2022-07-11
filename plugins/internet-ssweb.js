f17f8c2125bf32d859576b8641d7b49dcee049a5
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-11 12:53:41 +0700

    Update internet-ssweb.js

--- plugins/internet-ssweb.js
+++ plugins/internet-ssweb.js
@@ -1,17 +1,53 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command, args }) => {
-	if (!args[0]) throw 'Input URL'
-	await m.reply('_In progress, please wait..._')
-	let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0],
-		ss = /f$/i.test(command) ? API('nrtm', '/api/ssweb', { delay: 1000, url, full: true }) : API('nrtm', '/api/ssweb', { delay: 1000, url }),
-		res = await fetch(ss)
-	if (!res.ok) throw await res.text()
-	conn.sendMessage(m.chat, { image: { url: ss }, caption: url }, { quoted: m })
+   if (!args[0]) return conn.reply(m.chat, 'Masukkan Linknya Kak', m)
+
+  await m.reply('_𝚃𝚞𝚗𝚐𝚐𝚞 𝚂𝚎𝚋𝚎𝚗𝚝𝚊𝚛⏲️_')
+  
+   let img = await (await fetch(`https://shot.screenshotapi.net/screenshot?url=${args[0]}&full_page=true&fresh=true&output=image&file_type=png&wait_for_event=load`)).buffer()
+
+  
+   conn.sendMessage(m.chat, { image: img, caption: 'Here' }, { quoted: m })
 }
-handler.help = ['ss', 'ssf']
+handler.help = ['ssweb']
 handler.tags = ['internet']
-handler.alias = ['ss', 'ssf', 'ssweb', 'sswebf']
 handler.command = /^ss(web)?f?$/i
 
+handler.limit = false
+handler.fail = null
+
 export default handler
+
+const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(json => {
+            // console.log(json)
+            resolve(json)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+})
+
+
+const getBuffer = async (url, options) => {
+	try {
+		options ? options : {}
+		const res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+                    'User-Agent': 'GoogleBot',
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (e) {
+		console.log(`Error : ${e}`)
+	}
+}
