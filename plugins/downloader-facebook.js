145b6f0f7686ae0c1b152e2aa04113a16a0258e6
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-16 05:36:48 +0700

    Update downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,17 +1,19 @@
-import { facebookDl } from './scraper.js'
-import { savefrom } from '@bochilteam/scraper'
-
-let handler = async (m, { conn, args }) => {
-	if (!args[0]) throw 'Input URL'
-	let res = await facebookDl(args[0]).catch(async _ => await savefrom(args[0])).catch(_ => null)
-	if (!res) throw 'Can\'t download the post'
-	let url = res?.url?.[0]?.url || res?.url?.[1]?.url || res?.['720p'] || res?.['360p']
-	await m.reply('_In progress, please wait..._')
-	conn.sendMessage(m.chat, { video: { url }, caption: res?.meta?.title || '' }, { quoted: m })
+import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `Use example ${usedPrefix + command} https://www.facebook.com/watch?v=636541475139*`
+const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `✨ *ᴜʀʟ:* ${url}`, m)
+  let info = `💝 *ʟᴏᴀᴅɪɴɢ....., 
+ᴇʟᴀɪɴᴀ-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ*
+  `.trim()
+  
+await conn.sendHydrated(m.chat, info, wm, null, sig, 'ɪɴsᴛᴀɢʀᴀᴍ✨', null, null, [
+['🥀️⃟⃪͡ꦽᴍᴇɴᴜ🔖️⃟⃪͡ꦽ', '/menu'],
+['☙⃝✈️ᴋᴇᴄᴇᴘᴀᴛᴀɴ ʙᴏᴛ☙⃝🚀️', '/ping']
+], m,)
 }
-handler.help = ['facebook']
+handler.help = ['facebook'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.alias = ['fb', 'fbdl', 'facebook', 'facebookdl']
-handler.command = /^((facebook|fb)(dl)?)$/i
-
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
+handler.exp = 35
 export default handler
