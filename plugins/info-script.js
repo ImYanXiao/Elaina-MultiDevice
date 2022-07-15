9f8c60fa234297a9585452d79a933641ff94c0b7
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-16 05:58:03 +0700

    Nah bener👌

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,17 +1,17 @@
 //Credits Jangan Dihapus
-//Thanks For Kannachann & Papah-Chan
+//Thanks For Kannachann(Kang Ngamok) & Papah-Chan
 import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/ImYanXiao/Kanna-MultiDevice*`
+let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
 let wibu = `https://api.zacros.my.id/randomimg/waifu` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/Xiao_yan_21",
     mediaType: "VIDEO",
     description: "https://Instagram.com/Xiao_yan_21", 
-    title: 'Kanna-MultiDevice',
+    title: 'Elaina-MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
