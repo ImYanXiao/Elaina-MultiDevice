b3f7cb4f68e02c6cb4fe65b95b6fcf2b76c3e065
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-16 05:52:45 +0700

    Nah bener👌

--- plugins/Waalaikumsalam.js
+++ plugins/Waalaikumsalam.js
@@ -4,45 +4,6 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `Waalaikumsalam`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-/*const message = {
-            document: { url: thumb },
-            jpegThumbnail: await (await fetch(thumb)).buffer(), fileName: global.wm, mimetype: td, fileLength: '9999999', pageCount: '999',
-            caption: info,
-            footer: wm,
-            templateButtons: [
-                {
-                    urlButton: {
-                        displayText: '🌎 OFFICIAL GROUP',
-                        url: sgc
-                    }
-                },
-                {
-                    callButton: {
-                        displayText: '📞 Add me',
-                        phoneNumber: nomorown
-                    }
-                },
-                {
-                    quickReplyButton: {
-                        displayText: 'MENU',
-                        id: '.menu'
-                    }
-                },
-                {
-                    quickReplyButton: {
-                        displayText: 'PING',
-                        id: '.ping'
-                    }
-                },
-                {
-                    quickReplyButton: {
-                        displayText: 'DONASI',
-                        id: '.donasi'
-                    }
-                },
-            ]
-        }
-        return await conn.sendMessage(m.chat, message)*/
 conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
       mediaUrl: "https://github.com/ImYanXiao",
       mediaType: 2,
@@ -52,7 +13,7 @@ conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply
       thumbnail: thumb,
       sourceUrl: sig  }}})
 }
-handler.customPrefix = /^(assalamualaikum)$/i
+handler.customPrefix = /^(assalamualaikum|salam)$/i
 handler.command = new RegExp
 
 export default handler
