56ded9f9fd7d459b9a84072334092fe510a4e084
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-23 13:09:21 +0700

    Janji gak error? 😅☝

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, {text, args}) => {
   if (!args[0]) throw `Use example .simi halo`
-  let api = await fetch(`https://simsimi.info/api/?text=${text}&lc=id`)
+  let api = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=id`)
   let res = await api.json()
   m.reply(res.success)
 }
