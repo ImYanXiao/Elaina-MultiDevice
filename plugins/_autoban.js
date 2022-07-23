e659eac8bb33e27ef213f982b7fd101ea226eeb4
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-23 13:07:50 +0700

    No Meresahkan

--- plugins/_autoban.js
+++ plugins/_autoban.js
@@ -8,6 +8,14 @@ handler.before = async function (m) {
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
    }
+   
+   if (m.sender.startsWith('91' || '91')) {
+   	global.db.data.users[m.sender].banned = true
+   }
+   
+   if (m.sender.startsWith('90' || '90')) {
+   	global.db.data.users[m.sender].banned = true
+   }
     }
 
 export default handler
