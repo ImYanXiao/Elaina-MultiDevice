c572b2d5bdffd5ccb9ddf2c0b9e1092a31243591
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-08-19 13:50:51 +0700

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,6 +1,8 @@
+import db from '../lib/database.js'
 import { canLevelUp } from '../lib/levelling.js'
+
 export function before(m) {
-    let user = global.db.data.users[m.sender]
+    let user = db.data.users[m.sender]
     if (!user.autolevelup)
         return !0
     let before = user.level * 1
@@ -8,9 +10,12 @@ export function before(m) {
         user.level++
 
     if (before !== user.level) {
+        user.role = global.rpg.role(user.level).name
         m.reply(`
-Selamat, anda telah naik level!
-*${before}* -> *${user.level}*
+🎉ᴄᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴs, ${this.getName(m.sender)} ʟᴇᴠᴇʟ ᴜᴩ﹗
+• 📉 ᴩʀᴇᴠɪᴏᴜs ʟᴇᴠᴇʟ : ${before}
+• 📈 ɴᴇᴡ ʟᴇᴠᴇʟ : ${user.level}
+• 🎎 ʀᴏʟᴇ : ${user.role}
 gunakan *.profile* untuk mengecek
 	`.trim())
     }
