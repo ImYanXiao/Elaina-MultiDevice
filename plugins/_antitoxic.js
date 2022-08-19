453479a468bb3e45810c005c4b7cd7c004d0d583
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-08-19 14:05:44 +0700

    Add files via upload

---
+++ plugins/_antitoxic.js
@@ -0,0 +1,23 @@
+let handler = m => m
+
+let linkRegex = /(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
+handler.before = function (m, { user }) {
+  if (m.isBaileys && m.fromMe) return true
+  if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
+  let chat = global.DATABASE.data.chats[m.chat]
+  let isGroupToxic = linkRegex.exec(m.text)
+
+  if (chat.antiToxic && isGroupToxic) {
+    m.reply('Jangan Toxic ya!!\n' + readMore + '\nMau Matikan? ketik */disable antitoxic*')
+    if (global.opts['restrict']) {
+      // if (!user.isAdmin) return true
+      // this.groupRemove(m.chat, [m.sender])
+    }
+  }
+  return true
+}
+
+export const disable = true
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
