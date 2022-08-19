7923043510a5a5a53a2bdaeafe90d6346f18bf40
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-08-19 13:48:55 +0700

    To text

---
+++ plugins/tool-ocr.js
@@ -0,0 +1,20 @@
+import uploadImage from '../lib/uploadImage.js'
+import ocrapi from 'ocr-space-api-wrapper'
+import { MessageType } from '@adiwajshing/baileys'
+let handler = async (m, { conn, text }) => {
+      let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (!mime) throw `balas gambar dengan perintah .ocr`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*jenis ${mime} tidak didukung!*_`
+    let img = await q.download()
+    let url = await uploadImage(img)
+    let hasil = await ocrapi.ocrSpace(url)
+ await m.reply(hasil.ParsedResults[0].ParsedText)    
+}
+
+handler.help = ['ocr', 'totext']
+handler.tags = ['tools']
+handler.command = /^(ocr|totext)$/i
+handler.limit = true
+
+export default handler
