51b9d6d2e4586bb15e29d82b67bd6ba944956632
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-08-30 20:52:35 +0700

    Runtime Fake

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -1,37 +1,66 @@
+Thanks Papah-Chan https://github.com/FahriAdison
+let { generateWAMessageFromContent } = (await import("@adiwajshing/baileys")).default 
+import { performance } from 'perf_hooks'
 import fs from 'fs'
-let handler = async (m, { conn, args, command }) => {
-	let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
-    }
-    let muptime = clockString(_muptime)
- conn.reply(m.chat, `${htki} *R U N T I M E* ${htka}\n${muptime}\n`, m, {
-contextInfo: { externalAdReply :{
-                        mediaUrl: '',
-                        mediaType: 2,
-                        description: 'anu',
-                        title: bottime,
-                        body: wm2,          previewType: 0,
-                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                        sourceUrl: snh
-                      }}
-})
-}
+let handler  = async (m, { conn, usedPrefix: _p }) => {
 
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var days = Math.floor(seconds / (24 * 60 * 60 * 1000));
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+
+  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+  return `\n\t\t*⏰ʀᴜɴᴛɪᴍᴇ「 \t${pad(days)}D\t ${pad(hours)}H ${pad(minutes)}M ${pad(seconds)}S \t」*\n`
+}
+					const runtime = process.uptime()
+		            const teks = `${kyun(runtime)}`
+					const itsme = `0@s.whatsapp.net`
+					const split = `uwu >//<`
+					const rtimebro = {
+					contextInfo: {
+					participant: itsme,
+					quotedMessage: {
+					extendedTextMessage: {
+				    text: split
+									}
+								}
+							}
+					}
+						    
+						  let prep = generateWAMessageFromContent(m.chat, { orderMessage: { 
+itemCount: -10062007, status: 500,
+surface: 999,
+message: teks,
+description: '^^',
+orderTitle: 'Haii Kak',
+token: '9',
+curreyCode: 'IDR',
+totalCurrencyCode: '>〰<',
+totalAmount1000: '1000000',
+sellerJid: '6285736178354@s.whatsapp.net',
+thumbnail: fs.readFileSync('./thumbnail.jpg')
+}}, {contextInfo: null, quoted: m})
+conn.relayWAMessage(prep)
+				/*	conn.sendMessage(m.chat, `${teks}`, MessageType.text, rtimebro)*/
+}
 
 handler.help = ['runtime']
 handler.tags = ['info']
-handler.command = ['runtime', 'rt']
-export default handler
+handler.command = /^(up|run)time$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
-function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
-}
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+export default handler
