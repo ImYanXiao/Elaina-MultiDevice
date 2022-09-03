4a2d90c5e9c8b30aee71562085292d4d4b535573
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-03 10:31:32 +0700

    ....

--- plugins/broadcastchats.js
+++ plugins/broadcastchats.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
   let chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map(v => v[0])
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
+  conn.reply(m.chat, `_Mengirim broadcast ke ${chats.length} chat_`, m)
   for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + author + ' All Chat Broadcast 」\n' + randomID(32)), true).catch(_ => _)
   m.reply('Selesai Broadcast All Chat :)')
 }
