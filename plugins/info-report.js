2717ebbbeaf9c0312d126f834681fb25519f0648
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-06 02:12:33 +0700

    tydak sofun

--- plugins/info-report.js
+++ plugins/info-report.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `kalo kamu nemu pesan eror, lapor pake perintah ini\n\ncontoh:\n${usedPrefix + command} selamat siang owner, sy menemukan eror seperti berikut <copy/tag pesan erornya>`
-    if (text.length < 10) throw `Laporan terlalu pendek, minimal 10 karakter!`
-    if (text.length > 1000) throw `Laporan terlalu panjang, maksimal 1000 karakter!`
+    if (text.length < 10) throw `kependekan gblk, minimal 10 karakter!`
+    if (text.length > 1000) throw `mo lapor atau curhat?, maksimal 1000 karakter!`
     let teks = `*${command.toUpperCase()}!*\n\nDari : *@${m.sender.split`@`[0]}*\n\nPesan : ${text}\n`
     conn.reply(global.nomorown + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
         contextInfo: {
