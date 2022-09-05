c9e726a267deb94867bb66507a6d927a4dce3f9f
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-06 01:42:00 +0700

    🗿

--- plugins/downloader-igstalk.js
+++ plugins/downloader-igstalk.js
@@ -1,27 +1,42 @@
+//https://github.com/Rlxfly/re-md
 import { instagramStalk } from '@bochilteam/scraper'
 
-let handler= async (m, { args, usedPrefix, command }) => {
+let handler= async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Example use ${usedPrefix}${command} <username>`
     const {
         username,
+      avatar,
         name,
         description,
         followersH,
         followingH,
         postsH,
     } = await instagramStalk(args[0])
-    m.reply(`
-${name} *(${username})*
-https://instagram.com/${username.replace(/^@/, '')}
-*${followersH}* Followers
-*${followingH}* Following
-*${postsH}* Posts
-*Bio:* ${description}
-`.trim())
+
+  let data = `
+💌 ${username} » 「 ${name} 」
+
+🎏 ${followersH}  ғᴏʟʟᴏᴡᴇʀs
+🎀 ${followingH}  ғᴏʟʟᴏᴡɪɴɢ
+📝 ${postsH} Pᴏꜱᴛ
+📑 Bɪᴏ: ${description}
+`.trim()
+
+  let pp = await( await conn.getFile(avatar)).data
+  
+  conn.sendHydrated(m.chat, '「  sᴛᴀʟᴋɪɴɢ 」' , data, pp, `https://instagram.com/${username.replace(/^@/, '')}`, 'Link Profile', '0', 'Nothing', [
+
+      [null],
+
+      [null],
+
+      [null, null]
+
+    ], null,  { asLocation: true })
 }
 
 handler.help = ['igstalk'].map(v => v + ' <username>')
-handler.tags = ['downloader']
+handler.tags = ['internet']
 
 handler.command = /^(igstalk)$/i
 
