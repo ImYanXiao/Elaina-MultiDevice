1a2e97b5a700cfb7f05769ff1eb9db3f60101207
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-06 02:21:54 +0700

    Lol

--- plugins/logo-loli.js
+++ plugins/logo-loli.js
@@ -4,13 +4,10 @@ let response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('Proses...')
   let res = `https://ziy.herokuapp.com/api/maker/lolimaker?nama=${response[0]}&apikey=xZiyy`
-  conn.sendFile(m.chat, res, 'sadboy.jpg', `Sudah Jadi`, m, false)
+  conn.sendFile(m.chat, res, 'loli.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['logololi'].map(v => v + ' <text>')
 handler.tags = ['maker']
 handler.command = /^(logololi)$/i
-handler.register = false
-
-handler.limit = false
 
 export default handler
