3c9e827cf0d6e5c62711bf8cfb5141cd2613adf7
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-06 01:29:38 +0700

    dikasih cobaan malah dicobain

--- plugins/tool-delete.js
+++ plugins/tool-delete.js
@@ -1,13 +1,16 @@
-let handler = function (m) {
-    if (!m.quoted) throw false
-    let { chat, fromMe, isBaileys } = m.quoted
-    if (!fromMe) throw false
-    //if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
-    conn.sendMessage(chat, { delete: m.quoted.vM.key })
+let handler = async (m, { conn, command }) => {
+if (!m.quoted) throw 'Reply pesan yang ingin dihapus'
+try {
+let bilek = m.message.extendedTextMessage.contextInfo.participant
+let banh = m.message.extendedTextMessage.contextInfo.stanzaId
+return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: banh, participant: bilek }})
+ } catch {
+return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+}
 }
 handler.help = ['del', 'delete']
 handler.tags = ['tools']
 
-handler.command = /^del(ete)?$/i
+handler.command = /^d(el(ete)?$/i
 
 export default handler
