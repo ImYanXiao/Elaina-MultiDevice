51b155761554dfc7b92631e2149f55d60e08c61a
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-06 02:01:29 +0700

    I d k

--- plugins/susunkata_ans.js
+++ plugins/susunkata_ans.js
@@ -2,36 +2,37 @@ import similarity from 'similarity'
 const threshold = 0.72
 export async function before(m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*suka/i.test(m.quoted.contentText)) 
-      return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Ketik.*suka/i.test(m.quoted.text) || /.*suka/i.test(m.text))
+        return !0
     this.susunkata = this.susunkata ? this.susunkata : {}
-    if (!(id in this.susunkata)) 
-        return conn.sendButton(m.chat, 'Soal itu telah berakhir', author, buttonSusunkata1, m) 
-                let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
-        if (isSurrender) {
-        	clearTimeout(this.susunkata[id][3])
-            delete this.susunkata[id]
-            return conn.sendButton(m.chat, '*Yah Menyerah :( !*', author, null, buttonSusunkata1, m)
-        }
+    if (!(id in this.susunkata))
+        return conn.sendButton(m.chat, 'Soal itu telah berakhir', author, null, buttonsusunkata, m)
     if (m.quoted.id == this.susunkata[id][0].id) {
-        let json = JSON.parse(JSON.stringify(this.susunkata[id][1]))
-        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.susunkata[id][2]
-            await this.sendButton(m.chat, `*Benar!* +${this.susunkata[id][2]} XP`, author, buttonSusunkata1,m)
+        let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
+        if (isSurrender) {
             clearTimeout(this.susunkata[id][3])
             delete this.susunkata[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) 
-             m.reply(`*Dikit Lagi!*`)
+            return conn.sendButton(m.chat, '*Yah Menyerah :( !*', author, null, buttonsusunkata, m)
+        }
+        let json = JSON.parse(JSON.stringify(this.susunkata[id][1]))
+        // m.reply(JSON.stringify(json, null, '\t'))
+        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
+            global.db.data.users[m.sender].exp += this.susunkata[id][2]
+            conn.sendButton(m.chat, `*Benar!*\n+${this.susunkata[id][2]} XP`, author, null, buttonsusunkata, m)
+            clearTimeout(this.susunkata[id][3])
+            delete this.susunkata[id]
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
+            m.reply(`*Dikit Lagi!*`)
         else
             conn.sendButton(m.chat, `*Salah!*`, author, null, [
-                ['hint', '/suka'],
-                ['nyerah', 'menyerah']
+                ['Hint', '/suka'],
+                ['Nyerah', 'menyerah']
             ], m)
     }
     return !0
 }
 export const exp = 0
 
-const buttonSusunkata1 = [
+const buttonsusunkata = [
     ['susunkata', '/susunkata']
 ]
