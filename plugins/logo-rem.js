13469fd872b3e6e664268ac915575bee6d011908
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-06 02:23:29 +0700

    Update logo-rem.js

--- plugins/logo-rem.js
+++ plugins/logo-rem.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
 let response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
+  if (!args[0]) throw 'Masukkan Nama'
   m.reply('proses..')
   let res = `https://ziy.herokuapp.com/api/maker/rem?nama=${response[0]}&apikey=xZiyy`
   conn.sendFile(m.chat, res, 'rem.jpg', `Nih kak`, m, false)
@@ -9,8 +9,5 @@ let response = args.join(' ').split('|')
 handler.help = ['logorem'].map(v => v + ' <text|text>')
 handler.tags = ['maker']
 handler.command = /^(logorem)$/i
-handler.register = false
-
-handler.limit = false
 
 export default handler
