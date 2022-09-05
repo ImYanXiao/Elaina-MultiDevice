9fce1681e8163e73f73ff569cfd9a3d8fcb447c5
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-06 01:59:30 +0700

    Tretretet

--- plugins/susun-kata.js
+++ plugins/susun-kata.js
@@ -1,32 +1,32 @@
 import fetch from 'node-fetch'
-
 let timeout = 120000
-let poin = 500
-let handler = async (m, { conn, usedPrefix }) => {
+let poin = 4999
+let handler = async (m, { conn, command, usedPrefix }) => {
+let imgr = flaaa.getRandom()
+
     conn.susunkata = conn.susunkata ? conn.susunkata : {}
     let id = m.chat
     if (id in conn.susunkata) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.susunkata[id][0])
+        conn.sendButton(m.chat, 'Masih ada soal belum terjawab di chat ini', author, null, buttons, conn.susunkata[id][0])
         throw false
     }
-    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/susunkata.json')
-    if (!res.ok) throw eror
-    let data = await res.json()
-    let json = data[Math.floor(Math.random() * data.length)]
-    let caption = `
-${json.soal}
-
-Tipe: ${json.tipe}
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/susunkata.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+  let caption = `
+  ${json.soal}
+  ${json.tipe}
 
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}suka untuk bantuan
 Bonus: ${poin} XP
-`.trim()
+    `.trim()
     conn.susunkata[id] = [
-        await conn.sendButton(m.chat, caption, author, ['hint', `${usedPrefix}suka`], m),
+        await conn.sendButton(m.chat, caption, author, `${imgr + command}`, buttons, m),
         json, poin,
         setTimeout(() => {
-            if (conn.susunkata[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, author, ['susunkata', `${usedPrefix}susunkata`], conn.susunkata[id][0])
+            if (conn.susunkata[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, author, null, [
+                ['susunkata', '/susunkata']
+            ], conn.susunkata[id][0])
             delete conn.susunkata[id]
         }, timeout)
     ]
@@ -38,5 +38,6 @@ handler.command = /^susunkata/i
 export default handler
 
 const buttons = [
-    ['nyerah', 'menyerah']
+    ['Hint', '/suka'],
+    ['Nyerah', 'menyerah']
 ]
