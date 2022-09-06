6d9a2582452d2a9536a27e6192b3a686f5443501
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-06 18:08:19 +0700

    P

--- plugins/group-settings.js
+++ plugins/group-settings.js
@@ -2,12 +2,16 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     let isClose = { // Switch Case Like :v
         'open': 'not_announcement',
         'close': 'announcement',
+        'unlock': 'unlocked',
+        'lock': 'locked',
     }[(args[0] || '')]
     if (isClose === undefined)
         throw `
 *Format salah! Contoh :*
   *○ ${usedPrefix + command} close*
   *○ ${usedPrefix + command} open*
+  *○ ${usedPrefix + command} unlock*
+  *○ ${usedPrefix + command} lock*
 `.trim()
     await conn.groupSettingUpdate(m.chat, isClose)
 }
