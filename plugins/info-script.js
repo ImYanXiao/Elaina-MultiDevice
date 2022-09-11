2b34963a71b00b1bcf5285fbd81f455b4566fdb5
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-11 13:54:48 +0700

    Sourcode?

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -5,16 +5,14 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
 let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
-let wibu = `https://api.zacros.my.id/randomimg/waifu` 
+let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
-conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/Xiao_yan_21",
-    mediaType: "VIDEO",
-    description: "https://Instagram.com/Xiao_yan_21", 
     title: 'Elaina-MultiDevice',
-    body: wm,
+    body: 'want source code?',
     thumbnail: thumb,
-    sourceUrl: sgc
+    sourceUrl: sig
   }
   } }) 
           }
