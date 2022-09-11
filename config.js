fc6f02c0665eeb7aae7b7a45f7934bb735161188
AsoyMD <113269574+AsoyMD@users.noreply.github.com>
2022-09-11 09:17:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -59,9 +59,9 @@ global.ppulsa = '085736178354' //Nomor SimCard Yang Kamu Pake
 global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada Ketik - aja
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6281231079387' //Nomor Bot
-global.nomorown = '6285736178354' //Nomor Owner
-global.namebot = 'Elaina BOT' //Nama Bot
+global.nomorbot = '6289628357498' //Nomor Bot
+global.nomorown = '6289517544584' //Nomor Owner
+global.namebot = 'Asoy BOT' //Nama Bot
 global.nameown = '𝓧𝓲𝓪𝓸 - 𝓨𝓪𝓷' // Nama Owner
 
 
