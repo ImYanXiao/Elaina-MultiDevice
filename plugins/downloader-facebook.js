aac8a298b335e86484e9391fe3d9b8cd3067eba8
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-11 13:51:47 +0700

    Typo

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
     for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
 }
-handler.help = ['facebbok'].map(v => v + ' <url>')
+handler.help = ['facebook'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^((facebook|fb)(downloder|dl)?)$/i
