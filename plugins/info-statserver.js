6d44ce75a991a3a1cbffe6d91c6c023b2318e3bb
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-12 17:26:05 +0700

    Update info-statserver.js

--- plugins/info-statserver.js
+++ plugins/info-statserver.js
@@ -2,7 +2,7 @@ import cp from 'child_process'
 import { promisify } from 'util'
 let exec = promisify(cp.exec).bind(cp)
 let handler = async (m) => {
-	await conn.reply(m.chat, "Wait", m)
+	await conn.reply(m.chat, "ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ", m)
     let o
     try {
         o = await exec('df -h')
