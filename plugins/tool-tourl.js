793185707c2bde6ebcd823ea91dc3aaabdf7badc
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-14 21:10:44 +0700

    Ytta

--- plugins/tool-tourl.js
+++ plugins/tool-tourl.js
@@ -1,32 +1,41 @@
-import fs from 'fs'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
-import FormData from 'form-data'
 
 let handler = async (m) => {
-	let q = m.quoted ? m.quoted : m
-	let mime = q.mediaType || ''
-	if (/image|video|audio|sticker|document/.test(mime)) {
-		let media = await q.download(true)
-		let data = await uploadFile(media)
-		m.reply(data.files[0].url)
-	} else throw 'No media found'
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let name = await conn.getName(who)
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'No media found'
+  let media = await q.download()
+  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+  let link = await (isTele ? uploadImage : uploadFile)(media)
+  let caption = `📮 *L I N K :*
+${link}
+📊 *S I Z E :* ${media.length} Byte
+📛 *E x p i r e d :* ${isTele ? 'No Expiry Date' : 'Unknown'}
+
+*S H O R T :* ${await shortUrl(link)}`
+
+conn.reply(m.chat, caption, m, { contextInfo: {
+          externalAdReply :{
+    mediaUrl: sgh,
+    mediaType: 2,
+    title: wm,
+    body: botdate,
+    thumbnail: await(await fetch(link)).buffer(),
+    sourceUrl: link
+     }}
+  })
 }
 handler.help = ['tourl']
-// handler.tags = ['tools']
-handler.command = /^(tourl)$/i
-
+handler.tags = ['tools']
+handler.command = /^(tourl|upload)$/i
 export default handler
 
-async function uploadFile(path) {
-	let form = new FormData()
-	form.append('files[]', fs.createReadStream(path))
-	let res = await (await fetch('https://uguu.se/upload.php', {
-		method: 'post',
-		headers: {
-			...form.getHeaders()
-		},
-		body: form
-	})).json()
-	await fs.promises.unlink(path)
-	return res
+async function shortUrl(url) {
+	let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
+	return await res.text()
 }
