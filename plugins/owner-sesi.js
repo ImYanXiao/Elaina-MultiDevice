32305d3165bff8b31bf3c1f184a9190fa820f005
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-20 16:16:55 +0700

    Update owner-sesi.js

--- plugins/owner-sesi.js
+++ plugins/owner-sesi.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
-    m.reply('Tunggu Sebentar, Sedang mengambil file sesi mu')
+    m.reply('Tunggu Sebentar, Sedang mengambil file sesi bot')
     let sesi = await fs.readFileSync('./session.data.json')
     return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'session.data.json' }, { quoted: m })
 }
