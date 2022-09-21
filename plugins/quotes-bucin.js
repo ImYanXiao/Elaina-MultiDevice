c3df6f906e822f2b69d1b41df8ede8bb11c90902
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-21 18:56:20 +0700

    Huft

--- plugins/quotes-bucin.js
+++ plugins/quotes-bucin.js
@@ -1,12 +1,9 @@
-import fetch from 'node-fetch'
-let handler = async(m, { conn, text }) => {
-  let res = await (await fetch('https://api.lolhuman.xyz/api/random/bucin?apikey=Apikeymu'))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if(!json.result[0]) throw json
-  let { result } = json.result[0]
-m.reply(`${json.result}`)
-}
+import { bucin } from '@bochilteam/scraper'
+
+let handler = async (m, { conn, usedPrefix, command }) => conn.sendButton(m.chat, await bucin(), author, [
+    ['Next', `${usedPrefix+command}`], 
+], m)
+
 handler.help = ['bucin']
 handler.tags = ['quotes']
 handler.command = /^(bucin)$/i
