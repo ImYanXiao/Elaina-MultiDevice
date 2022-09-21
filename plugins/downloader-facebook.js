3c721d85799ab59e1b4ca91e4ffcb018ba76f869
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-21 18:53:48 +0700

    Nth

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,12 +1,13 @@
-import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+import fbDownloader from 'fb-downloader-scrapper'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/azFEBmFRcy/`
-    const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-    for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
+    let res = await fbDownloader(args[0])
+    for (let result of res.download) {
+    conn.sendFile(m.chat, result.url, 'facebook.mp4', author, m)
+  }
 }
 handler.help = ['facebook'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-
 handler.command = /^((facebook|fb)(downloder|dl)?)$/i
 
 export default handler
