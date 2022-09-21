186d94f693eb0454486dfd17fc0109a44ee7fef0
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-21 19:22:47 +0700

    Sometimes

--- plugins/internet-brainly.js
+++ plugins/internet-brainly.js
@@ -23,7 +23,7 @@ ${v.content}${v.attachments.length > 0 ? `\n*Media Url*: ${v.attachments.join(',
 		}
 	}
 }
-handler.help = handler.alias = ['brainly']
+handler.help = ['brainly']
 handler.tags = ['tools']
 handler.command = /^(brainly)$/i
 
