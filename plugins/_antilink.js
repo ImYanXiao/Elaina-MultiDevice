52a14c39fd1e8f26b50b4a055fb005e6128edc3d
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-28 13:09:48 +0700

    Ya

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -6,6 +6,8 @@ export async function before(m, { isAdmin, isBotAdmin }) {
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
     const isGroupLink = linkRegex.exec(m.text)
+    let hapus = m.key.participant
+    let pesan = m.key.id
 
     if (chat.antiLink && isGroupLink && !isAdmin) {
         if (isBotAdmin) {
@@ -14,7 +16,7 @@ export async function before(m, { isAdmin, isBotAdmin }) {
         }
         await conn.sendButton(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, author, ['off antilink', '/disable antilink'], m)
         if (isBotAdmin && bot.restrict) {
-           await conn.sendMessage(m.chat, { delete: m.key})
+           return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: pesan, participant: hapus }})
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
         } else if (!bot.restrict) return m.reply('Owner disable auto kick!')
     }
