9dfd1850547128d80f17568c5e216d958d3551bb
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-29 18:53:05 +0700

    Menpess

---
+++ plugins/main-menfess.js
@@ -0,0 +1,41 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    conn.menfess = conn.menfess ? conn.menfess : {}
+    if (!text) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonymous|Hai.`;
+    let [jid, name, pesan] = text.split('|');
+    if ((!jid || !name || !pesan)) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonymous|Hai.`;
+    jid = jid.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
+    let data = (await conn.onWhatsApp(jid))[0] || {};
+    if (!data.exists) throw 'Nomer tidak terdaftar di whatsapp.';
+    if (jid == m.sender) throw 'tidak bisa mengirim pesan menfess ke diri sendiri.'
+    let mf = Object.values(conn.menfess).find(mf => mf.status === true)
+    if (mf) return !0
+    try {
+    	let id = + new Date
+        let tek = `Hai @${data.jid.split('@')[0]}, kamu menerima pesan Menfess nih.\n\nDari: *${name}*\nPesan: \n${pesan}\n\nMau balas pesan ini kak? bisa kok kak. tinggal ketik pesan kakak lalu kirim, nanti saya sampaikan ke *${name}*.`.trim();
+        let imgr = flaaa.getRandom()
+        await conn.sendButton(data.jid, bottime, tek, `${imgr + 'Menfess'}`, [['BALAS PESAN', '.balasmenfess']], fakes)
+        .then(() => {
+            m.reply('Berhasil mengirim pesan menfess.')
+            conn.menfess[id] = {
+                id,
+                dari: m.sender,
+                nama: name,
+                penerima: data.jid,
+                pesan: pesan,
+                status: false
+            }
+            return !0
+        })
+    } catch (e) {
+        console.log(e)
+        m.reply('eror');
+    }
+}
+handler.tags = ['main']
+handler.help = ['menfess', 'menfes'].map(v => v + ' <nomor|nama pengirim|pesan>')
+handler.command = /^(menfess|menfes)$/i
+handler.private = true
+
+export default handler
+
+//Credit? https://github.com/ImYanXiao
