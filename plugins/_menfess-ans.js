10aa802f018eb0095eae220cb8b0f7c614eafd6e
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-09-29 18:53:34 +0700

    Add files via upload

---
+++ plugins/_menfess-ans.js
@@ -0,0 +1,20 @@
+const delay = time => new Promise(res => setTimeout(res, time))
+export async function before(m) {
+	if (!m.chat.endsWith('@s.whatsapp.net')) return !0;
+	this.menfess = this.menfess ? this.menfess : {}
+	let mf = Object.values(this.menfess).find(v => v.status === false && v.penerima == m.sender)
+	if (!mf) return !0
+	console.log({ text: m.text, type: m.quoted?.mtype })
+	if ((m.text === 'BALAS PESAN' || m.text === '') && m.quoted.mtype == 'buttonsMessage') return m.reply("Silahkan kirim pesan balasan kamu.\nKetik pesan sesuatu lalu kirim, maka pesan otomatis masuk ke target balas pesan.");
+	else {
+		let imgr = flaaa.getRandom()
+		let txt = `Hai kak @${mf.dari.split('@')[0]}, kamu menerima balasan nih.\n\nPesan yang kamu kirim sebelumnya:\n${mf.pesan}\n\nPesan balasannya:\n${m.text}\n`.trim();
+		await this.sendButton(mf.dari, bottime, txt, `${imgr + 'Menfess'}`, [['BALAS PESAN', '.balasmenfess']], null).then(() => {
+			m.reply('Berhasil Mengirim balasan.')
+			delay(1500)
+			delete this.menfess[mf.id]
+			return !0
+		})
+	}
+	return !0
+}
