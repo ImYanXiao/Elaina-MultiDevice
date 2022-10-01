f8d2b45643770152f0e2ecf6d4105d8438e6df55
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-10-01 20:01:29 +0700

    Fix Translate

--- plugins/tool-translate.js
+++ plugins/tool-translate.js
@@ -1,42 +1,17 @@
-import translate from 'translate-google-api'
-const defaultLang = 'en'
-const tld = 'cn'
+import translate from '@vitalets/google-translate-api'
 
 let handler = async (m, { args, usedPrefix, command }) => {
-    let err = `
-Contoh:
-${usedPrefix + command} <lang> [text]
-${usedPrefix + command} id your messages
-
-kamu bisa melihat list bahasa yang didukung: https://cloud.google.com/translate/docs/languages
-`.trim()
-
-    let lang = args[0]
-    let text = args.slice(1).join(' ')
-    if ((args[0] || '').length !== 2) {
-        lang = defaultLang
-        text = args.join(' ')
-    }
-    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
-
-    let result
-    try {
-        result = await translate(`${text}`, {
-            tld,
-            to: lang,
-        })
-    } catch (e) {
-        result = await translate(`${text}`, {
-            tld,
-            to: defaultLang,
-        })
-        throw err
-    } finally {
-        m.reply(result[0])
-    }
-
+	let lang, text
+	if (args.length >= 2) {
+		lang = args[0] ? args[0] : 'id', text = args.slice(1).join(' ')
+	} else if (m.quoted && m.quoted.text) {
+		lang = args[0] ? args[0] : 'id', text = m.quoted.text
+	} else throw `Ex: ${usedPrefix + command} id hello i am robot`
+	let res = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null)
+	if (!res) throw `Error : Bahasa"${lang}" Tidak Support`
+	m.reply(`*Terdeteksi Bahasa:* ${res.from.language.iso}\n*Ke Bahasa:* ${lang}\n\n*Terjemahan:* ${res.text}`.trim())
 }
-handler.help = ['translate'].map(v => v + ' <lang> <teks>')
+handler.help = ['translate'].map(v => v + ' <bahasa> <teks>')
 handler.tags = ['tools']
 handler.command = /^(tr(anslate)?)$/i
 
