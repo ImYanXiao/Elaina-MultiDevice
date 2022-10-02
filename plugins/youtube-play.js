38443ccef3051db0d631f8283afb7ceed353bd1b
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-10-02 19:11:43 +0700

    Iyh

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -1,40 +1,35 @@
+// Source? https://github/DikaArdnt/Hisoka-Morou
+// Recode ImYanXiao
 import fetch from 'node-fetch'
-import { youtubeSearch } from '@bochilteam/scraper'
+import yts from 'yt-search'
 let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
-try {
-  if (!text) throw `Use example ${usedPrefix}${command} gustixa`
-  let vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
-  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
-  const url = 'https://www.youtube.com/watch?v=' + videoId
-  await conn.sendHydrated(m.chat, `*YOUTUBE PLAY*
-${htjava} *Title:* ${title}
-📤 *Published:* ${publishedTime}
-⏰ *Duration:* ${durationH}
-👁️ *Views:* ${viewH}
-🔗 *Url:* ${url}
-📔 *Description:* ${description}
-  `.trim(), author, thumbnail, url, '📺Go To Youtube!', null, null, [
-    ['Audio 🎧', `${usedPrefix}yta ${url} yes`],
-    ['Video 🎥', `${usedPrefix}ytv ${url} yes`],
-    ['Youtube Search🔎', `${usedPrefix}yts ${url}`]
-  ], m)
-} catch {
-if (!text) throw 'Input Query'
-  let vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw 'Video/Audio Tidak Ditemukan'
-  let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
-  let url = 'https://www.youtube.com/watch?v=' + videoId
-  let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
-  let capt = `*Title:* ${title}\n*Published:* ${publishedTime}\n*Duration:* ${durationH}\n*Views:* ${viewH}\n*Url:* ${url}`
-  let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${url}` }]
-  let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: '_Audio on progress..._', buttons }, { quoted: m })
-  // if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
-  conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: msg })
-}
-
-}
-handler.help = ['play', 'play'].map(v => v + ' <pencarian>')
+if (!text) throw `Example : ${prefix + command} story wa anime`
+                let search = await yts(text)
+                let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
+                let buttons = [
+                    {buttonId: `ytmp3 ${anu.url}`, buttonText: {displayText: '♫ Audio'}, type: 1},
+                    {buttonId: `ytmp4 ${anu.url}`, buttonText: {displayText: '► Video'}, type: 1}
+                ]
+                let buttonMessage = {
+                    image: { url: anu.thumbnail },
+                    caption: `
+⭔ Title : ${anu.title}
+⭔ Ext : Search
+⭔ ID : ${anu.videoId}
+⭔ Duration : ${anu.timestamp}
+⭔ Viewers : ${anu.views}
+⭔ Upload At : ${anu.ago}
+⭔ Author : ${anu.author.name}
+⭔ Channel : ${anu.author.url}
+⭔ Description : ${anu.description}
+⭔ Url : ${anu.url}`,
+                    footer: wm,
+                    buttons: buttons,
+                    headerType: 4
+                }
+                conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+            }
+handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
 handler.command = /^(y((outube|((utu|t)b|t))play|tp)|play(yt)?)$/i
 
@@ -43,9 +38,6 @@ handler.limit = false
 
 export default handler
 
-async function shortUrl(url) {
-  url = encodeURIComponent(url)
-  let res = await fetch(`https://is.gd/create.php?format=simple&url=${url}`)
-  if (!res.ok) throw false
-  return await res.text()
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
 }
