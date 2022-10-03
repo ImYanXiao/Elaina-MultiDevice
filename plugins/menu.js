4beefe47d30778d4078527295942fbc842bbe058
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-10-04 00:40:25 +0700

    Hamdeh

--- plugins/menu.js
+++ plugins/menu.js
@@ -8,7 +8,7 @@ import moment from 'moment-timezone'
 import os from 'os'
 import fs from 'fs'
 import fetch from 'node-fetch'
-const { generateWAMessageFromContent } = (await import('@adiwajshing/baileys')).default
+const { makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = (await import('@adiwajshing/baileys')).default
 
 const defaultMenu = {
   before: `
