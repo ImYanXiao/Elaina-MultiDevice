c0d1191b5dd8b74eb0a4a2194c97e9eead405e85
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-10-10 18:42:16 +0700

    🧢

--- plugins/cek-warn.js
+++ plugins/cek-warn.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
       let mention = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-      let warn = global.db.data.users[target].warn
+      let warn = global.db.data.users[mention].warn
       m.reply(`Kamu memiliki Total Warning : ${warn}`)
 }
 
