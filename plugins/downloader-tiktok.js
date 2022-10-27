5f26ee78b2a080b6dfaf7db7a889e46486efaa3a
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-10-27 22:04:24 +0700

    Tq kang aguz

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,23 +1,13 @@
-//Source : https://github.com/carlxjoe/Rain-BotV2/blob/main/scraper/savefrom.js
-import { savefrom } from "../lib/savefrom.js"
-import { tiktokdlv3 } from "@bochilteam/scraper"
+import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-//
-    let res = ( await savefrom(args[0])).url[0]
-    let result = await savefrom(args[0])
-   
-   let { title, source, duration } = result.meta
-   let { url } = res
-   if (!url) throw 'Can\'t download video!'
-   await conn.reply(m.chat, wait, m)
-   await  conn.sendFile(m.chat, url, 'tiktok.mp4', `*${command} Downloader*
-   
-${title ? "*Title:* "+ title : "Untitled"}
-${source ? "*Source:* " + source : "unsource"}
-${duration ? "*Duration:* " + duration : "null"}`.trim(), m)
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    let res = (await axios.get(API('males', '/tiktok2', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    conn.sendButtonVid(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', 'menu', usedPrefix + 'menu', m)
 }
-handler.help = ['tiktok', 'tiktok2', 'tiktokdl', "savefrom"].map(v => v + ' <url>')
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok2)?(tok)?(dl)?(nowm)?|save(from)?)$/i
+handler.command = /^(tik(tok)?(dl)?)$/i
+
 export default handler
