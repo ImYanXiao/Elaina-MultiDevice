7528f9ef09b69e5872e189e1c965c4b6519d56e3
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-11-04 15:27:25 +0700

    A

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -9,10 +9,10 @@ let thumb = await(await fetch(wibu)).buffer()
 user.afk = + new Date
 user.afkReason = text
  conn.sendButtonDoc(m.chat, `${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}`, wm, 'ᴊᴀɴɢᴀɴ ᴅɪɢᴀɴɢɢᴜ ʏᴀ ᴋᴀᴋ', 'Bilek', m,  { contextInfo: { externalAdReply: { showAdAttribution: true,
-        mediaUrl: "https://Instagram.com/Xiao_yan_21",
+        mediaUrl: sig,
         mediaType: "VIDEO",
-        description: "https://Instagram.com/Xiao_yan_21", 
-        title: 'Elaina-MultiDevice',
+        description: sig, 
+        title: wm3,
         body: wm,
         thumbnail: thumb,
         sourceUrl: sig
