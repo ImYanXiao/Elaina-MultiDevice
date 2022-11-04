4c625528df31f8f53a1cedf892d00ef18da1a772
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-11-04 15:30:42 +0700

    C

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -8,8 +8,8 @@ let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/Xiao_yan_21",
-    title: 'Elaina-MultiDevice',
+    mediaUrl: global.sig,
+    title: wm3,
     body: 'want source code?',
     thumbnail: thumb,
     sourceUrl: sig
