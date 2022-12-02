2193d4750b0877c59283f51d1426c764e8425c33
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-12-02 20:18:41 +0700

    Update xp-profile.js

--- plugins/xp-profile.js
+++ plugins/xp-profile.js
@@ -8,7 +8,6 @@ let handler = async (m, { conn }) => {
     let { premium, level, limit, exp, lastclaim, registered, regTime, age } = global.db.data.users[m.sender]
     let username = conn.getName(who)
     let name = conn.getName(who)
-    let anime = global.img
     let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let str = `
 ]──────────❏ *PROFILE* ❏──────────[
@@ -29,7 +28,7 @@ ${clockString(user.premiumTime)}
 					mediaUrl: sgc,
 					description: global.ucapan,
 					previewType: "PHOTO",
-					thumbnail: await (await fetch(anime)).buffer(),
+					thumbnail: await (await fetch(img)).buffer(),
 					sourceUrl: "https://github.com/ImYanXiao",					
 				}
 			}
