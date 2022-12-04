f1f8e50af90e434e8f357f4c44eba95ce88198ad
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-12-04 19:16:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Starting...🐾')
+console.log('🐾 Starting...')
 
 import yargs from 'yargs'
 import cfonts from 'cfonts'
@@ -32,7 +32,7 @@ function start(file) {
   setupMaster({ exec: args[0], args: args.slice(1) })
   let p = fork()
   p.on('message', data => {
-    console.log('[RECEIVED]✅', data)
+    console.log('[✅RECEIVED]', data)
     switch (data) {
       case 'reset':
         p.process.kill()
@@ -46,7 +46,7 @@ function start(file) {
   })
   p.on('exit', (_, code) => {
     isRunning = false
-    console.error('❗Exited with code:', code)
+    console.error('[❗]Exited with code:', code)
     if (code !== 0) return start(file)
     watchFile(args[0], () => {
       unwatchFile(args[0])
