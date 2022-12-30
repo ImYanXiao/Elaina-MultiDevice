b1b7bc5dc4697d0fa694492f4067507825435ee0
Re7Pntx <me@rlxfly.my.id>
2022-12-30 22:52:13 +0700

    Pacar gwe
    Emuach

--- plugins/internet-ssweb.js
+++ plugins/internet-ssweb.js
@@ -1,53 +1,68 @@
-import fetch from 'node-fetch'
+import axios from 'axios'
 
-let handler = async (m, { conn, command, args }) => {
-   if (!args[0]) return conn.reply(m.chat, 'Masukkan Linknya Kak', m)
-
-  await m.reply('_𝚃𝚞𝚗𝚐𝚐𝚞 𝚂𝚎𝚋𝚎𝚗𝚝𝚊𝚛⏰_')
-  
-   let img = await (await fetch(`https://botcahx.ddns.net/api/tools/ssweb?link=${args[0]}`)).buffer()
-
-  
-   conn.sendMessage(m.chat, { image: img, caption: 'Here' }, { quoted: m })
+let handler = async (m, { 
+conn, text, command, usedPrefix
+}) => {
+if (!text) return m.reply(`Gunakan format ${usedPrefix + command} <url>\n\n*Contoh :* ${usedPrefix + command} https://github.com/Rlxfly`)
+m.reply("_Loading. . ._")
+var phone = await ssweb(text, 'phone')
+var desktop = await ssweb(text, 'desktop')
+var tablet = await ssweb(text, 'tablet')
+var res = ``
+if (command === 'sshp') {
+await conn.sendFile(m.chat, phone.result, '',res, m, false)
 }
-handler.help = ['ssweb']
-handler.tags = ['internet']
-handler.command = /^ss(web)?f?$/i
+  if (command === 'ss') {
+await conn.sendFile(m.chat, phone.result, '',res, m, false)
+}
+if (command === 'ssweb' || command === 'sstablet') {
+await conn.sendFile(m.chat, tablet.result, '',res, m, false)
+}
+if (command === 'sspc') {
+await conn.sendFile(m.chat, desktop.result, '',res, m, false)
+}
+}
+handler.help = ['ssweb','sspc','sshp','sstablet'].map(v => v + ' <url>')
+handler.tags = ['tools']
+handler.command = /^(ss|ssweb|sstablet|sspc|sshp)$/i
 
 handler.limit = false
-handler.fail = null
 
 export default handler
 
-const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
-    fetch(url, options)
-        .then(response => response.json())
-        .then(json => {
-            // console.log(json)
-            resolve(json)
-        })
-        .catch((err) => {
-            reject(err)
-        })
-})
-
-
-const getBuffer = async (url, options) => {
-	try {
-		options ? options : {}
-		const res = await axios({
-			method: "get",
-			url,
-			headers: {
-				'DNT': 1,
-                    'User-Agent': 'GoogleBot',
-				'Upgrade-Insecure-Request': 1
-			},
-			...options,
-			responseType: 'arraybuffer'
-		})
-		return res.data
-	} catch (e) {
-		console.log(`Error : ${e}`)
-	}
+async function ssweb(url, device = 'desktop'){
+     return new Promise((resolve, reject) => {
+          const base = 'https://www.screenshotmachine.com'
+          const param = {
+            url: url,
+            device: device,
+            cacheLimit: 0
+          }
+          axios({url: base + '/capture.php',
+               method: 'POST',
+               data: new URLSearchParams(Object.entries(param)),
+               headers: {
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+               }
+          }).then((data) => {
+               const cookies = data.headers['set-cookie']
+               if (data.data.status == 'success') {
+                    axios.get(base + '/' + data.data.link, {
+                         headers: {
+                              'cookie': cookies.join('')
+                         },
+                         responseType: 'arraybuffer'
+                    }).then(({ data }) => {
+                       let result = {
+                            status: 200,
+                            author: 'Re7Pntx',
+                            result: data
+                        }
+                         resolve(result)
+                    })
+               } else {
+                    reject({ status: 404, author: 'Re7Pntx', message: data.data })
+               }
+          }).catch(reject)
+     })
 }
