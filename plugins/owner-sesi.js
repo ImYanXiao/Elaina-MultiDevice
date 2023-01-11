5aa78c8038b22c39a3f8ba9cbdd206104fa1a283
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-01-11 08:51:18 +0700

    Jir

--- plugins/owner-sesi.js
+++ plugins/owner-sesi.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 let handler = async (m, { conn, text }) => {
     m.reply('Tunggu Sebentar, Sedang mengambil file sesi bot')
     let sesi = await fs.readFileSync('./sessions/creds.json')
-    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'session.data.json' }, { quoted: m })
+    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'creds.json' }, { quoted: m })
 }
 handler.help = ['getsessi']
 handler.tags = ['owner']
