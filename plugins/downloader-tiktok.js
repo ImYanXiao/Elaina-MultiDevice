e255c500757e1469d0b0d8fef5076b0b89124d36
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-02-01 23:18:10 +0700

    Iyh

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,13 +1,50 @@
 import axios from 'axios'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = (await axios.get(API('males', '/tiktok2', { url: args[0] } ))).data;
-    if (res.status != 200) throw res.message;
-    if (!res) throw res.message;
-    conn.sendHydrated(m.chat, 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), res.video, sgh, '𝙼𝚢 𝙶𝚒𝚝𝚑𝚞𝚋 ❤', null, null, [['Audio', '.toaudio'], ['Donasi', '.donasi'], fakes, adReply)
+import cheerio from 'cheerio'
+var handler = async (m, { conn, args}) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+    const { thumbnail, video, audio } = await tiktokdl(args[0])
+    const url = video
+    if (!url) throw 'Can\'t download video!'
+    await conn.sendMessage(m.chat, { video: { url: url }},m) 
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok)?(dl)?)$/i
 
-export default handler
+handler.command = /^t(iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i
+
+export default handler 
+
+async function tiktokdl (url) {
+    if (!/tiktok/.test(url)) return error.link;
+    const gettoken = await axios.get("https://tikdown.org/id");
+    const $ = cheerio.load(gettoken.data);
+    const token = $("#download-form > input[type=hidden]:nth-child(2)").attr(
+        "value"
+    );
+    const param = {
+        url: url,
+        _token: token,
+    };
+    const {
+        data
+    } = await axios.request("https://tikdown.org/getAjax?", {
+        method: "post",
+        data: new URLSearchParams(Object.entries(param)),
+        headers: {
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+        },
+    });
+    getdata = cheerio.load(data.html);
+    if (data.status) {
+        return {
+            status: true,
+            thumbnail: getdata("img").attr("src"),
+            video: getdata("div.download-links > div:nth-child(1) > a").attr("href"),
+            audio: getdata("div.download-links > div:nth-child(2) > a").attr("href"),
+        };
+    } else
+        return {
+            status: false,
+        };
+};
