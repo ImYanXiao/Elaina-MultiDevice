5cf2855476a7fc40ca55994adc3a4f51120cd2b9
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-02-05 06:58:53 +0700

    Yasudah

--- plugins/_getvid.js
+++ plugins/_getvid.js
@@ -1,28 +1,9 @@
 import { 
     youtubedl,
-    youtubedlv2,
-    youtubedlv3
+    youtubedlv2 
 } from '@bochilteam/scraper'
-import fs from 'fs'
-const trol = {
-	key : {
-                          participant : '0@s.whatsapp.net'
-                        },
-       message: {
-                    orderMessage: {
-                            itemCount : 2525,
-                            itemCoun : 2525,
-                            surface :2525,
-                            message: author,
-                            orderTitle: 'B',
-                            thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                            sellerJid: '0@s.whatsapp.net'
-          
-                          }
-                        }
-                      }
 
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
+var handler = async (m, { conn, args}) => {
 
 let qu = args[1] || '360'
   let q = qu + 'p'
@@ -33,15 +14,18 @@ let qu = args[1] || '360'
     catch (e) { }
 
 // Kocak
-const yt = await youtubedlv2(v).catch(async _ => await youtubedl(v)).catch(async _ => await youtubedlv3(v))
+const yt = await youtubedl(v).catch(async () => await youtubedlv2(v))
 const dl_url = await yt.video[q].download()
   const ttl = await yt.title
 const size = await yt.video[q].fileSizeH
   
- await m.reply(wait)
-  await conn.sendMessage(m.chat, { [/^(?:-|--)doc$/i.test(args[1]) || null ? 'document' : 'video']: { url: dl_url }, fileName: `${author}.mp4`, mimetype: 'video/mp4', ..._thumb }, { quoted: trol })
+ await m.reply(`▢ Tɪᴛᴛʟᴇ: ${ttl}
+▢  Sɪᴢᴇ: ${size}
+
+▢ Ｌｏａｄｉｎｇ. . .`)
+  await conn.sendMessage(m.chat, { video : { url: dl_url, caption : ttl}}, { quoted: m })
 }
 
-handler.command = /^(getvid|ytmp4|ytv)$/i
-handler.limit = true
+
+handler.command = /^(getvid)$/i
 export default handler
