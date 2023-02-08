e44fec542548efdb506c7d9eca534d7ea9596bd3
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-02-08 19:41:09 +0700

    Njir

--- plugins/youtube-yts.js
+++ plugins/youtube-yts.js
@@ -9,8 +9,8 @@ let name = await conn.getName(m.sender)
     let listSections = []
 	Object.values(dapet).map((v, index) => {
 	listSections.push([index + ' ' + cmenub + ' ' + v.title, [
-          ['Video 🎥', usedPrefix + 'getvid' + v.url, '\n⌚ *Duration:* ' + v.timestamp + '\n⏲️ *Uploaded:* ' + v.ago + '\n👁️ *Views:* ' + v.views + '\n📎 *Url:* ' + v.url],
-          ['Audio 🎧', usedPrefix + 'getaud' + v.url, '\n⌚ *Duration:* ' + v.timestamp + '\n⏲️ *Uploaded:* ' + v.ago + '\n👁️ *Views:* ' + v.views + '\n📎 *Url:* ' + v.url]
+          ['Video 🎥', usedPrefix + 'getvid ' + v.url, '\n⌚ *Duration:* ' + v.timestamp + '\n⏲️ *Uploaded:* ' + v.ago + '\n👁️ *Views:* ' + v.views + '\n📎 *Url:* ' + v.url],
+          ['Audio 🎧', usedPrefix + 'getaud ' + v.url, '\n⌚ *Duration:* ' + v.timestamp + '\n⏲️ *Uploaded:* ' + v.ago + '\n👁️ *Views:* ' + v.views + '\n📎 *Url:* ' + v.url]
         ]])
 	}) 
 	return conn.sendList(m.chat, '*───「 Youtube Search 」───*', `Silahkan pilih type di bawah...\n*Teks yang anda minta:* ${text}\n\nKetik ulang *${usedPrefix + command}* teks anda untuk mengubah teks lagi`, author, `YouTube Search 🔎`, listSections, m)
