e13a54394b031def265f446a11e8581507a9885d
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-02-12 08:19:24 +0700

    Update info-totalfitur.js
    L

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
-let handler = async (m, { conn, args, command }) => {
+var handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
