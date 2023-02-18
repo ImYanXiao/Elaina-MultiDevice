2982123b67bf8b1c4a4470b96f842539a2e83f6d
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-02-18 18:26:31 +0700

    Feeling...

--- plugins/downloader-telestickers.js
+++ plugins/downloader-telestickers.js
@@ -1,7 +1,7 @@
-import { Telesticker } from 'xfarr-api'
 import { stickerTelegram } from '@bochilteam/scraper'
+import axios from 'axios'
 
-let handler = async (m, { conn, args }) => {
+var handler = async (m, { conn, args }) => {
 	if (args[0] && args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) {
 		let res = await Telesticker(args[0])
 		await m.reply(`Sending ${res.length} stickers...`)
@@ -28,3 +28,24 @@ handler.command = /^(telestic?ker|stic?kertele)$/i
 handler.limit = true
 
 export default handler
+
+//Thanks Xfarr : https://github.com/xfar05
+async function Telesticker(url) {
+    return new Promise(async (resolve, reject) => {
+        if (!url.match(/(https:\/\/t.me\/addstickers\/)/gi)) throw 'Enther your url telegram sticker'
+        packName = url.replace("https://t.me/addstickers/", "")
+        data = await axios(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, {method: "GET",headers: {"User-Agent": "GoogleBot"}})
+        const hasil = []
+        for (let i = 0; i < data.data.result.stickers.length; i++) {
+            fileId = data.data.result.stickers[i].thumb.file_id
+            data2 = await axios(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${fileId}`)
+            result = {
+            status: 200,
+            author: 'Xfarr05',
+            url: "https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/" + data2.data.result.file_path
+            }
+            hasil.push(result)
+        }
+    resolve(hasil)
+    })
+}
