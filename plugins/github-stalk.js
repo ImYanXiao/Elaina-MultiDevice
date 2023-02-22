0fb17d56c1c67e64b35de6054a0c7ff24d0244f9
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-02-22 17:03:05 +0700

    Fix ghstalk

--- plugins/github-stalk.js
+++ plugins/github-stalk.js
@@ -1,33 +1,60 @@
-import fetch from 'node-fetch'
-
-let handler = async(m, { conn, text }) => {
+import axios from 'axios'
+var handler = async(m, { conn, text }) => {
 
   if (!text) return conn.reply(m.chat, 'Harap Masukan Username', m)
 
   await m.reply('Searching...')
-    let res = await fetch(`https://hadi-api.herokuapp.com/api/githubstalk?username=${text}`)
-    let json = await res.json()
-    if (res.status !== 200) throw await res.text()
-    if (!json.status) throw json
-    let thumb = await (await fetch(json.result.avatar)).buffer()
+  let request = await githubstalk(text) 
+    let { username, following, followers, type, bio, company, blog, location, email, public_repo, public_gist, profile_pic } = request
+    let thumb = await getBuffer(profile_pic)
     let hasil = `*── 「 GITHUB STALK 」 ──*
-
-➸ *Bio*: ${json.result.bio}
-➸ *Perusahaan*: ${json.result.company}
-➸ *Email:* ${json.result.email}
-➸ *Twitter:* ${json.result.twiter_username}
-➸ *Repo Publik:* ${json.result.public_repo}
-➸ *Gists Publik:* ${json.result.public_gists}
-➸ *Follower:* ${json.result.follower}
-➸ *Following:* ${json.result.following}
-➸ *Lokasi:* ${json.result.location}
-➸ *Type:* ${json.result.Type}
+➸ *Username*: ${username}
+➸ *Bio*: ${bio}
+➸ *Perusahaan*: ${company}
+➸ *Email:* ${email}
+➸ *Blog:* ${blog}
+➸ *Repo Publik:* ${public_repo}
+➸ *Gists Publik:* ${public_gists}
+➸ *Follower:* ${followers}
+➸ *Following:* ${following}
+➸ *Lokasi:* ${location}
+➸ *Type:* ${type}
 `
 
     conn.sendFile(m.chat, thumb, 'githubstalk.jpg', hasil, m)
 }
 handler.help = ['githubstalk'].map(v => v + ' <query>')
-handler.tags = ['tools']
+handler.tags = ['internet']
 handler.command = /^(githubstalk)$/i
 
 export default handler
+
+async function githubstalk(user) {
+    return new Promise((resolve, reject) => {
+        axios.get('https://api.github.com/users/'+user)
+        .then(({ data }) => {
+            let hasil = {
+                username: data.login,
+                nickname: data.name,
+                bio: data.bio,
+                id: data.id,
+                nodeId: data.node_id,
+                profile_pic: data.avatar_url,
+                url: data.html_url,
+                type: data.type,
+                admin: data.site_admin,
+                company: data.company,
+                blog: data.blog,
+                location: data.location,
+                email: data.email,
+                public_repo: data.public_repos,
+                public_gists: data.public_gists,
+                followers: data.followers,
+                following: data.following,
+                ceated_at: data.created_at,
+                updated_at: data.updated_at
+            }
+            resolve(hasil)
+        })
+    })
+}
