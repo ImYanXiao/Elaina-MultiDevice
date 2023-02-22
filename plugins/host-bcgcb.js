f44929c117a1e07a7c250bf7b3a843ec7153a67e
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-02-22 16:22:26 +0700

    Yasudah

--- plugins/host-bcgcb.js
+++ plugins/host-bcgcb.js
@@ -1,18 +1,16 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, text } ) => {
+var handler = async (m, { conn, text } ) => {
  let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
 
 conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
  for (let id of groups) {
  let member = (await conn.groupMetadata(id)).participants.map(v => v.jid)
-conn.send2ButtonDoc(id, '────━┅ *BROADCAST* ┅━────\n' + text, wm, thumbbc, [['ᴏᴡɴᴇʀ 🎐', '.owner'],['ᴅᴏɴᴀsɪ ✨', '.donasi']], fpayment, { contextInfo: {
+conn.send2ButtonDoc(id, '────━┅ *BROADCAST* ┅━────\n' + text, wm, 'ғʀᴏᴍ ᴏᴡɴᴇʀ', thumbbc, [['ᴏᴡɴᴇʀ 🎐', '.owner'],['ᴅᴏɴᴀsɪ ✨', '.donasi']], flocation, { contextInfo: {
         externalAdReply: { showAdAttribution: true,
             title: `${htjava} BROADCAST`,
             body: titlebot,
             description: wm2,
             mediaType: 2,
-          thumbnail: await(await fetch(thumbbc)).buffer(),
+          thumbnail: await getBuffer(thumbbc),
          mediaUrl: sig
         }
      }
