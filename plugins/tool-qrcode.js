fede76ac37a99050a3cc84a796075b2d2fda02d3
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-03-19 15:44:42 +0700

    Yasudah

--- plugins/tool-qrcode.js
+++ plugins/tool-qrcode.js
@@ -1,8 +1,10 @@
-
 import { toDataURL } from 'qrcode'
 
-let handler = async (m, { conn, text }) => conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
+var handler = async (m, { conn, text }) => {
 
+if (!text) throw 'Teksnya mana banh?'
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
+}
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
 handler.command = /^qr(code)?$/i
