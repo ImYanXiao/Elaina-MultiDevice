d47b61dd6fa1816d3b2e08ca3d0b24e3f5aec1ec
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-03-23 18:51:07 +0700

    Don't forget create apikey :)

--- plugins/internet-openai.js
+++ plugins/internet-openai.js
@@ -1,30 +1,32 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from "openai"
+var handler = async (m, { conn, usedPrefix, command, text }) => {
+try {
+            if (!text) return reply(`Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Halo? `);
+            const configuration = new Configuration({
+              apiKey: 'Your Key', // Create Your Key
+            });                  // https://platform.openai.com/account/api-keys
+            const openai = new OpenAIApi(configuration);
 
-var handler = async (m, { text, command, usedPrefix }) => {
-     if (!text) throw `contoh:\n${usedPrefix + command} halo?`
-let res = await ask(text)
-await conn.reply(m.chat, res, m) 
+            const response = await openai.createCompletion({
+              model: "text-davinci-003",
+              prompt: text,
+              temperature: 0, // Higher values means the model will take more risks.
+              max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+              top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+              frequency_penalty: 0.3, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+              presence_penalty: 0 // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+          });
+            m.reply(`${response.data.choices[0].text}`);
+          } catch (error) {
+          if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+            console.log(`${error.response.status}\n\n${error.response.data}`);
+          } else {
+            console.log(error);
+            m.reply("error :"+ error.message);
+          }
+        }
 }
-handler.tags = ['internet']
 handler.command = /^(ai|openai)$/i
 export default handler
-
-async function ask(txt) {
-  const configuration = new Configuration({
-  apiKey: 'Your Key', // Create Your Key
-});                   // https://platform.openai.com/account/api-keys
-const openai = new OpenAIApi(configuration);
-  let text = `Q: ${txt}
-A:`
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: text,
-    temperature: 0.9,
-    max_tokens: 800,
-    top_p: 1,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-    stop: ["A:"],
-  });
-  return await response.data.choices[0].text
-		}
