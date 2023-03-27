6019c4e3fc3bcf03fcb1dda7ecec3d42a110ef66
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-03-27 20:42:13 +0700

    Okey Latest

--- plugins/internet-openai.js
+++ plugins/internet-openai.js
@@ -7,16 +7,11 @@ try {
             });                  // https://platform.openai.com/account/api-keys
             const openai = new OpenAIApi(configuration);
 
-            const response = await openai.createCompletion({
-              model: "text-davinci-003",
-              prompt: text,
-              temperature: 0, // Higher values means the model will take more risks.
-              max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-              top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-              frequency_penalty: 0.3, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-              presence_penalty: 0 // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+            const response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{role: "user", content: text}],
           });
-            m.reply(`${response.data.choices[0].text}`);
+          m.reply(`${response.data.choices[0].message.content}`);
           } catch (error) {
           if (error.response) {
             console.log(error.response.status);
@@ -24,7 +19,7 @@ try {
             console.log(`${error.response.status}\n\n${error.response.data}`);
           } else {
             console.log(error);
-            m.reply("error :"+ error.message);
+            m.reply("Maaf, sepertinya ada yang error :"+ error.message);
           }
         }
 }
