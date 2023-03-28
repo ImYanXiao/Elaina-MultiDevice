83ba433b9e8b0e1bb4c5eac64cd538134440787a
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-03-29 03:45:25 +0700

    :v

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,24 +1,16 @@
 //Credits Jangan Dihapus
 //Thanks To Papah-Chan
 import fetch from 'node-fetch'
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+var handler = async(m, { conn, text, usedPrefix, command }) => {
 
-let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
+    let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
     let waifu = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/waifu.txt')
     let chen = await waifu.text()
     let ran = chen.split('\n')
     let yae = ran[Math.floor(Math.random() * ran.length)]
     let thumb = await(await fetch(yae)).buffer()
-conn.sendButtonDoc(m.chat, str, wm, 'ᴡᴀɴᴛ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ?', 'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: global.sig,
-    title: wm3,
-    body: 'want source code?',
-    thumbnail: thumb,
-    sourceUrl: sig
-  }
-  } }) 
-          }
+    await conn.reply(m.chat, str, m, global.fakeig) 
+}
 handler.help = ['sc', 'script']
 handler.tags = ['info', 'main']
 handler.command =  /^(script|sc)$/i
