0b89a3df8803d6746bee94851b4c7974655c5a3b
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-03-28 23:44:28 +0700

    Without api 🙌

--- plugins/anime-ppcp.js
+++ plugins/anime-ppcp.js
@@ -1,11 +1,9 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, command }) => {
-let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${global.lolkey}`)
-if (res.status != 200) throw await res.text()
-let json = await res.json()
-if (!json.status) throw json
-conn.sendButton(m.chat, '𝙶𝚒𝚛𝚕𝚜', wm, json.result.female,[['NEXT', `/${command}`]], m)
-conn.sendButton(m.chat, '𝙱𝚘𝚢𝚜', wm, json.result.male, [['NEXT', `/${command}`]], m)
+import { Couples } from 'dhn-api'
+var handler = async (m, { conn, command }) => {
+
+const res = await Couples() 
+await conn.sendFile(m.chat, res.male, 'ppcp.jpg', 'Male', m) 
+return conn.sendFile(m.chat, res.female, 'ppcp.jpg', 'Female', m)
 }
 handler.help = ['ppcouple']
 handler.tags = ['internet']
