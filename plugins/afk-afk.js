26c854c4ec6e4247102c11af7e8ef187b5a6152f
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-03-29 13:08:55 +0700

    :v

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,23 +1,11 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-let handler = async (m, { text }) => {
-let name = m.pushName || conn.getName(m.sender)
-
-let user = global.db.data.users[m.sender]
-let thumb = await getBuffer(global.pic)
-user.afk = + new Date
-user.afkReason = text
- conn.sendButtonDoc(m.chat, `${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}`, wm, name, 'ᴊᴀɴɢᴀɴ ᴅɪɢᴀɴɢɢᴜ ʏᴀ ᴋᴀᴋ', 'Bilek', m,  { contextInfo: { externalAdReply: { showAdAttribution: true,
-        mediaUrl: sig,
-        mediaType: "VIDEO",
-        description: sig, 
-        title: wm3,
-        body: wm,
-        thumbnail: thumb,
-        sourceUrl: sig
-    }
-    } })
-            }
+var handler = async (m, { text }) => {
+  let user = global.db.data.users[m.sender]
+  user.afk = + new Date
+  user.afkReason = text
+  m.reply(`
+${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
+`)
+}
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
 handler.command = /^afk$/i
