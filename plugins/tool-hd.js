246aa182ba1d0e522897a0724815fc1407e3a5ea
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-04-04 00:20:14 +0700

    Update tool-hd.js
    Ytta

--- plugins/tool-hd.js
+++ plugins/tool-hd.js
@@ -1,26 +1,4 @@
-// import fs from 'fs'
-// import deepai from 'deepai'
-// deepai.setApiKey('31c3da72-e27e-474c-b2f4-a1b685722611')
-
-// let handler = async (m) => {
-// 	let q = m.quoted ? m.quoted : m
-// 	let mime = (q.msg || q).mimetype || ''
-// 	if (!mime) throw 'ʀᴇᴘʟʏ ɢᴀᴍʙᴀʀɴʏᴀ ᴋᴀᴋ (～￣▽￣)～'
-//     await m.reply(global.wait)
-// 	if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
-// 	let img = mime.split('/')[1]
-// 	img = Date.now() + '.' + img
-// 	fs.writeFileSync(`./${img}`, await q.download())
-// 	let resp = await deepai.callStandardApi('waifu2x', {
-// 		image: fs.readFileSync(`./${img}`)
-// 	})
-// 	await conn.sendFile(m.chat, resp.output_url, 'hd.jpg', 'ɪɴɪ ᴋᴀᴋ ʜᴀꜱɪʟɴʏᴀヾ(≧▽≦*)ᴏ', m).then(() => fs.unlinkSync(`./${img}`))
-// }
-// handler.help = ['hd <caption|reply media>']
-// handler.tags = ['tools|anime']
-// handler.command = /^(hd|jernih)$/i
-
-// export default handler
+// Credits By https://github.com/Xnuvers007
 import fs from 'fs';
 import fetch from 'node-fetch';
 import FormData from 'form-data';
@@ -54,7 +32,7 @@ let handler = async (m) => {
 };
 
 handler.help = ['hd <caption|reply media>'];
-handler.tags = ['tools|anime'];
+handler.tags = ['tools'];
 handler.command = /^(hd|jernih)$/i;
 
 export default handler;
