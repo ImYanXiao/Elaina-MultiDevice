f334c9eaeb7fb95277e4e71e9ef0e96020866811
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-04-05 21:30:48 +0700

    Refactoring

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -1,62 +1,26 @@
-let limit = 80
-import fetch from 'node-fetch'
-import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
-let name = await conn.getName(who)
+import { 
+    youtubedl,
+    youtubedlv2 
+} from '@bochilteam/scraper'
 
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
-  let chat = global.db.data.chats[m.chat]
-  const isY = /y(es)/gi.test(args[1])
-  const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
-  const limitedSize = (isPrems || isOwner ? 99 : limit) * 1024
-  let audio, source, res, link, lastError, isLimit
-  for (let i in _audio) {
-    try {
-      audio = _audio[i]
-      isLimit = limitedSize < audio.fileSize
-      if (isLimit) continue
-      link = await audio.download()
-      if (link) res = await fetch(link)
-      isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
-      if (isLimit) continue
-      if (res) source = await res.arrayBuffer()
-      if (source instanceof ArrayBuffer) break
-    } catch (e) {
-      audio = link = source = null
-      lastError = e
-    }
-  }
-  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw 'Error: ' + (lastError || 'Can\'t download audio')
-  if (!isY && !isLimit) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
-*${htki} YOUTUBE ${htka}*
+var handler = async (m, { conn, args }) => {
 
-*${htjava} Title:* ${title}
-*${htjava} Type:* mp3
-*${htjava} Filesize:* ${audio.fileSizeH}
+if (!args[0]) throw 'Urlnya Mana Banh?'
+  let q = '128kbps'
+  let v = args[0]
 
-*L O A D I N G. . .*
-`.trim(), m)
-  if (!isLimit) await conn.sendFile(m.chat, source, title + '.mp3', '', fakes, null, { fileLength: fsizedoc, seconds: fsizedoc, mimetype: 'audio/mp4', contextInfo: {
-          externalAdReply :{
-    body: 'Size: ' + audio.fileSizeH,
-    containsAutoReply: true,
-    mediaType: 2, 
-    mediaUrl: args[0],
-    showAdAttribution: true,
-    sourceUrl: args[0],
-    thumbnailUrl: thumbnail,
-    renderLargerThumbnail: true,
-    title: 'Nih Kak, ' + name,
-     }}
-  })
+// Kocak
+const yt = await youtubedl(v).catch(async () => await  youtubedlv2(v))
+const dl_url = await yt.audio[q].download()
+  const ttl = await yt.title
+const size = await yt.audio[q].fileSizeH
+  
+ await m.reply(`▢ Tɪᴛᴛʟᴇ: ${ttl}
+▢  Sɪᴢᴇ: ${size}
+
+▢ Ｌｏａｄｉｎｇ. . .`)
+await conn.sendMessage(m.chat, { document: { url: dl_url}, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3`}, {quoted: m})
 }
-handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> <without message>`)
-handler.tags = ['downloader']
-handler.command = /^y((outube|tb)audio|(outube|tb?)mp3|utubaudio|taudio|ta)$/i
-
-handler.exp = 0
-handler.register = false
-
+// If u want a not document you can change mimetype 'audio/mp4' n remove .mp3 on fileName
+handler.command = /^getaud|ytmp3$/i
 export default handler
