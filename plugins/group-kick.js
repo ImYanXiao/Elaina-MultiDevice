cf36a1459725428f6687e0f302da4f844d0a2620
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-04-05 13:03:38 +0700

    update fitur kick agar lebih mantap & hanya admin.
    update fitur kick, biar gak disalah gunakan oleh member. dan pengoptimalan kode agar lebih bagus
    fitur kick hanya bisa digunakan oleh admin seorang.

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,5 +1,13 @@
+// Code by Xnuvers007
+// https://github.com/Xnuvers007/
+
 import { areJidsSameUser } from '@adiwajshing/baileys'
-let handler = async (m, { conn, participants }) => {
+
+let handler = async (m, { conn, participants, isAdmin }) => {
+    if (!isAdmin) {
+        return m.reply('Perintah ini hanya dapat digunakan oleh admin grup')
+    }
+
     let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
     let kickedUser = []
     for (let user of users)
@@ -9,8 +17,8 @@ let handler = async (m, { conn, participants }) => {
             await delay(1 * 1000)
         }
     m.reply(`Mampos Dikick Kau ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
-
 }
+
 handler.help = ['kick'].map(v => v + ' @user')
 handler.tags = ['group']
 handler.command = /^(kick)$/i
@@ -18,7 +26,7 @@ handler.command = /^(kick)$/i
 handler.owner = false
 handler.group = true
 handler.botAdmin = true
-
-export default handler
+handler.admin = true // hanya admin grup yang dapat menggunakan perintah ini
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+export default handler
