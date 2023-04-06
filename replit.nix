31bd399918ed305ed2ab651469d00753563f1c57
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-04-07 02:11:37 +0700

    Remove unused package

--- replit.nix
+++ replit.nix
@@ -5,12 +5,8 @@
         pkgs.ffmpeg
         pkgs.imagemagick
         pkgs.git
-        pkgs.neofetch
         pkgs.libwebp
-        pkgs.speedtest-cli
-        pkgs.wget
         pkgs.yarn
-        pkgs.libuuid
     ];
     env = {
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
