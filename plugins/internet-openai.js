99485f7be40f798bebb4877ad5e241453364eba1
Ervan <91395824+Ervan2525@users.noreply.github.com>
2023-04-17 13:17:51 +0930

    Update internet-openai.js

--- plugins/internet-openai.js
+++ plugins/internet-openai.js
@@ -1,11 +1,12 @@
-import { Configuration, OpenAIApi } from "openai"
-var handler = async (m, { conn, usedPrefix, command, text }) => {
+import fetch from "node-fetch"
+import { generateWAMessageFromContent } from "@adiwajshing/baileys"
+import fs from 'fs'
+import { Configuration, OpenAIApi } from 'openai'
+const configuration = new Configuration({ organization: 'Your-Org', apiKey: 'Your-Key' });
+const openai = new OpenAIApi(configuration);
+let handler = async (m, { conn, text }) => {
 try {
             if (!text) return m.reply(`Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Halo? `);
-            const configuration = new Configuration({
-              apiKey: 'Your Key', // Create Your Key
-            });                  // https://platform.openai.com/account/api-keys
-            const openai = new OpenAIApi(configuration);
 
             const response = await openai.createChatCompletion({
           model: "gpt-3.5-turbo",
