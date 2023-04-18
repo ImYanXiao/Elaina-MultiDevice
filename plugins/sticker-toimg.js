5a647fcdb2e4caaa6451e73955318304d05c62b3
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-04-18 10:56:23 +0700

    Done

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -1,46 +1,16 @@
-//source? Https://github.com/Rlxfly 
-// Don't Delete Source! 
-import * as fs from 'fs'
-import { exec } from 'child_process'
-
-let handler = async (m, { conn }) => {
-	if (m.quoted && /sticker/.test(m.quoted.mtype) && !m.quoted.isAnimated) {
-		let img = await m.quoted.download()
-		await conn.sendMessage(m.chat, { image: img, jpegThumbnail: img }, { quoted: m })
-	} else if (m.quoted && /sticker/.test(m.quoted.mtype) && m.quoted.isAnimated) {
-		await m.reply('_In progress, please wait..._')
-		let img = await m.quoted.download()
-		let out = await webpToVideo(img)
-		await conn.sendMessage(m.chat, { video: out, gifPlayback: /gif/i.test(m.text), gifAttribution: ~~(Math.random() * 2) }, { quoted: m })
-	} else throw 'Reply a sticker!'
+import { webp2png } from '../lib/webp2mp4.js'
+var handler = async (m, { conn, usedPrefix, command }) => {
+    if (!m.quoted) throw `Reply sticker with command *${usedPrefix + command}*`
+    const q = m.quoted ? m.quoted : m
+    let name = await conn.getName(m.sender) 
+    let mime = q.mediaType || ''
+    if (!/sticker/.test(mime)) throw `Reply sticker with command *${usedPrefix + command}*`
+    let media = await q.download()
+    let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
+    await conn.sendFile(m.chat, out, 'out.png', 'Request By ' + name, m)
 }
-handler.help = ['toimg']
+handler.help = ['toimg (reply)']
 handler.tags = ['sticker']
-handler.command = /^toimg$/i
+handler.command = ['toimg']
 
 export default handler
-
-function webpToVideo(bufferImage) {
-	return new Promise((resolve, reject) => {
-		try {
-			let pathFile = "./tmp/" + ~~(Math.random() * 1000000 + 1) + ".webp"
-			fs.writeFileSync(pathFile, bufferImage)
-			exec(`convert ${pathFile} ${pathFile}.gif`, (error, stdout, stderr) => {
-				exec(`ffmpeg -i ${pathFile}.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${pathFile}.mp4`, (error, stdout, stderr) => {
-					if (!fs.existsSync(pathFile + ".gif") || !fs.existsSync(pathFile + ".mp4")) {
-						reject("Failed convert file!")
-						fs.unlinkSync(pathFile)
-						return
-					}
-					let videoBuffer = fs.readFileSync(pathFile + ".mp4")
-					fs.unlinkSync(pathFile)
-					fs.unlinkSync(pathFile + ".gif")
-					fs.unlinkSync(pathFile + ".mp4")
-					resolve(videoBuffer)
-				})
-			})
-		} catch(e) {
-			reject(e)
-		}
-	})
-}
