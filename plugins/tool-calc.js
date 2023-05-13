b80ddcb2f32840b9f55426ab6367d9d8a8aaf3e8
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-05-14 03:29:07 +0700

    Update tool-calc.js
    nambahin kuadrat dan akar

--- plugins/tool-calc.js
+++ plugins/tool-calc.js
@@ -1,3 +1,4 @@
+// update by xnuvers007
 let handler = async (m, { conn, text }) => {
   let id = m.chat
   conn.math = conn.math ? conn.math : {}
@@ -5,34 +6,43 @@ let handler = async (m, { conn, text }) => {
     clearTimeout(conn.math[id][3])
     delete conn.math[id]
     m.reply('Hmmm...ngecheat?')
+    return
   }
   let val = text
-    .replace(/[^0-9\-\/+*×÷πEe()piPI/]/g, '')
+    .replace(/[^0-9\-\/+*×÷πEe()#^√]/g, '')
     .replace(/×/g, '*')
     .replace(/÷/g, '/')
     .replace(/π|pi/gi, 'Math.PI')
     .replace(/e/gi, 'Math.E')
+    .replace(/#/g, 'Math.sqrt')
+    .replace(/√(\d+)/g, 'Math.sqrt($1)')
+    .replace(/(\d+)\s*√\s*(\d+)/g, '$1*Math.sqrt($2)')
+    .replace(/\^/g, '**')
     .replace(/\/+/g, '/')
     .replace(/\++/g, '+')
     .replace(/-+/g, '-')
-  let format = val
+  let format = text
     .replace(/Math\.PI/g, 'π')
     .replace(/Math\.E/g, 'e')
+    .replace(/Math\.sqrt/g, '√')
     .replace(/\//g, '÷')
     .replace(/\*×/g, '×')
+    .replace(/\\/g, '^')
   try {
-    console.log(val)
-    let result = (new Function('return ' + val))()
-    if (!result) throw result
-    m.reply(`*${format}* = _${result}_`)
+    let result = eval(val)
+    if (!isNaN(result)) {
+      m.reply(`*${format}* = ${result}`)
+    } else {
+      throw result
+    }
   } catch (e) {
-    if (e == undefined) throw 'Isinya?'
-    throw 'Format salah, hanya 0-9 dan Simbol -, +, *, /, ×, ÷, π, e, (, ) yang disupport'
+    if (e === undefined) throw 'Isinya?'
+    throw 'Format salah, hanya 0-9 dan Simbol -, +, *, /, ×, ÷, π, e, (, ), ^, √ yang disupport'
   }
 }
 handler.help = ['calc <expression>']
 handler.tags = ['tools']
 handler.command = /^(calc(ulat(e|or))?|kalk(ulator)?)$/i
-handler.exp = 5
+handler.exp = 100
 
 export default handler
