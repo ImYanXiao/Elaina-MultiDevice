8055f73c0c172f1ac1ddaffe41e3889304e7dd14
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-05-14 03:17:58 +0700

    Update tool-calc.js
    Nambahin kuadrat sama akar

--- plugins/tool-calc.js
+++ plugins/tool-calc.js
@@ -1,3 +1,5 @@
+// Update by Xnuvers007
+
 let handler = async (m, { conn, text }) => {
   let id = m.chat
   conn.math = conn.math ? conn.math : {}
@@ -5,29 +7,41 @@ let handler = async (m, { conn, text }) => {
     clearTimeout(conn.math[id][3])
     delete conn.math[id]
     m.reply('Hmmm...ngecheat?')
+    return
   }
   let val = text
-    .replace(/[^0-9\-\/+*×÷πEe()piPI/]/g, '')
+    .replace(/[^0-9\-\/+*×÷πEe()#^]/g, '')
     .replace(/×/g, '*')
     .replace(/÷/g, '/')
     .replace(/π|pi/gi, 'Math.PI')
     .replace(/e/gi, 'Math.E')
+    .replace(/#/g, 'Math.sqrt')
+    .replace(/\^/g, '**')
     .replace(/\/+/g, '/')
     .replace(/\++/g, '+')
     .replace(/-+/g, '-')
-  let format = val
+  let format = text
     .replace(/Math\.PI/g, 'π')
     .replace(/Math\.E/g, 'e')
+    .replace(/Math\.sqrt/g, '√')
     .replace(/\//g, '÷')
     .replace(/\*×/g, '×')
+    .replace(/\\/g, '^')
   try {
-    console.log(val)
-    let result = (new Function('return ' + val))()
-    if (!result) throw result
-    m.reply(`*${format}* = _${result}_`)
+    let result = eval(val)
+    if (!isNaN(result)) {
+      if (text.includes('√')) {
+        let number = text.match(/√(\d+)/)[1]
+        m.reply(`√${number} = ${Math.sqrt(number)}`)
+      } else {
+        m.reply(`*${format}* = ${result}`)
+      }
+    } else {
+      throw result
+    }
   } catch (e) {
-    if (e == undefined) throw 'Isinya?'
-    throw 'Format salah, hanya 0-9 dan Simbol -, +, *, /, ×, ÷, π, e, (, ) yang disupport'
+    if (e === undefined) throw 'Isinya?'
+    throw 'Format salah, hanya 0-9 dan Simbol -, +, *, /, ×, ÷, π, e, (, ), ^, √ yang disupport'
   }
 }
 handler.help = ['calc <expression>']
