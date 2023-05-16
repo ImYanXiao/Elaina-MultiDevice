eb31c36a5d167357d0206372a6478b006f085bf3
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-05-17 00:50:56 +0700

    Update downloader-igstalk.js
    Kumaha Barudak SMILE

--- plugins/downloader-igstalk.js
+++ plugins/downloader-igstalk.js
@@ -1,56 +1,87 @@
-import fetch from 'node-fetch'
-import axios from 'axios'
-import cheerio from 'cheerio'
+import fetch from 'node-fetch';
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 async function igstalk(Username) {
-  return new Promise((resolve, reject) => {
-    axios.get('https://dumpor.com/v/'+Username, {
-      headers: {
-        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
-      }
-    }).then(res => {
-      const $ = cheerio.load(res.data)
+  try {
+    const trResponse = await axios.get(`https://tr.deployers.repl.co/igstalk?user=${Username}`);
+    if (trResponse.status === 200) {
+      return trResponse.data;
+    } else {
+      throw new Error('Failed to fetch Instagram profile');
+    }
+  } catch (error) {
+    try {
+      const dumpoirResponse = await axios.get(`https://dumpoir.com/v/${Username}`, {
+        headers: {
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0'
+        }
+      });
+
+      const $ = cheerio.load(dumpoirResponse.data);
       const result = {
-        profile: $('#user-page > div.user > div.row > div > div.user__img').attr('style').replace(/(background-image: url\(\'|\'\);)/gi, ''),
-        fullname: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > div > a > h1').text(),
-        username: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > div > h4').text(),
-        post: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(1)').text().replace(' Posts',''),
-        followers: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(2)').text().replace(' Followers',''),
-        following: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(3)').text().replace(' Following',''),
-        bio: $('#user-page > div.user > div > div.col-md-5.my-3 > div').text()
-      }
-      resolve(result)
-    }).catch(e => {
+        bio: $('#user-page > div.user > div > div.col-md-5.my-3 > div').text().trim(),
+        credits: 'Xnuvers007 (https://github.com/Xnuvers007)',
+        followers: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(2)')
+          .text()
+          .replace(' Followers', '')
+          .trim(),
+        following: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(3)')
+          .text()
+          .replace(' Following', '')
+          .trim(),
+        fullname: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > div > a > h1').text().trim(),
+        post: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(1)')
+          .text()
+          .replace(' Posts', '')
+          .trim(),
+        profile: $('#user-page > div.user > div.row > div > div.user__img')
+          .attr('style')
+          .replace(/(background-image: url\(\'|\'\);)/gi, '')
+          .trim(),
+        status: 200,
+        url: `https://www.instagram.com/${Username.replace('@', '')}`,
+        username: `@${Username}`
+      };
+
+      return result;
+    } catch (e) {
       if (e.response?.status === 404) {
-        reject('Error: Akun tidak ditemukan')
+        throw new Error('Error: Akun tidak ditemukan');
       } else if (e.response?.status === 403) {
-        reject('Error: Akunnya Di Private')
+        throw new Error('Error: Akunnya Di Private');
       } else {
-        reject('Error: Failed to fetch Instagram profile')
+        throw new Error('Error: Failed to fetch Instagram profile');
       }
-    })
-  })
+    }
+  }
 }
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `Example use ${usedPrefix}${command} username`
-  let res = await igstalk(args[0]) 
-  
-  let username = res.username
-  let fullname = res.fullname
-  let post = res.post
-  let followe = res.followers
-  let followi = res.following
-  let bio = res.bio
-  let pepe = res.profile
+  if (!args[0]) throw `Example use ${usedPrefix}${command} username`;
 
-let data = `
+  try {
+    let res = await igstalk(args[0]);
+
+    let username = res.username;
+    let fullname = res.fullname;
+    let post = res.post;
+    let followe = res.followers;
+    let followi = res.following;
+    let bio = res.bio;
+    let pepe = res.profile;
+    let url = res.url;
+    let credits = res.credits;
+
+    let data = `
 💌 ᴜsᴇʀɴᴀᴍᴇ » 「 ${username} 」
 📧 ғᴜʟʟɴᴀᴍᴇ » 「 ${fullname} 」
 🎏 ${followe}  ғᴏʟʟᴏᴡᴇʀs
-🎀 ${followi}  ғᴏʟʟᴏᴡɪɴɢ
-📝 ᴘᴏsᴛ ${post} 
+🎀 ${followi} ғᴏʟʟᴏᴡɪɴɢ
+📝 ᴘᴏsᴛ ${post}
 📑 Bɪᴏ: ${bio}
+☣ url ${url}
+☣ Credits By ${credits}
 `.trim();
 
 if (pepe) {
@@ -59,10 +90,13 @@ if (pepe) {
 } else {
   conn.reply(m.chat, data, m);
 }
+} catch (error) {
+conn.reply(m.chat, error.message, m);
+}
+};
 
-handler.help = ['igstalk'].map(v => v + ' <username>')
-handler.tags = ['internet']
+handler.help = ['igstalk'].map(v => v + ' <username>');
+handler.tags = ['tools'];
+handler.command = /^(igstalk)$/i;
 
-handler.command = /^(igstalk)$/i
-
-export default handler
+export default handler;
