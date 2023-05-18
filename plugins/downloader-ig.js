d6c12a276c88a176fb59620e6836048cfdec2d01
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-05-18 21:58:41 +0700

    Update downloader-ig.js
    Ywsdh

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,15 +1,15 @@
-import * as bochil from '@bochilteam/scraper'
+import { instagramdlv3, instagramv2 } from '@bochilteam/scraper'
 var handler = async (m, { args }) => {
     if (!args[0]) throw 'Input URL'
     try { 
-    	let res = await bochil.instagramdlv2(args[0]) 
+    	let res = await instagramdlv2(args[0]) 
     let media = await res[0].url
     if (!res) throw 'Can\'t download the post'
     m.reply('_In progress, please wait..._')
     conn.sendMessage(m.chat, { video : { url : media }}, m) 
     } catch {
      try {
-     	let res2 = await bochil.instagramdlv3(args[0]) 
+     	let res2 = await instagramdlv3(args[0]) 
    let media2 = res2.url
    let cap = res2.title
      return this.sendFile(m.chat, media, 'instagram.mp4', cap, m)
