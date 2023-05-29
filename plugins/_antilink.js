4b0b6c79bd676e2332961e66b993385317a19c92
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-05-29 22:43:06 +0700

    Ywdh sih😂

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,24 +1,19 @@
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-export async function before(m, { isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
-    const isGroupLink = linkRegex.exec(m.text)
-    let hapus = m.key.participant
-    let pesan = m.key.id
-
-    if (chat.antiLink && isGroupLink && !isAdmin) {
-        if (isBotAdmin) {
-            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-            if (m.text.includes(linkThisGroup)) return !0
-        }
-        await conn.sendButton(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, author, ['off antilink', '/disable antilink'], m)
-        if (isOwner && isAdmin) {
-             await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: pesan, participant: hapus }})
-            conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!isOwner && isAdmin) return m.reply('Owner & Admin Not Removed!')
+let handler = m => m
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe) return true
+  let chat = global.db.data.chats[m.chat]
+    let isGroupLink = linkRegex.exec(m.text)
+  if (chat.antiLink && isGroupLink && !isAdmin && !m.isBaileys && m.isGroup) {
+    let thisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+  if (m.text.includes(thisGroup)) throw false // jika link grup itu sendiri gak dikick
+    if (!isBotAdmin) m.reply(` *「 ANTILINK 」* ${isAdmin ? "Admin mah bebas ygy :'v" : `\n\nlink group terdeteksi dan ${global.namebot} bukan admin jadi tidak bisa ngekick!`}`)
+  if (isBotAdmin) {
+      m.reply(` *「 ANTILINK 」* \n\nLink Group Terdeteksi, bye Kamu Akan Di Kick!!`.trim())
+      await this.delay(500)
+      await this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
     }
-    return !0
+  }
+  return true
 }
+export default handler
