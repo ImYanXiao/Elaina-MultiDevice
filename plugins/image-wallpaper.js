2b205b1560399c1c4d3b400c1ea6fd7749096183
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-06-09 09:38:39 +0700

    Update image-wallpaper.js

--- plugins/image-wallpaper.js
+++ plugins/image-wallpaper.js
@@ -1,13 +1,13 @@
-import { wallpaper, wallpaperv2 } from '@bochilteam/scraper'
+import { wallpaper } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
-    const res = await (/2/.test(command) ? wallpaperv2 : wallpaper)(text)
+    const res = await wallpaper(text)
     const img = res[Math.floor(Math.random() * res.length)]
     conn.sendFile(m.chat, img, 'wallpaper.jpg', `Result from *${text}*`, m)
 }
-handler.help = ['', '2'].map(v => 'wallpaper' + v + ' <query>')
+handler.help = ['wallpaper' + ' <query>']
 handler.tags = ['downloader']
 
-handler.command = /^(wallpaper2?)$/i
+handler.command = /^(wallpaper)$/i
 
 export default handler
