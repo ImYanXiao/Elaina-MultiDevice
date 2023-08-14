4edb307c9f80abf48944f0d38d076acdfc732fcd
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-08-15 02:52:04 +0700

    Create internet-pddikti.js

---
+++ plugins/internet-pddikti.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch';
+
+var handler = async (m, { conn, text }) => {
+  if (!text) throw `*_Masukan Nama Mahasiswa/Siswa Yang Ingin Kamu Cari !_*`;
+  conn.reply(m.chat, 'Sedang mencari Orangnya... Silahkan tunggu', m);
+  let res = await fetch('https://api-frontend.kemdikbud.go.id/hit_mhs/' + text);
+  if (!res.ok) throw 'Tidak Ditemukan';
+  let json = await res.json();
+  let message = '';
+  let Xnuvers007 = 'Scrape By Xnuvers007';
+
+  json.mahasiswa.forEach(data => {
+    let nama = data.text;
+    let websiteLink = data['website-link'];
+    let website = `https://pddikti.kemdikbud.go.id${websiteLink}`;
+    message += `\nNama = ${nama}\n\nData Ditemukan pada website = ${website}\n\n\n`;
+  });
+  const mySecret = 'Nomer kamu'
+  conn.reply(m.chat, message, m);
+  conn.reply(m.chat, `JANGAN LUPA SUPPORT DEVELOPERNYA\nXnuvers007\nhttps://saweria.co/xnuvers007\n\nfollow Me On github\nhttps://github.com/Xnuvers007\nDana = ${mySecret}`, m);
+}
+
+handler.help = ['mhs <nama>','mhs <nim>'];
+handler.tags = ['internet', 'mahasiswa', 'pendidikan'];
+handler.command = /^(mahasiswa|mhs|mhssiswa|msiswa|mhsiswa|pddikti|dikti)$/i;
+
+export default handler;
