034a04e31447350da17bd4a3fd1f0c9a247c0d75
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-08-15 03:07:29 +0700

    Update internet-ghsearch.js

--- plugins/internet-ghsearch.js
+++ plugins/internet-ghsearch.js
@@ -1,40 +1,48 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
+
 let handler = async (m, { text, command, usedPrefix }) => {
-    if (!text) throw `contoh:\n${usedPrefix + command} stikerinbot`
-    let res = await fetch(global.API('https://api.github.com', '/search/repositories', {
-        q: text
-    }))
-    if (!res.ok) throw eror
-    let json = await res.json()
-    let str = json.items.map((repo, index) => {
-        return `>      「 ${ 1 + index } 」       <
-ɴᴀᴍᴇ ʀᴇᴘᴏ : ${repo.name}
-ʙʏ : ${repo.owner.login}
-ғᴏʀᴋᴇᴅ : ${repo.fork ? 'True' : 'False'}
-ᴘʀɪᴠᴀᴛᴇ : ${repo.private ? 'True': 'False'}
+    if (!text) throw `Contoh:\n${usedPrefix + command} stikerinbot`;
+    try {
+        let res = await fetch(global.API('https://api.github.com', '/search/repositories', {
+            q: text
+        }));
+        if (!res.ok) throw 'Eror';
+        let json = await res.json();
 
-➔ ᴄʀᴇᴀᴛᴇᴅ ᴏɴ : ${formatDate(repo.created_at)}
-➔ ʟᴀsᴛ ᴜᴘᴅᴀᴛᴇ ᴏɴ :${formatDate(repo.updated_at)}
-👁  ${repo.watchers}   🍴  ${repo.forks}   ⭐  ${repo.stargazers_count}
-❗ ɪssᴜᴇ : ${repo.open_issues} ${repo.description ? `
-📚 ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:
-${repo.description}` : ''}
+        let repoDetails = json.items.map((repo, index) => {
+            return `
+            >      「 ${1 + index} 」       <
+            ɴᴀᴍᴇ ʀᴇᴘᴏ : ${repo.name}
+            ʙʏ : ${repo.owner.login}
+            ғᴏʀᴋᴇᴅ : ${repo.fork ? 'True' : 'False'}
+            ᴘʀɪᴠᴀᴛᴇ : ${repo.private ? 'True' : 'False'}
 
-⑂ ᴄʟᴏɴᴇ :
-$ git clone ${repo.clone_url}
-`.trim()
-    }).join('\n— — — — — — — — — — — — — —\n')
-    conn.sendHydrated(m.chat, `*${htki} ɢɪᴛʜᴜʙ sᴇᴀʀᴄʜ ${htka}*\n` + str, botdate, null, json.items.map((repo, index) => { return `${repo.html_url}` }), ['[ 1 ] ʟ ɪ ɴ ᴋ', '[ 2 ] ʟ ɪ ɴ ᴋ', '[ 3 ] ʟ ɪ ɴ ᴋ'], null,null, [[null,null],[null,null],[null,null]],m)
-}
-handler.help = ['githubsearch'].map(v => v + ' <pencarian>')
-handler.tags = ['internet','downloader']
+            ➔ ᴄʀᴇᴀᴛᴇᴅ ᴏɴ : ${formatDate(repo.created_at)}
+            ➔ ʟᴀsᴛ ᴜᴘᴅᴀᴛᴇ ᴏɴ : ${formatDate(repo.updated_at)}
+            👁  ${repo.watchers}   🍴  ${repo.forks}   ⭐  ${repo.stargazers_count}
+            ❗ ɪssᴜᴇ : ${repo.open_issues}${repo.description ? `\n📚 ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${repo.description}` : ''}
+            
+            ⑂ ᴄʟᴏɴᴇ :
+            $ git clone ${repo.clone_url}
+            `.trim();
+        }).join('\n— — — — — — — — — — — — — —\n');
 
-handler.command = /^g(ithub|h)s(earch)?$/i
+        let replyText = `*${htki} ɢɪᴛʜᴜʙ sᴇᴀʀᴄʜ ${htka}*\n${repoDetails}`;
+        conn.reply(m.chat, replyText, m);
 
-export default handler
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+handler.help = ['githubsearch'].map(v => v + ' <pencarian>');
+handler.tags = ['internet', 'downloader'];
+handler.command = /^g(ithub|h)s(earch)?$/i;
+
+export default handler;
 
 function formatDate(n, locale = 'id') {
-    let d = new Date(n)
+    let d = new Date(n);
     return d.toLocaleDateString(locale, {
         weekday: 'long',
         day: 'numeric',
@@ -43,5 +51,5 @@ function formatDate(n, locale = 'id') {
         hour: 'numeric',
         minute: 'numeric',
         second: 'numeric'
-    })
+    });
 }
