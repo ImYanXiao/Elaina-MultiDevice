d0f55ba8f3ca8d0b1abec262ce8bad53418c7bc8
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-08-15 09:27:23 +0700

    Places

--- plugins/game-susunkata_ans.js
+++ plugins/game-susunkata_ans.js
@@ -6,33 +6,27 @@ export async function before(m) {
         return !0
     this.susunkata = this.susunkata ? this.susunkata : {}
     if (!(id in this.susunkata))
-        return conn.sendButton(m.chat, 'Soal itu telah berakhir', author, null, buttonsusunkata, m)
+        return conn.reply(m.chat, 'Soal itu telah berakhir', m)
     if (m.quoted.id == this.susunkata[id][0].id) {
         let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
         if (isSurrender) {
             clearTimeout(this.susunkata[id][3])
             delete this.susunkata[id]
-            return conn.sendButton(m.chat, '*Yah Menyerah :( !*', author, null, buttonsusunkata, m)
+            return conn.reply(m.chat, '*Yah Menyerah :( !*', m)
         }
         let json = JSON.parse(JSON.stringify(this.susunkata[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.susunkata[id][2]
-            conn.sendButton(m.chat, `*Benar!*\n+${this.susunkata[id][2]} XP`, author, null, buttonsusunkata, m)
+            conn.reply(m.chat, `*Benar!*\n+${this.susunkata[id][2]} XP`, m)
             clearTimeout(this.susunkata[id][3])
             delete this.susunkata[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
             m.reply(`*Dikit Lagi!*`)
         else
-            conn.sendButton(m.chat, `*Salah!*`, author, null, [
-                ['Hint', '/suka'],
-                ['Nyerah', 'menyerah']
-            ], m)
+            conn.sendButton(m.chat, `*Salah!*` + '\nketik /suka untuk bantuan\nketik menyerah untuk mengakhiri permainan', m)
     }
     return !0
 }
 export const exp = 0
 
-const buttonsusunkata = [
-    ['susunkata', '/susunkata']
-]
