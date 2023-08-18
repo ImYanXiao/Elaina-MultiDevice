6db449cd04c73dd3738abaefbe94e6718adcb99b
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-08-18 18:26:26 +0700

    Update image-pinterest.js

--- plugins/image-pinterest.js
+++ plugins/image-pinterest.js
@@ -3,29 +3,33 @@ import sharp from 'sharp';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `Example use ${usedPrefix + command} minecraft`;
-  const json = await pinterest(text);
-  const imageUrl = json.getRandom().replace(/&amp;/g, '&');
-  
-  const response = await fetch(imageUrl);
-  const imageBuffer = await response.buffer();
-  
-  const maxWhatsAppWidth = 9999; // Lebar maksimum WhatsApp padahal 640 :V gw tambah 9999 :V
-  const metadata = await sharp(imageBuffer).metadata();
-  const imageWidth = metadata.width;
-  
-  let resizedImage;
-  if (imageWidth > maxWhatsAppWidth) {
-    resizedImage = await sharp(imageBuffer)
-      .resize({ width: maxWhatsAppWidth, withoutEnlargement: true })
-      .toBuffer();
-  } else {
-    resizedImage = imageBuffer;
-  }
-  
-  conn.sendFile(m.chat, resizedImage, 'pinterest.jpg', `
+
+  const maxImages = 10; // ubah mau kirim berapa foto (pake angka)
+  for (let i = 0; i < maxImages; i++) {
+    const json = await pinterest(text);
+    const imageUrl = json.getRandom().replace(/&amp;/g, '&');
+
+    const response = await fetch(imageUrl);
+    const imageBuffer = await response.buffer();
+
+    const maxWhatsAppWidth = 9999; // Lebar maksimum WhatsApp
+    const metadata = await sharp(imageBuffer).metadata();
+    const imageWidth = metadata.width;
+
+    let resizedImage;
+    if (imageWidth > maxWhatsAppWidth) {
+      resizedImage = await sharp(imageBuffer)
+        .resize({ width: maxWhatsAppWidth, withoutEnlargement: true })
+        .toBuffer();
+    } else {
+      resizedImage = imageBuffer;
+    }
+
+    conn.sendFile(m.chat, resizedImage, 'pinterest.jpg', `
 *Hasil pencarian*
 ${text}
 `.trim(), m);
+  }
 };
 handler.help = ['pinterest <keyword>'];
 handler.tags = ['internet'];
