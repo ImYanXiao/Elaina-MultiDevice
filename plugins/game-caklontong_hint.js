c308f3a442a94a23e114fa01b6ec5d86d3953c3d
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-08-21 02:19:41 +0700

    Create game-caklontong_hint.js

---
+++ plugins/game-caklontong_hint.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn }) => {
+    conn.caklontong = conn.caklontong ? conn.caklontong : {}
+    let id = m.chat
+    if (!(id in conn.caklontong)) throw false
+    let json = conn.caklontong[id][1]
+    let ans = json.jawaban
+    let clue = ans.replace(/[AIUEO]/gi, '_')
+    m.reply('```' + clue + '```')
+}
+handler.command = /^calo$/i
+
+// handler.limit = true
+
+export default handler
