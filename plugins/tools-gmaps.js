a0e80b42c94dec419bac95a279b343ff2b5be60d
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-08-23 23:22:38 +0700

    Update tools-gmaps.js

--- plugins/tools-gmaps.js
+++ plugins/tools-gmaps.js
@@ -1,15 +1,25 @@
-/*import { jarak } from "../lib/scrape.js"
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-let [dari, ke] = text.split(",")
-if (!dari || !ke) throw `*GMAPS JARAK*\n\n*• Gesek pesan ini* atau\nKetik *${usedPrefix}${command} pekalongan,sukabumi`
-let { result } = await jarak(dari, ke)
-
-conn.sendFile(m.chat, result.img, "", `*GOOGLE MAPS*
-${result.desc ? result.desc : "404"}`, m)
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+	let [dari, ke] = text.split('|') 
+	if (!dari || !ke) throw `Ex: ${usedPrefix + command} pekalongan|sukabumi`
+	let result = await jarak(from, to)
+	if (result.img) return conn.sendMessage(m.chat, { image: result.img, caption: result.desc }, { quoted: m })
+	else m.reply(result.desc)
 }
-handler.help = ['jarak', 'gmaps'].map(v => v + ' <dari> <ke>')
+
+handler.help = ['jarak', 'gmaps'].map(v => v + ' dari|ke')
 handler.tags = ['tools']
-handler.desc = ["Melihat jarak dari kota A ke kota B"]
 handler.command = /^(jarak|gmaps)$/i
 
-export default handler*/
+export default handler
+
+import axios from 'axios'
+import cheerio from 'cheerio'
+
+async function jarak(dari, ke) {
+	let html = (await axios(`https://www.google.com/search?q=${encodeURIComponent('jarak ' + dari + ' ke ' + ke)}&hl=id`)).data
+	let $ = cheerio.load(html), obj = {}
+	let img = html.split("var s=\'")?.[1]?.split("\'")?.[0]
+	obj.img = /^data:.*?\/.*?;base64,/i.test(img) ? Buffer.from(img.split`,` [1], 'base64') : ''
+	obj.desc = $('div.BNeawe.deIvCb.AP7Wnd').text()?.trim()
+	return obj
+}
