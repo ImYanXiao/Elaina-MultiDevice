7d39867c70ad06ab434716b54fc6c5b2581577dd
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-08-31 20:15:03 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,21 +1,27 @@
 import fs from 'fs'
-var handler = async (m, { conn, args, command }) => {
-let totalf = Object.values(global.plugins).filter(
-    (v) => v.help && v.tags
-  ).length;
- await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm + '\n\n' + botdate, giflogo, [['MENU','.menu']], m, {
-contextInfo: { externalAdReply :{
-                        mediaUrl: '',
-                        mediaType: 2,
-                        description: 'anu',
-                        title: bottime,
-                        body: 'Total Cintaku Padamu',          previewType: 0,
-                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                        sourceUrl: sig
-                      }}
-})
-}
 
+var handler = async (m, { conn, args, command }) => {
+    let totalf = Object.values(global.plugins).filter(
+        (v) => v.help && v.tags
+    ).length;
+
+    let replyMessage = `Total Fitur Bot Saat ini: ${totalf}\n`;
+
+    await conn.reply(m.chat, replyMessage, m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: '',
+                mediaType: 2,
+                description: 'anu',
+                title: bottime,
+                body: 'Total Cintaku Padamu',
+                previewType: 0,
+                thumbnail: fs.readFileSync("./thumbnail.jpg"),
+                sourceUrl: sig
+            }
+        }
+    });
+}
 
 handler.help = ['totalfitur']
 handler.tags = ['info']
