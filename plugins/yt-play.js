bb64165c8c931a0e076eceab5be4a1e20cec2deb
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-09-01 17:16:49 +0700

    Update yt-play.js

--- plugins/yt-play.js
+++ plugins/yt-play.js
@@ -58,7 +58,7 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
 
 handler.help = ['play'].map((v) => v + ' <pencarian>');
 handler.tags = ['downloader'];
-handler.command = /^play$/i;
+handler.command = /^(play|yt|youtubeplay|ytplay)$/i;
 
 handler.exp = 0;
 handler.limit = true;
