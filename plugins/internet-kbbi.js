a72347328f03a02d916cb3b4e9e1db7456a399d1
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-09-01 21:16:21 +0700

    Update internet-kbbi.js

--- plugins/internet-kbbi.js
+++ plugins/internet-kbbi.js
@@ -1,3 +1,47 @@
+import fetch from 'node-fetch';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const apiKey = process.env.xzn;
+const apiKey = 'APIKEYMU';
+
+const url = 'https://xzn.wtf/api/kbbi';
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) {
+    conn.reply(m.chat, `Silakan berikan kata yang ingin dicari artinya.\nContoh: ${usedPrefix + command} meja`, m);
+    return;
+  }
+
+  try {
+    const response = await fetch(`${url}?query=${encodeURIComponent(text)}&apikey=${apiKey}`);
+    const data = await response.json();
+
+    if (!data.error) {
+      const { title, arti } = data.data;
+      const result = `Kata: ${title}\nArti: ${arti}`;
+      conn.reply(m.chat, result, m);
+    } else {
+      console.error('API Error:', data.message); // Log the API error message
+      conn.reply(m.chat, 'Maaf, kata tidak ditemukan dalam kamus.', m);
+    }
+  } catch (error) {
+    console.error('Terjadi kesalahan saat mengambil data:', error);
+    conn.reply(m.chat, 'Terjadi kesalahan saat mengambil data.', m);
+  }
+};
+
+handler.help = ['kbbi <kata>'];
+handler.tags = ['tools'];
+handler.command = /^(kbbi|kamusbesarbahasaindonesia|kamusbahasa|kamusbahasaindonesia|kamusindonesia|kamusindo)$/i;
+
+export default handler;
+
+
+
+// BACKUP
+/*
 import { kbbi } from '@bochilteam/scraper'
 
 let handler = async (m, { text, usedPrefix, command }) => {
@@ -20,3 +64,4 @@ handler.tags = ['internet']
 handler.command = /^kbbi$/i
 
 export default handler
+*/
