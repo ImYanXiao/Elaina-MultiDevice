dfee39d55f704571803f9f019c604b485db3a191
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-09-02 09:25:04 +0700

    Update sticker-colong.js

--- plugins/sticker-colong.js
+++ plugins/sticker-colong.js
@@ -1,7 +1,7 @@
 let { MessageType } = (await import('@adiwajshing/baileys')).default
 import { sticker } from '../lib/sticker.js'
 
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn, args, usedPrefix, command }) => {
     let stiker = false
     try {
         let q = m.quoted ? m.quoted : m
@@ -13,18 +13,22 @@ let handler = async (m, { conn, args }) => {
             stiker = await sticker(img, false, senderName + ' ✅', 'Agar silahturahmi tidak terputus, izinkan saya pinjam seratus😁')
         } else if (args[0]) {
             let senderName = m.sender ? conn.getName(m.sender) : 'User'
-            stiker = await sticker(false, args[0], senderName + ' ✅', 'Agar silahturahmi tidak terputus, izinkan saya pinjam seratus😁')
+            let url = args[0]
+            if (!url.startsWith('http://') && !url.startsWith('https://')) {
+                // auto tambahkan https:// atau https:// kalo user lupa
+                url = 'https://' + url
+            }
+            stiker = await sticker(false, url, senderName + ' ✅', 'Agar silahturahmi tidak terputus, izinkan saya pinjam seratus😁')
         } else {
-            throw 'Reply stiker nya atau ketik `colong [link/url]`'
+            throw `Reply stiker nya  atau gambarnya atau ketik 'colong [link/url]'\n\n==============================\n${usedPrefix + command} <Reply gambar>\n${usedPrefix + command} <Reply sticker>\n${usedPrefix + command} https://api.duniagames.co.id/api/content/upload/file/7081780811647600895.png\n${usedPrefix + command} api.duniagames.co.id/api/content/upload/file/7081780811647600895.png\n==============================`
         }
     } finally {
         if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     }
 }
 
-handler.help = ['colong']
+handler.help = ['colong <reply sticker>', 'colong <reply gambar>', 'colong <URL/LINK>']
 handler.tags = ['sticker']
 handler.command = /^(colong|maling|colongsticker|colongstiker|malingsticker|malingstiker)$/i
-// handler.owner = true
 
 export default handler
