0fcd2e9194817d8850c2dc99d527a5cad270424b
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-09-03 19:02:02 +0700

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -2,16 +2,39 @@
 // https://github.com/Xnuvers007
 /////////////////////////////////
 
-let handler = async (m, { conn, text, participants }) => {
-  let users = participants.map(u => u.jid).filter(Boolean) // filter element yang tidak valid
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-  })
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+
+let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+
+    if (text || m.quoted?.text) {
+        m.reply(`Isi pesan : _*${text ? `${text}*_\n` : ''}\n\n\n┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
+            mentions: users
+        })
+
+        let usersDecode = participants.map(u => conn.decodeJid(u.id))
+        let q = m.quoted ? m.quoted : m
+        let c = m.quoted ? m.quoted : m.msg
+        const msg = conn.cMod(m.chat,
+            generateWAMessageFromContent(m.chat, {
+                [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+                    text: c || ''
+                }
+            }, {
+                quoted: m,
+                userJid: conn.user.jid
+            }),
+            text || q.text, conn.user.jid, { mentions: usersDecode }
+        )
+        await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+    } else {
+        m.reply("Reply pesan atau ketik pesan yang ingin di-tag.")
+    }
 }
 
 handler.help = ['tagall']
 handler.tags = ['group']
-handler.command = ['tagall']
+handler.command = ['tagall', 't']
 handler.admin = true
 handler.group = true
 
