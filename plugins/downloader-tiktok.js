7ceb9ef68a7f7abffbbfdc995b30d51fcb2c4e7c
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-09-05 17:55:22 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -75,7 +75,7 @@ var handler = async (m, { conn, args }) => {
   try {
     await conn.reply(m.chat, 'Tunggu sebentar kak, video sedang di download...', m);
 
-    const tiktokData = await downloadTikTok(args[0]);
+    const tiktokData = await tryServer1(args[0]);
 
     if (!tiktokData) {
       throw 'Gagal mendownload video!';
@@ -85,24 +85,47 @@ var handler = async (m, { conn, args }) => {
 
     const videoURLWatermark = tiktokData.video.watermark;
 
-    const ppTiktok = tiktokData.author.avatar;
+    let ppTiktok = '';
+    if (tiktokData.author && tiktokData.author.avatar) {
+      ppTiktok = tiktokData.author.avatar;
+    }
 
-    const infonya_gan =`Judul: ${tiktokData.title}\nUpload: ${tiktokData.created_at}\n\nSTATUS:\n=====================\nLike = ${tiktokData.stats.likeCount}\nKomen = ${tiktokData.stats.commentCount}\nShare = ${tiktokData.stats.shareCount}\nViews = ${tiktokData.stats.playCount}\nSimpan = ${tiktokData.stats.saveCount}\n=====================\n\nUploader: ${tiktokData.author.name} (${tiktokData.author.unique_id} - https://www.tiktok.com/@${tiktokData.author.unique_id}) \nBio: ${tiktokData.author.signature}\nLagu: ${tiktokData.music.play_url}\nResolusi: ${tiktokData.video.ratio}\nFoto Profile: ${ppTiktok}`;
+    const infonya_gan = `Judul: ${tiktokData.title}\nUpload: ${tiktokData.created_at}\n\nSTATUS:\n=====================\nLike = ${tiktokData.stats.likeCount}\nKomen = ${tiktokData.stats.commentCount}\nShare = ${tiktokData.stats.shareCount}\nViews = ${tiktokData.stats.playCount}\nSimpan = ${tiktokData.stats.saveCount}\n=====================\n\nUploader: ${tiktokData.author.name || 'Tidak ada informasi penulis'}\nLagu: ${tiktokData.music.play_url}\nResolusi: ${tiktokData.video.ratio}\nFoto Profile: ${ppTiktok}`;
 
     if (videoURL || videoURLWatermark) {
-      await conn.sendFile(m.chat, ppTiktok, 'profile.png', 'ini foto profilenya', m);
+      if (ppTiktok) {
+        await conn.sendFile(m.chat, ppTiktok, 'profile.png', 'ini foto profilenya', m);
+      }
       await conn.sendFile(m.chat, videoURL, 'tiktok.mp4', `Ini kak videonya\n\n${infonya_gan}`, m);
       setTimeout(async () => {
         await conn.sendFile(m.chat, videoURLWatermark, 'tiktokwm.mp4', `*Ini Versi Watermark*\n\n${infonya_gan}`, m);
         conn.reply(m.chat, "•⩊• Ini kak Videonya ૮₍ ˶ᵔ ᵕ ᵔ˶ ₎ა\nDitonton yah ₍^ >ヮ<^₎", m); 
       }, 5000);
-
-      // await conn.sendFile(m.chat, videoURLWatermark, 'tiktokwm.mp4', `*Ini Versi Watermark*\n\n${infonya_gan}`, m);
     } else {
       throw 'Tidak ada tautan video yang tersedia.';
     }
-  } catch (error) {
-    conn.reply(m.chat, `Error: ${error}`, m);
+  } catch (error1) {
+    // jika server 1 gagal, pake server 2
+    try {
+      const tiktokData2 = await tryServer2(args[0]);
+
+      if (!tiktokData2) {
+        throw 'Gagal mendownload video!';
+      }
+
+      const videoURL2 = tiktokData2.video;
+      const audioURL2 = tiktokData2.audio;
+      const thumbnailURL = tiktokData2.thumbnail;
+
+      // pengambilan data server 2
+      await conn.sendFile(m.chat, thumbnailURL, 'thumbnail.jpg', 'Ini thumbnail videonya', m);
+      await conn.sendFile(m.chat, videoURL2, 'tiktok2.mp4', 'Ini kak videonya dari Server 2', m);
+      await conn.sendFile(m.chat, audioURL2, 'tiktok.mp3', 'Ini kak audionya', m);
+
+    } catch (error2) {
+      // Jika server kedua juga gagal, tangani error di sini
+      conn.reply(m.chat, `Error: ${error2}`, m);
+    }
   }
 };
 
@@ -112,53 +135,52 @@ handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i;
 
 export default handler;
 
-async function downloadTikTok(url) {
-  // Try using tiklydown.eu.org API first
+async function tryServer1(url) {
+  // server 1 tiklydown.eu.org
   let tiklydownAPI = `https://api.tiklydown.eu.org/api/download?url=${url}`;
-  try {
-    let response = await axios.get(tiklydownAPI);
-    return response.data;
-  } catch (error) {
-    // If tiklydown.eu.org API fails, try using tikdown.org/id
-    return tiktokdl(url);
-  }
+  let response = await axios.get(tiklydownAPI);
+  return response.data;
 }
 
-async function tiktokdl(url) {
-  if (!/tiktok/.test(url)) {
-    throw 'Invalid TikTok URL!';
+async function tryServer2(url) {
+  // jika server 1 gagal, gunakan tikdown.org/id
+  const tikdownURL = 'https://tikdown.org/id';
+  const response = await axios.get(tikdownURL);
+
+  if (response.status === 200) {
+    const $ = cheerio.load(response.data);
+    const token = $('[name="_token"]').val();
+
+    const param = {
+      url: url,
+      _token: token,
+    };
+
+    const { data } = await axios.request('https://tikdown.org/getAjax?', {
+      method: 'post',
+      data: new URLSearchParams(Object.entries(param)),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
+      },
+    });
+
+    var getdata = cheerio.load(data.html);
+
+    if (data.status) {
+      return {
+        status: true,
+        thumbnail: $('.download-result .preview-image').attr('src'),
+        video: $('.download-result .download-links a[href*=".mp4"]').attr('href'),
+        audio: $('.download-result .download-links a[href*=".mp3"]').attr('href'),
+      };
+    } else {
+      return {
+        status: false,
+      };
+    }
   }
 
-  const gettoken = await axios.get('https://tikdown.org/id');
-  const $ = cheerio.load(gettoken.data);
-  const token = $('#download-form > input[type=hidden]:nth-child(2)').attr('value');
-  const param = {
-    url: url,
-    _token: token,
-  };
-
-  const { data } = await axios.request('https://tikdown.org/getAjax?', {
-    method: 'post',
-    data: new URLSearchParams(Object.entries(param)),
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
-    },
-  });
-
-  var getdata = cheerio.load(data.html);
-
-  if (data.status) {
-    return {
-      status: true,
-      thumbnail: getdata('img').attr('src'),
-      video: getdata('div.download-links > div:nth-child(1) > a').attr('href'),
-      audio: getdata('div.download-links > div:nth-child(2) > a').attr('href'),
-    };
-  } else {
-    return {
-      status: false,
-    };
-  }
+  throw 'Server 2 failed as well.';
 }
 */
