0b2cd5a7db46a6c1129b6028a5bffe33c13407a2
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-09-19 11:04:02 +0700

    Next Fix...

--- plugins/tool-ocr.js
+++ plugins/tool-ocr.js
@@ -1,6 +1,7 @@
+/*
 import uploadImage from '../lib/uploadImage.js'
 import ocrapi from 'ocr-space-api-wrapper'
-import { MessageType } from '@adiwajshing/baileys'
+
 let handler = async (m, { conn, text }) => {
       let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
@@ -18,3 +19,4 @@ handler.command = /^(ocr|totext)$/i
 handler.limit = true
 
 export default handler
+*/
