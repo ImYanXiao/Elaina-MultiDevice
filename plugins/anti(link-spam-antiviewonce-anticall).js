7c943f6ba9c5c67b737d996a733a0b79aee8b71a
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-09-19 10:48:57 +0700

    Update and rename _antilink-and-spam-antiviewonce.js to anti(link-spam-antiviewonce-anticall).js

--- plugins/anti(link-spam-antiviewonce-anticall).js
+++ plugins/anti(link-spam-antiviewonce-anticall).js
@@ -1,4 +1,5 @@
-let { downloadContentFromMessage } = (await import('@adiwajshing/baileys'));
+let { downloadContentFromMessage, WAMessageStubType } = (await import('@adiwajshing/baileys'));
+import { format } from 'util';
 
 export async function all(m) {
     if (!m.message)
@@ -66,4 +67,23 @@ export async function before(m, { isAdmin, isBotAdmin }) {
             return this.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
         }
     }
+}
+
+const isNumber = x => typeof x === 'number' && !isNaN(x)
+const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
+    clearTimeout(this)
+    resolve()
+}, ms))
+
+export async function all(m) {
+	if (m.fromMe && m.isBaileys) return !0
+	let text;
+	let setting = global.db.data.settings[this.user.jid]
+	if(!setting.anticall) return 
+	
+	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO )) {
+	    await delay(1000) 
+		await conn.reply(m.chat, `Kamu Diblokir Karena Menelpon Bot!!`, fakes)
+		return conn.updateBlockStatus(m.chat, "block") 
+	}
 }
