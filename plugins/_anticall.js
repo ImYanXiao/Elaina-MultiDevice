1f5280e2344ee43048360bfeaf4c0db7354bad19
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-09-22 07:26:38 +0700

    Kerja Bagus

---
+++ plugins/_anticall.js
@@ -0,0 +1,19 @@
+const { WAMessageStubType } = (await import('@adiwajshing/baileys')).default
+const isNumber = x => typeof x === 'number' && !isNaN(x)
+const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
+    clearTimeout(this)
+    resolve()
+}, ms))
+
+export async function all(m) {
+	if (m.fromMe && m.isBaileys) return !0
+	let text;
+	let setting = global.db.data.settings[this.user.jid]
+	if(!setting.anticall) return 
+	
+	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO )) {
+	    await delay(1000) 
+		await conn.reply(m.chat, `Kamu Diblokir Karena Menelpon Bot!!`, fakes)
+		return conn.updateBlockStatus(m.chat, "block") 
+	}
+}
