c8090ab911eeb29be4cf35c4c3a7e96ba3971dc7
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-11-10 16:30:21 +0700

    Avoid Error: EISDIR: illegal operation on a directory
    Menghindari Error Illegal operation

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -1,29 +1,46 @@
-import { tmpdir } from 'os'
-import path, { join } from 'path'
-import {
-  readdirSync,
-  statSync,
-  unlinkSync,
-  existsSync,
-  readFileSync,
-  watch
-} from 'fs'
+import { tmpdir } from 'os';
+import path, { join } from 'path';
+import { readdirSync, statSync, unlinkSync, existsSync } from 'fs';
+
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 
-conn.reply(m.chat, 'Succes !', m)
+  const tmp = [tmpdir(), join(__dirname, '../tmp')];
+  const filenames = [];
 
-const tmp = [tmpdir(), join(__dirname, '../tmp')]
-  const filename = []
-  tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
-  return filename.map(file => {
-    const stats = statSync(file)
-    unlinkSync(file)
-})
-}
-handler.help = ['cleartmp']
-handler.tags = ['owner']
-handler.command = /^(cleartmp)$/i
+  tmp.forEach(dirname => {
+    readdirSync(dirname).forEach(file => {
+      filenames.push(join(dirname, file));
+    });
+  });
 
-handler.rowner = true
+  const deletedFiles = [];
 
-export default handler
+  filenames.forEach(file => {
+    const stats = statSync(file);
+
+    if (stats.isDirectory()) {
+      console.log(`Skipping directory: ${file}`);
+    } else {
+      unlinkSync(file);
+      deletedFiles.push(file);
+    }
+  });
+
+  conn.reply(m.chat, 'Success!', m);
+
+  if (deletedFiles.length > 0) {
+    console.log('Deleted files:', deletedFiles);
+    conn.reply(m.chat, `Deleted files:\n${deletedFiles.join('\n')}`, m);
+  }
+
+  if (deletedFiles.length == 0) {
+    conn.reply(m.chat, 'file tidak ada file yang tersisa di tmp', m);
+  }
+};
+
+handler.help = ['cleartmp'];
+handler.tags = ['owner'];
+handler.command = /^(cleartmp|clear|tmpclear|cleantmp)$/i;
+handler.rowner = true;
+
+export default handler;
