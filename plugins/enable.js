edbc9a346366e80589b1477ee65929dfd4d7c4da
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-11-19 01:34:43 +0700

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -1,257 +1,189 @@
 var handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
-	const sections = "List Options : /n✨ | Welcome/n🚫 | Delete/n👁 | Antiviewonce/n🤖 | Self/n🌎 | Public/🗣️ | Simi/n🔞 | Nsfw/n🌟 | PremNsfwChat/n🔗 | Antilink/n☎ | AntiCall/n🚫 | Antidelete/n📩 | Antispam/n🖼 | Autosticker/n⏏️ | Autolevelup/n🔎 | Detect/n❗ | Restrict/n😐 | Nyimak/n☑️ | Autoread/n💬 | PcOnly/n🏢 | GcOnly/n📷 | SwOnly/n🎌| AnimeUpdate\n"
-     const contoh = sections + usedPrefix + "enable self"
-  let isEnable = /true|enable|(turn)?on|1/i.test(command)
-  let chat = global.db.data.chats[m.chat]
-  let user = global.db.data.users[m.sender]
-  let bot = global.db.data.settings[conn.user.jid] || {}
-  let type = (args[0] || '').toLowerCase()
-  let isAll = false, isUser = false
+	const sections = "List Options :\n✨ | Welcome\n🚫 | Delete\n👁 | Antiviewonce\n🤖 | Self\n🌎 | Public\n🗣️ | Simi\n🔞 | Nsfw\n🌟 | PremNsfwChat\n🔗 | Antilink\n☎ | AntiCall\n🚫 | Antidelete\n📩 | Antispam\n🖼 | Autosticker\n⏏️ | Autolevelup\n🔎 | Detect\n❗ | Restrict\n😐 | Nyimak\n☑️ | Autoread\n💬 | PcOnly\n🏢 | GcOnly\n📷 | SwOnly\n🎌| AnimeUpdate\n";
+  const contoh = sections + usedPrefix + "enable self";
+  let isEnable = /true|enable|(turn)?on|1/i.test(command);
+  let chat = global.db.data.chats[m.chat];
+  let user = global.db.data.users[m.sender];
+  let bot = global.db.data.settings[conn.user.jid] || {};
+  let type = (args[0] || '').toLowerCase();
+  let isAll = false, isUser = false;
+  
   switch (type) {
     case 'welcome':
       if (!m.isGroup) {
         if (!isOwner) {
-          global.dfail('group', m, conn)
-          throw false
+          return await conn.reply(m.chat, 'Only owner can use this command in private chat!', m);
         }
       } else if (!isAdmin) {
-        global.dfail('admin', m, conn)
-        throw false;
+        return await conn.reply(m.chat, 'Only admin can use this command in group chat!', m);
       }
-      chat.welcome = isEnable
+      chat.welcome = isEnable;
       break;
-     case 'detect':
-       if (!m.isGroup) {
-         if (!isOwner) {
-           global.dfail('group', m, conn)
-           throw false;
-         }
-       } else if (!isAdmin) {
-         global.dfail('admin', m, conn)
-         throw false;
-       }
-       chat.detect = isEnable
-       break;
+    case 'detect':
+      if (!m.isGroup) {
+        if (!isOwner) {
+          return await conn.reply(m.chat, 'Only owner can use this command in private chat!', m);
+        }
+      } else if (!isAdmin) {
+        return await conn.reply(m.chat, 'Only admin can use this command in group chat!', m);
+      }
+      chat.detect = isEnable;
+      break;
     case 'viewonce':
     case 'antiviewonce':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false;
+          return await conn.reply(m.chat, 'Only admin or owner can use this command in group chat!', m);
         }
       }
-      chat.viewonce = isEnable
+      chat.viewonce = isEnable;
       break;
-        case 'banned':
-        case 'restrick':
-        case 'ban':
-            if (m.isGroup) {
-                if (!isOwner) {
-                    conn.reply(m.chat, 'You are not authorized to perform this action.', m);
-                    throw false;
-                }
-            }
-            break;
     case 'delete':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false;
+          return await conn.reply(m.chat, 'Only admin or owner can use this command in group chat!', m);
         }
       }
-      chat.delete = isEnable
+      chat.delete = isEnable;
       break;
-    // case 'antidelete':
-    //   if (m.isGroup) {
-    //     if (!(isAdmin || isOwner)) {
-    //       global.dfail('admin', m, conn)
-    //       throw false
-    //     }
-    //   }
-    //   chat.delete = isEnable
-    //   break
-        case 'antidelete':
-            if (m.isGroup) {
-                if (!(isAdmin || isOwner)) {
-                    conn.reply(m.chat, 'You are not authorized to perform this action.', m);
-                    throw false;
-                }
-            }
-            chat.delete = !isEnable;
-            break;
-    // case 'autodelvn':
-    //   if (m.isGroup) {
-    //     if (!(isAdmin || isOwner)) {
-    //       global.dfail('admin', m, conn)
-    //       throw false
-    //     }
-    //   }
-    //   chat.autodelvn = isEnable
-    //   break
-     case 'document':
-       chat.useDocument = isEnable
-      break;
-      case 'self':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
+    case 'antidelete':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          return await conn.reply(m.chat, 'Only admin or owner can use this command in group chat!', m);
+        }
       }
-      global.opts['self'] = isEnable
-      break
+      chat.delete = !isEnable;
+      break;
+    case 'document':
+      chat.useDocument = isEnable;
+      break;
+    case 'self':
+      isAll = true;
+      if (!isROwner) {
+        return await conn.reply(m.chat, 'Only regular owner can use this command!', m);
+      }
+      global.opts['self'] = isEnable;
+      break;
     case 'public':
-      isAll = true
+      isAll = true;
       if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
+        return await conn.reply(m.chat, 'Only regular owner can use this command!', m);
       }
-      global.opts['self'] = !isEnable
-      break
+      global.opts['self'] = !isEnable;
+      break;
     case 'antilink':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
+          return await conn.reply(m.chat, 'Only admin or owner can use this command in group chat!', m);
         }
       }
-      chat.antiLink = isEnable
-      break
-      case 'autoSticker':
+      chat.antiLink = isEnable;
+      break;
+    case 'autoSticker':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
+          return await conn.reply(m.chat, 'Only admin or owner can use this command in group chat!', m);
         }
       }
-      chat.autoSticker = isEnable
-      break
-      case 'autoupnime':
-        if (!isROwner) {
-          global.dfail('rowner', m, conn)
-          throw false
-        }
-      chat.updateAnime = isEnable
-      break
-      case 'simi':
-        if (!isROwner) {
-          global.dfail('rowner', m, conn)
-          throw false
-        }
-      chat.simi = isEnable
-      break
-      case 'antispam':
-       if (m.isGroup) {
-         if (!(isAdmin || isOwner)) {
-           global.dfail('admin', m, conn)
-           throw false
-         }
-       }
-       chat.antiSpam = isEnable
-       break
-       case 'anticall':
-       isAll = true
-         if (!isOwner) {
-           global.dfail('rowner', m, conn)
-           throw false
-       }
-       chat.anticall = isEnable
-       break
-      case 'nsfw':
-        if (m.isGroup) {
+      chat.autoSticker = isEnable;
+      break;
+    case 'autoupnime':
+      if (!isROwner) {
+        return await conn.reply(m.chat, 'Only regular owner can use this command!', m);
+      }
+      chat.updateAnime = isEnable;
+      break;
+    case 'simi':
+      if (!isROwner) {
+        return await conn.reply(m.chat, 'Only regular owner can use this command!', m);
+      }
+      chat.simi = isEnable;
+      break;
+    case 'antispam':
+      if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }}
-      chat.nsfw = isEnable
-      break
-      case 'premnsfwchat':
-        if (m.isGroup) {
-        if (!isROwner) {
-          global.dfail('rowner', m, conn)
-          throw false
-        }}
-      chat.premnsfw = isEnable
-      break
-    // case 'toxic':
-    //   if (m.isGroup) {
-    //     if (!(isAdmin || isOwner)) {
-    //       global.dfail('admin', m, conn)
-    //       throw false
-    //     }
-    //   }
-    //   chat.antiToxic = !isEnable
-    //   break
-    case 'autolevelup':
-    isUser = true
-    user.autolevelup = isEnable
-     break
-    // case 'mycontact':
-    // case 'mycontacts':
-    // case 'whitelistcontact':
-    // case 'whitelistcontacts':
-    // case 'whitelistmycontact':
-    // case 'whitelistmycontacts':
-    //   if (!isOwner) {
-    //     global.dfail('owner', m, conn)
-    //     throw false
-    //   }
-    //   conn.callWhitelistMode = isEnable
-    //   break
-    case 'restrict':
-      isAll = true
+          return await conn.reply(m.chat, 'Only admin or owner can use this command in group chat!', m);
+        }
+      }
+      chat.antiSpam = isEnable;
+      break;
+    case 'anticall':
+      isAll = true;
       if (!isOwner) {
-        global.dfail('owner', m, conn)
-        throw false
+        return await conn.reply(m.chat, 'Only owner can use this command!', m);
       }
-      bot.restrict = isEnable
-      break
+      chat.anticall = isEnable;
+      break;
+    case 'nsfw':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          return await conn.reply(m.chat, 'Only admin or owner can use this command in group chat!', m);
+        }
+      }
+      chat.nsfw = isEnable;
+      break;
+    case 'premnsfwchat':
+      if (m.isGroup) {
+        if (!isROwner) {
+          return await conn.reply(m.chat, 'Only regular owner can use this command!', m);
+        }
+      }
+      chat.premnsfw = isEnable;
+      break;
+    case 'autolevelup':
+      isUser = true;
+      user.autolevelup = isEnable;
+      break;
+    case 'restrict':
+      isAll = true;
+      if (!isOwner) {
+        return await conn.reply(m.chat, 'Only owner can use this command!', m);
+      }
+      bot.restrict = isEnable;
+      break;
     case 'nyimak':
-      isAll = true
+      isAll = true;
       if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
+        return await conn.reply(m.chat, 'Only regular owner can use this command!', m);
       }
-      global.opts['nyimak'] = isEnable
-      break
+      global.opts['nyimak'] = isEnable;
+      break;
     case 'autoread':
-      isAll = true
+      isAll = true;
       if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
+        return await conn.reply(m.chat, 'Only regular owner can use this command!', m);
       }
-      global.opts['autoread'] = isEnable
-      break
+      global.opts['autoread'] = isEnable;
+      break;
     case 'pconly':
     case 'privateonly':
-      isAll = true
+      isAll = true;
       if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
+        return await conn.reply(m.chat, 'Only regular owner can use this command!', m);
       }
-      global.opts['pconly'] = isEnable
-      break
+      global.opts['pconly'] = isEnable;
+      break;
     case 'gconly':
     case 'grouponly':
-      isAll = true
+      isAll = true;
       if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
+        return await conn.reply(m.chat, 'Only regular owner can use this command!', m);
       }
-      global.opts['gconly'] = isEnable
-      break
+      global.opts['gconly'] = isEnable;
+      break;
     case 'swonly':
     case 'statusonly':
-      isAll = true
+      isAll = true;
       if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
+        return await conn.reply(m.chat, 'Only regular owner can use this command!', m);
       }
-      global.opts['swonly'] = isEnable
-      break
-        default:
-            if (!/[01]/.test(command)) return conn.reply(m.chat, contoh, m) 
-    }
-    conn.sendMessage(m.chat, `*${htki} OPTIONS ${htka}*\n🗂️ *Type:* ${type}\n📊 *Status:* Success ✅\n🎚️ *Options:* ${isEnable ? 'Enable' : 'Disable'}\n📣 *For:* ${isAll ? 'This Bot' : isUser ? '' : 'This Chats'}`, { quoted: m });
+      global.opts['swonly'] = isEnable;
+      break;
+    default:
+      if (!/[01]/.test(command)) return await conn.reply(m.chat, contoh, m);
+  }
+
+  conn.reply(m.chat, `*––––––『 OPTIONS 』––––––*\n🗂️ *Type:* ${type}\n📊 *Status:* Success ✅\n🎚️ *Options:* ${isEnable ? 'Enable' : 'Disable'}\n📣 *For:* ${isAll ? 'This Bot' : isUser ? '' : 'This Chats'}`, m);
 };
 
 handler.help = ['enable', 'disable'].map(v => v + 'able <option>');
