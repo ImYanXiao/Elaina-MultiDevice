25d71a52fd2b0e11222584a0ddb2e0919221b98a
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-11-19 01:54:48 +0700

    Update group-info.js
    alternatif buat menghindari image is not a buffer

--- plugins/group-info.js
+++ plugins/group-info.js
@@ -1,9 +1,11 @@
 let handler = async (m, { conn, participants, groupMetadata }) => {
-    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
-    const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, delete: del } = global.db.data.chats[m.chat]
-    const groupAdmins = participants.filter(p => p.admin)
-    const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
-    const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
+    const ppUrl = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null);
+    const ppBuffer = ppUrl ? await conn.downloadMediaMessage({ url: ppUrl }) : null;
+
+    const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, delete: del } = global.db.data.chats[m.chat];
+    const groupAdmins = participants.filter(p => p.admin);
+    const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
+    const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
     let text = `*「 Group Information 」*\n
 *ID:* 
 ${groupMetadata.id}
@@ -28,14 +30,20 @@ Welcome: ${sWelcome}
 Bye: ${sBye}
 Promote: ${sPromote}
 Demote: ${sDemote}
-`.trim()
-    conn.sendFile(m.chat, pp, 'pp.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
+`.trim();
+
+    if (ppBuffer) {
+        conn.sendFile(m.chat, ppBuffer, 'pp.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] });
+        conn.sendFile(m.chat, Buffer.from(ppBuffer), 'pp.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] });
+    } else {
+        conn.reply(m.chat, text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] });
+    }
 }
 
-handler.help = ['infogrup']
-handler.tags = ['group']
-handler.command = /^(gro?upinfo|info(gro?up|gc))$/i
+handler.help = ['infogrup'];
+handler.tags = ['group'];
+handler.command = /^(gro?upinfo|info(gro?up|gc))$/i;
 
-handler.group = true
+handler.group = true;
 
-export default handler
+export default handler;
