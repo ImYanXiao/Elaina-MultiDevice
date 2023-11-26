cf4b0d9e348953bbdcb306f32b44dab9ff86cd5d
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-11-26 15:31:13 +0700

    Update tool-ocr.js

--- plugins/tool-ocr.js
+++ plugins/tool-ocr.js
@@ -1,19 +1,55 @@
 import uploadImage from '../lib/uploadImage.js'
 import ocrapi from 'ocr-space-api-wrapper'
 const { MessageType } = (await import('@adiwajshing/baileys')).default
-let handler = async (m, { conn, text }) => {
-      let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (!mime) throw `balas gambar dengan perintah .ocr`
-    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*jenis ${mime} tidak didukung!*_`
-    let img = await q.download()
-    let url = await uploadImage(img)
-    let hasil = await ocrapi.ocrSpace(url)
- await m.reply(hasil.ParsedResults[0].ParsedText)    
+
+async function performOCR(url) {
+  try {
+    return await ocrapi.ocrSpace(url)
+  } catch (error) {
+    console.error(error)
+    return null
+  }
 }
 
-handler.help = ['ocr', 'totext']
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+
+    if (!mime) throw `Balas gambar dengan perintah ${usedPrefix}${command}`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `Jenis ${mime} tidak didukung`
+
+    let img = await q.download()
+    let url = await uploadImage(img)
+
+    m.reply('tunggu sebentar...')
+
+    let maxRetries = 99
+    let retryCount = 0
+    let hasil
+
+    do {
+      hasil = await performOCR(url)
+      retryCount++
+    } while (!hasil && retryCount < maxRetries)
+
+    if (hasil && hasil.ParsedResults && hasil.ParsedResults.length > 0) {
+      
+      let parsedText = hasil.ParsedResults[0].ParsedText;
+      
+      await m.reply(`${parsedText}`);
+      
+    } else {
+      throw 'Tidak dapat menemukan teks dalam gambar'
+    }
+  } catch (error) {
+    console.error(error)
+    m.reply('Terjadi kesalahan dalam melakukan OCR')
+  }
+}
+
+handler.help = ['ocr', 'totext','keteks', 'ketext', 'ketulisan']
 handler.tags = ['tools']
-handler.command = /^(ocr|totext)$/i
+handler.command = /^(ocr|totext|keteks|ketext|ketulisan)$/i
 
 export default handler
