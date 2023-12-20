8e42e718dbaec1c22a0df86a9698413f945a1f09
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2023-12-20 08:02:48 +0700

    Bingung Pilih Gpt 4/3.5 🐦

--- plugins/internet-openai.js
+++ plugins/internet-openai.js
@@ -1,36 +1,28 @@
 // Informasi, setiap akun ChatGPT/Openai, memiliki jumlah permintaan sebanyak $5.00 jika hoki dapat 18.00 tergantung akun, setiap 1 permintaan $ 0.00008. Kalian bisa cek disini https://platform.openai.com/account/usage
 
-import fetch from "node-fetch";
-import { generateWAMessageFromContent } from "@adiwajshing/baileys";
-import fs from 'fs';
-import { Configuration, OpenAIApi } from 'openai';
-
-const configuration = new Configuration({ organization: 'KEY-ORG-KAMU', apiKey: 'KEY-OPENAI-APIKEY-KAMU' }); //KEY-OPENAI-APIKEY-KAMU = https://platform.openai.com/account/api-keys , KEY-ORG-KAMU = https://platform.openai.com/account/org-settings
-const openai = new OpenAIApi(configuration);
-
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-  try {
-    if (!text) throw new Error(`Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Halo?`);
-
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: text }],
-    });
-
-    conn.reply(m.chat, `${response.data.choices[0].message.content}`, m);
-
-  } catch (error) {
-    console.log(error);
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-      conn.reply(m.chat, `${error.response.status}\n\n${error.response.data}`, m);
-    } else {
-      conn.reply(m.chat, `${error.message}`, m);
-    }
-  }
+import OpenAI from "openai";
+var handler = async (m, { conn, usedPrefix, command, text }) => {
+try {
+            if (!text) return reply(`Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Apa itu resesi`);
+            const openai = new OpenAI({
+              apiKey: "'KEY-OPENAI-APIKEY-KAMU", //KEY-OPENAI-APIKEY-KAMU = https://platform.openai.com/account/api-keys 
+            });
+            const response = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: [{role: "user", content: text}],
+          });
+          m.reply(`${response.choices[0].message.content}`) 
+          } catch (error) {
+          if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+            console.log(`${error.response.status}\n\n${error.response.data}`);
+          } else {
+            console.log(error);
+            m.reply("Maaf, sepertinya ada yang error :"+ error.message);
+          }
+        }
 }
-
 handler.command = /^(ai|openai|chatgpt)$/i;
 handler.help = ["ai", "openai", "chatgpt"].map(v => v + " <teks>");
 handler.tags = ["internet"];
