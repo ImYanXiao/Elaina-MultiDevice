885b106b907c014bc669a4e5bd995dbe7ccba951
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-01-11 13:53:04 +0700

    Update internet-jadwalbola.js
    ubah tr.deployers.repl.co menjadi https://0e87ad76-6c4e-40ff-bb5a-6bbdab145ae2-00-39qk1kw7vab6l.worf.replit.dev/

--- plugins/internet-jadwalbola.js
+++ plugins/internet-jadwalbola.js
@@ -5,9 +5,10 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, command }) => {
-  let res = await fetch('https://tr.deployers.repl.co/jadwal-pertandingan');
+  let res = await fetch('https://0e87ad76-6c4e-40ff-bb5a-6bbdab145ae2-00-39qk1kw7vab6l.worf.replit.dev/jadwal-pertandingan');
   if (res.status !== 200) throw await res.text();
   let json = await res.json();
+  m.reply('bentar dulu yakk...')
   
   if (!Array.isArray(json) || json.length === 0) {
     throw new Error('No data found');
@@ -19,10 +20,10 @@ let handler = async (m, { conn, command }) => {
     let data = json[i].data;
     
     if (typeof data === 'string') {
-      output += `*_${json[i].judul}:_*\n${data}\n\n`;
+      output += `*_${json[i].judul}:_*\n==========================\n${data}\n\n--------------------------------\n`;
     } else if (Array.isArray(data)) {
-      let matchInfo = data.join('\n');
-      output += `*_${json[i].judul}:_*\n${matchInfo}\n\n`;
+      let matchInfo = data.join('\n\n');
+      output += `*_${json[i].judul}:_*\n==========================\n${matchInfo}\n\n----------------------------\n`;
     }
   }
   
