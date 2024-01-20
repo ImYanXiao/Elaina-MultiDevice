72d34d3dcaf594dcc3784b6c8e9e570a13623d28
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-01-20 22:32:47 +0700

    Update internet-jadiwebtoon.js

--- plugins/internet-jadiwebtoon.js
+++ plugins/internet-jadiwebtoon.js
@@ -19,7 +19,8 @@ let handler = async (m, {
         let name = await conn.getName(who)
         let q = m.quoted ? m.quoted : m
         let mime = (q.msg || q).mimetype || ''
-        if (!mime.trim()) throw `Kirim/Reply Gambar dengan caption ${usedPrefix + command}`
+        // if (!mime.trim()) throw `Kirim/Reply Gambar dengan caption ${usedPrefix + command}`
+        if (!mime || !mime.trim()) throw `Kirim/Reply Gambar dengan caption ${usedPrefix + command}`
         m.reply('Gambar sedang di download');
         let media = await q.download()
 
@@ -28,53 +29,57 @@ let handler = async (m, {
         let url;
         url = await uploadImage(media);
         let res = await fetch(`${server}?url=${url}&apikey=${Xa}`);
-        let json = await res.json();
-        m.reply('dikit lagi jadi, 30 detik - 5 menit');
+        
+        if (res.ok) {
+            let json = await res.json();
+            m.reply('dikit lagi jadi, 30 detik - 6 menit\nKalo Lebih dari 5 menit, silahkan kirim foto yang jernih + ada muka');
 
-        if (json.status === 200 && json.data && json.data.results) {
-            let inferenceImageUrls = json.data.results.map(result => result.inferenceImageUrl);
+            if (json.status === 200 && json.data && json.data.results) {
+                let inferenceImageUrls = json.data.results.map(result => result.inferenceImageUrl);
 
-            const sendImages = async () => {
-                for (let imageUrl of inferenceImageUrls) {
+                const sendImages = async () => {
+                    for (let imageUrl of inferenceImageUrls) {
+                        try {
+                            await conn.sendFile(m.chat, imageUrl, '', `Ini gambarnya kak @${sender}\n\n${global.wm}`, m);
+                        } catch (error) {
+                            console.error(error);
+                            continue;
+                        }
+                    }
+                };
+
+                let maxRetries = 5;
+                let retries = 0;
+
+                while (retries < maxRetries) {
                     try {
-                        await conn.sendFile(m.chat, imageUrl, '', `Ini gambarnya kak @${sender}\n\n${global.wm}`, m);
+                        await sendImages();
+                        break;
                     } catch (error) {
                         console.error(error);
-                        continue;
+                        retries++;
                     }
                 }
-            };
 
-            let maxRetries = 5;
-            let retries = 0;
-
-            while (retries < maxRetries) {
-                try {
-                    await sendImages();
-                    break;
-                } catch (error) {
-                    console.error(error);
-                    retries++;
+                if (retries >= maxRetries) {
+                    throw 'Gagal mengirim gambar setelah beberapa percobaan';
                 }
-            }
-
-            if (retries >= maxRetries) {
-                throw 'Gagal mengirim gambar setelah beberapa percobaan';
+            } else {
+                throw 'Gagal mendapatkan jawaban dari server';
             }
         } else {
-            throw 'Gagal mendapatkan jawaban dari server';
+            throw `webnya down\nA error code 502 (${res.statusText}) is an HTTP status code that indicates a communication error between two web servers.\nKesalahan gateway buruk (${res.statusText}) 502 adalah kode status HTTP yang mengindikasikan kesalahan komunikasi antara dua server web.`;
         }
     } catch (error) {
         console.error(error);
-        m.reply(`webnya down`);
+        m.reply(`${error}`);
     }
 };
 
 handler.help = ['jadiwebtoon <url>'];
 handler.tags = ['webtoon', 'ai', 'anime'];
-handler.command = /^(towebtoon|jadiwebtoon)$/i;
-// handler.premium = true;
-handler.register = true;
+handler.command = /^(to|jadi)webtoon$/i;
+
 handler.limit = 2;
 
 export default handler;
