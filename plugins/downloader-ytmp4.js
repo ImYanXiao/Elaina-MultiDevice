1bb1823a5d476634329a45fbb7ccfb55da8649d2
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-01-21 17:44:38 +0700

    Update downloader-ytmp4.js

--- plugins/downloader-ytmp4.js
+++ plugins/downloader-ytmp4.js
@@ -1,21 +1,30 @@
-import ytdl from '@distube/ytdl-core';
+//import ytdl from '@distube/ytdl-core';
+import ytdl from 'ytdl-core';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  // const videoURL = 'https://www.youtube.com/watch?v=LkKSzz_S3xk';
   if (!args[0]) {
     return m.reply(`Masukkan Link:\n${usedPrefix + command} https://youtu.be/iik25wqIuFo`);
   }
 
   const videoURL = args[0];
+
   try {
+    const loadingMessage = await conn.reply(m.chat, `Sedang mengambil informasi video...\n${global.wait}`, m);
+
     const info = await ytdl.getInfo(videoURL);
+
     for (const format of info.formats) {
-      console.log(format.qualityLabel, format.url);
-      await conn.sendFile(m.chat, format.url, 'video.mp4', `Resolusi Video ${format.qualityLabel}`, m);
+      await conn.sendFile(m.chat, format.url, 'video.mp4', `Resolusi Video ${format.qualityLabel}`, m, null, true);
     }
+
+    // const allUrls = info.formats.map(format => `${format.qualityLabel}: ${format.url}`).join('\n');
+    // await m.reply(`Resolusi Video:\n${allUrls}`);
+
+    await m.reply("Done\nAll video has been sent");
+      
   } catch (error) {
     console.error('Error fetching video info:', error);
-    await m.reply("Error fetching video info");
+    await m.reply(`Error fetching video info: error ${error}`);
   }
 }
 
