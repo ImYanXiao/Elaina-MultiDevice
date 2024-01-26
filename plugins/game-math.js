7e0cddc25fa686caa077005341b759fa25a6fd6c
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-01-26 09:46:53 +0700

    Update game-math.js

--- plugins/game-math.js
+++ plugins/game-math.js
@@ -1,26 +1,27 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    conn.math = conn.math ? conn.math : {}
-    const buttons = Object.keys(modes).map(v => [v, `${usedPrefix}${command} ${v}`])
-    if (args.length < 1) return conn.sendButton(m.chat, `
-  Mode: ${Object.keys(modes).join(' | ')}
-  Contoh penggunaan: ${usedPrefix}math medium
-  `.trim(), author, null, buttons, m)
-    let mode = args[0].toLowerCase()
-    if (!(mode in modes)) return conn.sendButton(m.chat, `
-  Mode: ${Object.keys(modes).join(' | ')}
-  Contoh penggunaan: ${usedPrefix}math medium
-    `.trim(), author, null, buttons, m)
-    let id = m.chat
-    if (id in conn.math) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.math[id][0])
-    let math = genMath(mode)
-    conn.math[id] = [
-        await conn.reply(m.chat, `Berapa hasil dari *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nBonus Jawaban Benar: ${math.bonus} XP`, m),
-        math, 4,
-        setTimeout(() => {
-            if (conn.math[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah ${math.result}`, author, null, [['again', `${usedPrefix}${command} ${math.mode}`], ...buttons], conn.math[id][0])
-            delete conn.math[id]
-        }, math.time)
-    ]
+let handler = async (m, { conn, args, usedPrefix }) => {
+  conn.math = conn.math ? conn.math : {}
+  if (args.length < 1) throw `
+Mode: ${Object.keys(modes).join(' | ')}
+
+Contoh penggunaan: ${usedPrefix}math medium
+`.trim()
+  let mode = args[0].toLowerCase()
+  if (!(mode in modes)) throw `
+Mode: ${Object.keys(modes).join(' | ')}
+
+Contoh penggunaan: ${usedPrefix}math medium
+`.trim()
+  let id = m.chat
+  if (id in conn.math) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.math[id][0])
+  let math = genMath(mode)
+  conn.math[id] = [
+    await conn.reply(m.chat, `Berapa hasil dari *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nBonus Jawaban Benar: ${math.bonus} XP`, m),
+    math, 4,
+    setTimeout(() => {
+      if (conn.math[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah ${math.result}`, conn.math[id][0])
+      delete conn.math[id]
+    }, math.time)
+  ]
 }
 handler.help = ['math <mode>']
 handler.tags = ['game']
