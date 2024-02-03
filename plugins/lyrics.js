a0c1ec9e255b1d6745b01e43dcf1cc589f3c774a
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-02-03 08:04:06 +0700

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -5,37 +5,48 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `Gunakan contoh ${usedPrefix}${command} NamaPenyanyi NamaLagu\n*${usedPrefix}${command} one direction story of my life*`;
 
   try {
-    const artistAndSong = args.join(' '); // Menggabungkan kata-kata dalam args menjadi satu string
-    const result = await lyricsv2(artistAndSong).catch(async _ => await lyrics(artistAndSong));
+    const artistAndSong = args.join(' ');
+    let result;
 
-    const server = await fetch(`https://vihangayt.me/search/lyrics?q=${encodeURIComponent(artistAndSong)}`);
-    const data = await server.json();
+    try {
+      result = await lyricsv2(artistAndSong);
+    } catch (err1) {
+      try {
+        result = await lyrics(artistAndSong);
+      } catch (err2) {
+        const server = await fetch(`https://vihangayt.me/search/lyrics?q=${encodeURIComponent(artistAndSong)}`);
+        const data = await server.json();
 
-    m.reply(`
+        if (!data.status) {
+          if (data.data && data.data.error) {
+            conn.reply(m.chat, `Tidak dapat menemukan lirik. ${data.data.error}`, m);
+            return;
+          } else {
+            throw 'Gagal mendapatkan lirik lagu.';
+          }
+        }
+
+        const { artist, lyrics, title } = data.data;
+
+        conn.reply(m.chat, `
+Lyrics *${title}*
+Artist ${artist}
+
+${lyrics}
+      `.trim(), m);
+        return;
+      }
+    }
+
+    conn.reply(m.chat, `
 Lyrics *${result.title}*
 Author ${result.author}
 
 ${result.lyrics}
 
 Url ${result.link}
-    `.trim());
+    `.trim(), m);
 
-    if (data.status) {
-      const { artist, lyrics, title } = data.data;
-
-      conn.reply(m.chat, `
-Lyrics *${title}*
-Artist ${artist}
-
-${lyrics}
-      `.trim(), m);
-    } else {
-      if (data.data && data.data.error) {
-        conn.reply(m.chat, `Tidak dapat menemukan lirik. ${data.data.error}`, m);
-      } else {
-        throw 'Gagal mendapatkan lirik lagu.';
-      }
-    }
   } catch (error) {
     console.error('Error:', error);
     conn.reply(m.chat, `Terjadi kesalahan saat memproses permintaan. ${error}`, m);
