6087569c7b8931fe5c2e4c525db62168677a99f5
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-02-06 02:10:55 +0700

    Create internet-convertcode.js

---
+++ plugins/internet-convertcode.js
@@ -0,0 +1,54 @@
+import fetch from 'node-fetch';
+import os from 'os';
+
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) {
+        throw `Invalid input. Format: convertcode <from>|<to>|<code>\nEx: \n${usedPrefix + command} python|java|print("hallo")${os.EOL}a=5${os.EOL}b=10${os.EOL}print(a+b)`;
+    }
+
+    const [From, to, ...codeArray] = args[0].split('|');
+    const code = codeArray.join('|').replace(/\\n/g, os.EOL);
+
+    if (!From || !to || !code) {
+        throw `Invalid input. Format: convertcode <from>|<to>|<code>\nEx: \n${usedPrefix + command} python|java|print("hallo")${os.EOL}a=5${os.EOL}b=10${os.EOL}print(a+b)`;
+    }
+
+    const headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'id,en-US;q=0.7,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImMwYWQ5YzY1YzEzMGM1QGNhc2hiZW50aWVzLmNvbSIsImlhdCI6MTcwNzE1OTgwN30.y1EpKl7B67N5NPE8cpxG0OHGPnI32YxrjWdeDV57-wo',
+    'Origin': 'https://codeconverter.com',
+    'Connection': 'keep-alive',
+    'Referer': 'https://codeconverter.com/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'TE': 'trailers'
+    };
+
+    const json_data = {
+        'from': From,
+        'to': to,
+        'code': code,
+    };
+
+    const response = await fetch('https://api.codeconverter.com/convert_sample', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(json_data),
+    });
+
+    const data = await response.json();
+    const result = data.data.result;
+
+    conn.reply(m.chat, `Result: \n\n${result}`, m);
+};
+
+handler.help = ['convertcode <from>|<to>|<code>'];
+handler.tags = ['code'];
+handler.command = /^(convertcode|konversikode)$/i;
+
+export default handler;
