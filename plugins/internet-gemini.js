025076706ff513818e3055e4ad0c1480f7d9ba1b
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-02-13 22:48:44 +0700

    Create internet-gemini.js

---
+++ plugins/internet-gemini.js
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+  try {
+    const text = args?.join(' ');
+    if (!text) {
+      return conn.reply(m.chat, `Silakan coba lagi, teks jangan kosong.\n*Ex: ${usedPrefix + command} berikan saya kodingan python kalkulator!*`, m);
+    }
+
+    const response = await fetch(`https://aemt.me/gemini?text=${encodeURIComponent(text)}`);
+    if (!response.ok) {
+      return conn.reply(m.chat, 'Tidak dapat memproses permintaan Anda saat ini.', m);
+    }
+
+    const data = await response.json();
+    const result = data?.result;
+
+    if (!result) {
+      return conn.reply(m.chat, 'Tidak dapat memproses permintaan Anda saat ini.', m);
+    }
+
+    conn.reply(m.chat, result, m);
+  } catch (error) {
+    console.error('Error:', error);
+    conn.reply(m.chat, 'Terjadi kesalahan saat memproses permintaan Anda.', m);
+  }
+};
+
+handler.help = ['gemini <Query>'];
+handler.tags = ['tools','internet'];
+handler.limit = 2;
+
+handler.command = /^(bard|gemini?)$/i;
+
+export default handler;
