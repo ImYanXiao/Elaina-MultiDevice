709423b8c1f144465cf03692d0903112ad1b671a
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-02-18 10:13:50 +0700

    add headers to avoid bug

--- plugins/tool-shorturl_kursuang_txt2img_getsticker_ighighlight_capcutdownloader_bioskopDanbioskopnow.js
+++ plugins/tool-shorturl_kursuang_txt2img_getsticker_ighighlight_capcutdownloader_bioskopDanbioskopnow.js
@@ -9,7 +9,20 @@ const apiKeys = ['C9eLLoQZvX', 'euhsDaUPzl'];
 
 const stickersearch = (text) => {
   return new Promise((resolve, reject) => {
-    axios.get(`https://getstickerpack.com/stickers?query=${text}`)
+      const headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+      'Accept-Language': 'id,en-US;q=0.7,en;q=0.3',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Connection': 'keep-alive',
+      'Upgrade-Insecure-Requests': '1',
+      'Sec-Fetch-Dest': 'document',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'none',
+      'Sec-Fetch-User': '?1',
+      'TE': 'trailers'
+    };
+    axios.get(`https://getstickerpack.com/stickers?query=${text}`, { headers })
       .then(({ data }) => {
         const $ = cheerio.load(data);
         const source = [];
