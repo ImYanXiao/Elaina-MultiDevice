55147e2c922bc4356ac0a2b995f188b65759e95a
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-03-12 12:43:25 +0700

    Update downloader-telestickers.js

--- plugins/downloader-telestickers.js
+++ plugins/downloader-telestickers.js
@@ -23,23 +23,23 @@ handler.limit = true
 
 export default handler
 
-//Thanks Xfarr : https://github.com/xfar05
+//Thanks Xfarr : https://github.com/xfar05 , Xnuvers007 : https://github.com/Xnuvers007
 async function Telesticker(url) {
     return new Promise(async (resolve, reject) => {
-        if (!url.match(/(https:\/\/t.me\/addstickers\/)/gi)) throw 'Enther your url telegram sticker'
-        packName = url.replace("https://t.me/addstickers/", "")
-        data = await axios(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, {method: "GET",headers: {"User-Agent": "GoogleBot"}})
+        if (!url.match(/(https:\/\/t.me\/addstickers\/)/gi)) throw 'Enter your telegram sticker URL'
+        let packName = url.replace("https://t.me/addstickers/", "")
+        let data = await axios(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, { method: "GET", headers: { "User-Agent": "GoogleBot" } })
         const hasil = []
         for (let i = 0; i < data.data.result.stickers.length; i++) {
-            fileId = data.data.result.stickers[i].thumb.file_id
-            data2 = await axios(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${fileId}`)
-            result = {
-            status: 200,
-            author: 'Xfarr05',
-            url: "https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/" + data2.data.result.file_path
+            let fileId = data.data.result.stickers[i].thumb.file_id
+            let data2 = await axios(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${fileId}`)
+            let result = {
+                status: 200,
+                author: 'Xnuvers007',
+                url: "https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/" + data2.data.result.file_path
             }
             hasil.push(result)
         }
-    resolve(hasil)
+        resolve(hasil)
     })
 }
