a1cefb8200ff26f7807d9cc0b0c23f7ee12e86a3
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-03-22 12:50:42 +0700

    Update owner-addprem.js

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -3,15 +3,16 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = db.data.users[who]
-    if (!who) throw `tag or mention someone!`
+    if (!user) throw `User not found!`
+    if (!who) throw `Tag or mention someone!`
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw `where the number of days?`
-    if (isNaN(txt)) return m.reply(`only number!\n\nexample:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
+    if (!txt) throw `Where is the number of days?`
+    if (isNaN(txt)) return m.reply(`Only numbers are allowed!\n\nExample:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
     var jumlahHari = 86400000 * txt
     var now = new Date() * 1
-    if (now < user.premiumTime) user.premiumTime += jumlahHari
-    else user.premiumTime = now + jumlahHari
-user.premium = true
+    if (!user.premiumTime || now < user.premiumTime) user.premiumTime = now + jumlahHari
+    else user.premiumTime += jumlahHari
+    user.premium = true
     m.reply(`✔️ Success
 📛 *Name:* ${user.name}
 📆 *Days:* ${txt} days
