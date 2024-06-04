ae11e67b657e625f19b83a72dfc320c1bfa78a02
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-06-05 04:32:47 +0700

    Replaces after 2 years..

--- plugins/jadwalsholat.js
+++ plugins/jadwalsholat.js
@@ -1,15 +1,30 @@
-import { jadwalsholat } from '@bochilteam/scraper'
-let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `Use example ${usedPrefix}${command} semarang`
-    const res = await jadwalsholat(text)
-    m.reply(`
-Jadwal Sholat *${text}*
+import fetch from 'node-fetch'
 
-${Object.entries(res.today).map(([name, data]) => `*Sholat ${name}:* ${data}`).join('\n').trim()}
-`.trim())
+let handler = async (m, { text }) => {
+	if (!text) throw 'Masukan nama kotanya' 
+	let res = await jadwalsholat(text)
+	res = res.map(({ lokasi, daerah, jadwal }) => {
+		delete jadwal.tanggal, delete jadwal.date
+		jadwal = Object.keys(jadwal).map((v) => `• ${v.capitalize()}: ${jadwal[v]}`).join('\n')
+		return `*Lokasi:* ${lokasi}\n*Daerah:* ${daerah}\n*Jadwal:*\n${jadwal}`
+	}).join`\n\n`
+	m.reply(res)
 }
-handler.help = ['salat <daerah>']
-handler.tags = ['quran']
-handler.command = /^(jadwal)?s(a|o|ha|ho)lat$/i
+handler.help = ['jadwalsholat']
+handler.tags = ['tools']
+handler.command = /^(jadwalsholat)$/i
 
 export default handler
+
+async function jadwalsholat(query) {
+	let id = await (await fetch(`https://api.myquran.com/v2/sholat/kota/cari/${query}`)).json()
+	if (id.status !== true) throw id.message
+	id = id.data
+	let result = [], d = new Date().toLocaleDateString('id', { timeZone: 'Asia/Jakarta' }).split('/')
+	for (let i = 0; i < id.length; i++) {
+		let res = await fetch(`https://api.myquran.com/v2/sholat/jadwal/${id[i].id}/${d[2]}/${d[1]}/${d[0]}`)
+		res = await res.json()
+		result.push({ lokasi: res.data.lokasi, daerah: res.data.daerah, jadwal: res.data.jadwal })
+	}
+	return result
+}
