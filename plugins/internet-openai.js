e206a854f87c760d9470fb9de6a8d54e74cf5fba
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-06-08 20:02:03 +0700

    Update internet-openai.js

--- plugins/internet-openai.js
+++ plugins/internet-openai.js
@@ -1,28 +1,14 @@
-// Informasi, setiap akun ChatGPT/Openai, memiliki jumlah permintaan sebanyak $5.00 jika hoki dapat 18.00 tergantung akun, setiap 1 permintaan $ 0.00008. Kalian bisa cek disini https://platform.openai.com/account/usage
-
-import OpenAI from "openai";
 var handler = async (m, { conn, usedPrefix, command, text }) => {
 try {
-            if (!text) throw `Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Apa itu resesi`;
-            const openai = new OpenAI({
-              apiKey: "KEY-OPENAI-APIKEY-KAMU", //KEY-OPENAI-APIKEY-KAMU = https://platform.openai.com/account/api-keys 
-            });
-            const response = await openai.chat.completions.create({
-          model: "gpt-3.5-turbo",
-          messages: [{role: "user", content: text}],
-          });
-          m.reply(`${response.choices[0].message.content}`) 
+            if (!text) return m.reply(`Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Apa itu resesi`);
+            const data = await fetch('https://aemt.me/gpt4?text=' + text) 
+            const json = await data.json()
+          conn.reply(m.chat, json.result.trim(), m)
           } catch (error) {
-          if (error.response) {
-            console.log(error.response.status);
-            console.log(error.response.data);
-            console.log(`${error.response.status}\n\n${error.response.data}`);
-          } else {
             console.log(error);
-            m.reply("Maaf, sepertinya ada yang error :"+ error.message);
+            m.reply("Maaf, sepertinya ada yang error :"+ error);
           }
         }
-}
 handler.command = /^(ai|openai|chatgpt)$/i;
 handler.help = ["ai", "openai", "chatgpt"].map(v => v + " <teks>");
 handler.tags = ["internet"];
