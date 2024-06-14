14a9a40ffb51b43d657a528191eaf3d5b69838e6
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-06-14 19:25:04 +0700

    Update tool-tourl.js

--- plugins/tool-tourl.js
+++ plugins/tool-tourl.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let pp = await conn.profilePictureUrl(who).catch(_ => global.pic)
 let name = await conn.getName(who)
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
