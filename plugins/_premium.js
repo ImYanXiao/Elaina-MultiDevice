2a80cd3488a272165892be9c0d46dd080c244b6f
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-06-14 18:55:42 +0700

    Update _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -1,11 +1,11 @@
-let handler = m => m
-
-handler.before = async function (m) {
-    let user = global.db.data.users[m.sender]                              
-    if (new Date() - user.premiumTime > 0) {
+export async function all(m) {
+    let user = global.db.data.users[m.sender]
+    if (m.chat?.endsWith('broadcast')) return;
+    if (user.premiumTime != 0 && user.premium) {
+        if (new Date() * 1 >= user.premiumTime) {
+            await m.reply(`Waktu premium Kamu sudah habis!`)
             user.premiumTime = 0
             user.premium = false
         }
     }
-
-export default handler
+}
