6ea5e43b9896f979425872579a1daecc7d4b90c5
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-07-30 01:20:26 +0700

    Update downloader-igphotos.js

--- plugins/downloader-igphotos.js
+++ plugins/downloader-igphotos.js
@@ -1,83 +1,61 @@
 // by Xnuvers007
 
-import axios from 'axios';
+import fetch from 'node-fetch';
 
-const handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `Gunakan: ${usedPrefix}${command} <url>`;
-  const req = await igeh(text);
-  const { url_list } = req;
-  const sender = m.sender.split('@')[0];
-  const message = `Foto berhasil diunduh, request dari ${sender}`;
-  
-  if (url_list.length > 1) {
-    const media = [];
-    for (let i = 0; i < url_list.length; i++) {
-      const url = url_list[i];
-      const file = await getBuffer(url);
-      media.push({ url, file });
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+    try {
+        const sender = m.sender.split('@')[0];
+
+        let userInput = args[0];
+        if (!userInput) {
+            return m.reply(`Usage:\n${usedPrefix + command} https://www.instagram.com/p/C7gz-Q3tW3J/\n${usedPrefix + command} www.instagram.com/p/C7gz-Q3tW3J/\n${usedPrefix + command} http://www.instagram.com/p/C7gz-Q3tW3J/`);
+        }
+
+        if (userInput.startsWith('http://')) {
+            userInput = 'https://' + userInput.slice(7);
+        } else if (!userInput.startsWith('https://')) {
+            userInput = 'https://' + userInput;
+        }
+
+        if (!userInput.startsWith('https://www.instagram.com')) {
+            throw `URL tidak valid. Harap berikan URL postingan Instagram yang valid.`;
+        }
+
+        m.reply(`Tunggu sebentar...\nMengunduh gambar dari ${userInput}\n\n${wait}`);
+
+        let url = `https://bioskop-six.vercel.app/igp?u=${encodeURIComponent(userInput)}`;
+        
+        let response = await fetch(url);
+        if (!response.ok) throw `Error mengambil data dari server: ${response.statusText}`;
+        
+        let data = await response.json();
+
+        if (data && data.image_urls && data.image_urls.length > 0) {
+            for (let i = 0; i < data.image_urls.length; i++) {
+                let imgUrl = data.image_urls[i];
+                let imageResponse = await fetch(imgUrl);
+                if (!imageResponse.ok) throw `Error mengunduh gambar dari URL: ${imgUrl}`;
+                
+                let imageBuffer = await imageResponse.buffer();
+                
+                await conn.sendFile(m.chat, imageBuffer, `image${i + 1}.jpg`, ``, m);
+            }
+
+            await conn.reply(m.chat, `Selesai, ini semua gambarnya kak @${sender}`, m);
+
+        } else {
+            m.reply('Tidak ada gambar ditemukan');
+        }
+    } catch (error) {
+        console.error(error);
+        m.reply('Kesalahan server internal: ' + error.message);
     }
-    
-    const multiMediaMessage = {
-      content: { text: message, contextInfo: { mentionedJid: [sender + "@s.whatsapp.net"] } },
-      multiMedia: media
-    };
-    
-    return conn.sendMessage(m.chat, multiMediaMessage, 'mediaMessage');
-  } else {
-    const file = await getBuffer(url_list[0]);
-    
-    const imageMessage = {
-      image: { url: url_list[0], file },
-      caption: message
-    };
-    
-    return conn.sendMessage(m.chat, imageMessage, 'imageMessage', { quoted: m });
-  }
 };
 
-handler.help = ['igphoto'].map(v => v + ' <url>');
-handler.tags = ['downloader'];
-handler.command = /^(igphotos|igfoto)$/i;
+handler.help = ['igp'].map(v => v + ' <url>');
+handler.tags = ['internet', 'downloader', 'tools'];
+handler.command = /^(igp|igphotos|igphoto|igfoto|igf|igfdl|igfdownloader)$/i;
+
+// handler.limit = 2;
 
 export default handler;
-
-async function igeh(url) {
-  const BASE_URL = "https://instasupersave.com/";
-  const resp = await axios.get(BASE_URL);
-  const cookie = resp.headers["set-cookie"];
-  const session = cookie[0].split(";")[0].replace("XSRF-TOKEN=", "").replace("%3D", "");
-  
-  const config = {
-    method: 'post',
-    url: `${BASE_URL}api/convert`,
-    headers: {
-      'origin': 'https://instasupersave.com',
-      'referer': 'https://instasupersave.com/en/instagram-photo/',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'same-origin',
-      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52',
-      'x-xsrf-token': session,
-      'Content-Type': 'application/json',
-      'Cookie': `XSRF-TOKEN=${session}; instasupersave_session=${session}`
-    },
-    data: {
-      url: url
-    }
-  };
-  
-  const response = await axios(config);
-  const ig = Array.isArray(response.data)
-    ? response.data.map(post => post.sd === undefined ? post.thumb : post.sd.url)
-    : [response.data.url[0].url];
-
-  return {
-    results_number: ig.length,
-    url_list: ig
-  };
-}
-
-async function getBuffer(url) {
-  const res = await axios.get(url, { responseType: 'arraybuffer' });
-  return res.data;
-}
