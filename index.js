4a631a95f79c6a0edfa23d170cdd0b75aa334c33
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-09-27 01:11:06 +0700

    Idk

--- index.js
+++ index.js
@@ -1,64 +1,73 @@
-console.log('🐾 Starting...')
+import yargs from 'yargs';
+import cfonts from 'cfonts';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import { createRequire } from 'module';
+import { createInterface } from 'readline';
+import { setupMaster, fork } from 'cluster';
+import { watchFile, unwatchFile } from 'fs';
 
-import yargs from 'yargs'
-import cfonts from 'cfonts'
-import { fileURLToPath } from 'url'
-import { join, dirname } from 'path'
-import { createRequire } from 'module'
-import { createInterface } from 'readline'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
+// Setup console output
+const { say } = cfonts;
+const rl = createInterface(process.stdin, process.stdout);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(__dirname);
+const { name, author } = require(join(__dirname, './package.json'));
 
-// https://stackoverflow.com/a/50052194
-const { say } = cfonts
-const rl = createInterface(process.stdin, process.stdout)
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
-const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+say('Lightweight\nWhatsApp Bot', { font: 'chrome', align: 'center', gradient: ['red', 'magenta'] });
+say(`'${name}' By @${author.name || author}`, { font: 'console', align: 'center', gradient: ['red', 'magenta'] });
 
-say('Lightweight\nWhatsApp Bot', { font: 'chrome', align: 'center', gradient: ['red', 'magenta'] })
-say(`'${name}' By @${author.name || author}`, { font: 'console', align: 'center', gradient: ['red', 'magenta'] })
+console.log('🐾 Starting...'); 
 
-var isRunning = false
+var isRunning = false;
+
 /**
  * Start a js file
  * @param {String} file `path/to/file`
  */
 function start(file) {
-  if (isRunning) return
-  isRunning = true
-  let args = [join(__dirname, file), ...process.argv.slice(2)]
-  say([process.argv[0], ...args].join(' '), { font: 'console', align: 'center', gradient: ['red', 'magenta'] })
-  setupMaster({ exec: args[0], args: args.slice(1) })
-  let p = fork()
+  if (isRunning) return;
+  isRunning = true;
+
+  let args = [join(__dirname, file), ...process.argv.slice(2)];
+  say([process.argv[0], ...args].join(' '), { font: 'console', align: 'center', gradient: ['red', 'magenta'] });
+  
+  setupMaster({ exec: args[0], args: args.slice(1) });
+  let p = fork();
+
   p.on('message', data => {
-    console.log('[✅RECEIVED]', data)
+    console.log('[✅RECEIVED]', data);
     switch (data) {
       case 'reset':
-        p.process.kill()
-        isRunning = false
-        start.apply(this, arguments)
-        break
+        p.kill(); // Change here
+        isRunning = false;
+        start(file);
+        break;
       case 'uptime':
-        p.send(process.uptime())
-        break
+        p.send(process.uptime());
+        break;
     }
-  })
+  });
+
   p.on('exit', (_, code) => {
-    isRunning = false
-    console.error('[❗]Exited with code:', code)
+    isRunning = false;
+    console.error('[❗]Exited with code:', code);
     if (code !== 0) return start(file);
     watchFile(args[0], () => {
       unwatchFile(args[0]);
       start(file);
     });
   });
-  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-  if (!opts['test'])
-    if (!rl.listenerCount()) rl.on('line', line => {
-      p.emit('message', line.trim())
-    })
-  // console.log(p)
+
+  let opts = yargs(process.argv.slice(2)).exitProcess(false).parse();
+  
+  if (!opts['test']) {
+    if (!rl.listenerCount()) {
+      rl.on('line', line => {
+        p.emit('message', line.trim());
+      });
+    }
+  }
 }
 
-start('main.js')
+start('main.js');
