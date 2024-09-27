0a91a4aa0299af7d47314426c2ea7cac3d3c03e4
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-09-27 17:27:54 +0700

    optimize test.js aja

--- test.js
+++ test.js
@@ -8,30 +8,27 @@ import { createRequire } from 'module';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const require = createRequire(__dirname);
+const folders = ['.', ...Object.keys(require(path.join(__dirname, './package.json')).directories)];
+const files = [];
 
-let folders = ['.', ...Object.keys(require(path.join(__dirname, './package.json')).directories)];
-let files = [];
-
-for (let folder of folders) {
+folders.forEach(folder => {
     try {
-        for (let file of fs.readdirSync(folder).filter(v => v.endsWith('.js'))) {
-            files.push(path.resolve(path.join(folder, file)));
-        }
+        const jsFiles = fs.readdirSync(folder).filter(v => v.endsWith('.js')).map(file => path.resolve(folder, file));
+        files.push(...jsFiles);
     } catch (err) {
         console.error(`Error reading folder ${folder}:`, err);
     }
-}
+});
 
-for (let file of files) {
-    if (file == __filename) continue;
-    console.error('Checking', file);
-    
-    const error = syntaxError(fs.readFileSync(file, 'utf8'), file, {
-        sourceType: 'module',
-        allowReturnOutsideFunction: true,
-        allowAwaitOutsideFunction: true
-    });
-    
-    assert.ifError(error);
-    console.log('Done ☑️', file);
-}
+files.forEach(file => {
+    if (file !== __filename) {
+        console.error('Checking', file);
+        const error = syntaxError(fs.readFileSync(file, 'utf8'), file, {
+            sourceType: 'module',
+            allowReturnOutsideFunction: true,
+            allowAwaitOutsideFunction: true
+        });
+        assert.ifError(error);
+        console.log('Done ☑️', file);
+    }
+});
