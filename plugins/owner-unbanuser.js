c18991eabb296727a809d2f4abaf5b24ec0731f3
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-10-08 22:45:43 +0700

    Tag/Nomor?

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,14 +1,26 @@
 let handler = async (m, { conn, text }) => {
     if (!text) throw 'Who wants to be unbanned?'
-    let who
-    if (m.isGroup) who = m.mentionedJid[0]
-    else who = m.chat
-    if (!who) throw 'Tag??'
-    let users = global.db.data.users
-    users[who].banned = false
-    conn.reply(m.chat, 'sᴜᴄᴄᴇs!', m)
+    let who;
+    if (m.isGroup) {
+        if (m.mentionedJid.length > 0) {
+            who = m.mentionedJid[0];
+        } else {
+            let cleanedNumber = text.replace(/\D/g, ''); 
+            who = `${cleanedNumber}@s.whatsapp.net`;
+        }
+    } else {
+        let cleanedNumber = text.replace(/\D/g, '');
+        who = `${cleanedNumber}@s.whatsapp.net`;
+    }
+
+    let users = db.data.users;
+    if (!users[who]) throw 'Pengguna tidak ditemukan';
+
+    users[who].banned = false;
+    conn.reply(m.chat, `Pengguna dengan nomor ${who} telah diunban!`, m);
 }
-handler.help = ['unban']
+
+handler.help = ['unban <nomor>']
 handler.tags = ['owner']
 handler.command = /^unban(user)?$/i
 handler.rowner = true
