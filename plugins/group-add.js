9ab573dafaae1d02ef43ed1006dd0509dd8ab721
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-10-08 23:27:39 +0700

    Add...

--- plugins/group-add.js
+++ plugins/group-add.js
@@ -1,64 +1,58 @@
-const {
-	getBinaryNodeChild,
-	getBinaryNodeChildren
-} = (await import('@adiwajshing/baileys')).default
-
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
-	if (!text) throw `_Masukan nomor!_\nContoh:\n\n${usedPrefix + command} ${global.owner[0]}`
-	m.reply('_Sedang di proses..._')
-    let _participants = participants.map(user => user.id)
-    let users = (await Promise.all(
+    if (!text) throw `_Masukan nomor!_\nContoh:\n\n${usedPrefix + command} ${global.owner[0]}`;
+    
+    m.reply('_Sedang di proses..._');
+    const _participants = participants.map(user => user.id);
+    
+    const users = await Promise.all(
         text.split(',')
             .map(v => v.replace(/[^0-9]/g, ''))
             .filter(v => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
-            .map(async v => [
-                v,
-                await conn.onWhatsApp(v + '@s.whatsapp.net')
-            ])
-    )).filter(v => v[1][0]?.exists).map(v => v[0] + '@c.us')
-    
-    const response = await conn.query({
-        tag: 'iq',
-        attrs: {
-            type: 'set',
-            xmlns: 'w:g2',
-            to: m.chat,
-        },
-        content: users.map(jid => ({
-            tag: 'add',
-            attrs: {},
-            content: [{ tag: 'participant', attrs: { jid } }]
-        }))
-    })
-    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null)
-    const jpegThumbnail = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
-    const add = getBinaryNodeChild(response, 'add')
-    const participant = getBinaryNodeChildren(response, 'add')
-    let anu = participant[0].content.filter(v => v)
-    if (anu[0].attrs.error == 408) conn.reply(m.chat, `Tidak dapat menambahkan @${anu[0].attrs.jid.split('@')[0]}!\nMungkin @${anu[0].attrs.jid.split('@')[0]} baru keluar dari grup ini atau dikick`, m)
-    for (const user of participant[0].content.filter(item => item.attrs.error == 403)) {
-    	const jid = user.attrs.jid
-    	const content = getBinaryNodeChild(user, 'add_request')
-    	const invite_code = content.attrs.code
-    	const invite_code_exp = content.attrs.expiration
-    	const txt = `Mengundang @${jid.split('@')[0]} menggunakan invite...`
-    	await m.reply(txt, null, {
-    		mentions: await conn.parseMention(txt)
-    	})
-    	//await conn.delay(100)
-    	//conn.sendButton(m.chat, txt, wm, 'ᴍᴇɴᴜ', '.menu', m)
-    	await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, await conn.getName(m.chat), 'Undangan untuk bergabung ke grup WhatsApp saya', jpegThumbnail)
+            .map(async v => {
+                const userCheck = await conn.onWhatsApp(v + '@s.whatsapp.net');
+                return userCheck[0]?.exists ? v + '@c.us' : null;
+            })
+    ).then(results => results.filter(Boolean));
+
+    const response = await conn.groupParticipantsUpdate(m.chat, users, "add");
+
+    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(() => null);
+    const jpegThumbnail = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0);
+
+    for (const participant of response) {
+        const jid = participant.jid;
+        const status = participant.status;
+
+        if (status === '408') {
+            conn.reply(m.chat, `Tidak dapat menambahkan @${jid.split('@')[0]}!\nMungkin @${jid.split('@')[0]} baru keluar dari grup ini atau dikick`, m);
+        } else if (status === '403') {
+            const inviteCode = participant.content.attrs.code;
+            const inviteExp = participant.content.attrs.expiration;
+
+            const txt = `Mengundang @${jid.split('@')[0]} menggunakan invite...`;
+            await m.reply(txt, null, { mentions: await conn.parseMention(txt) });
+            
+            await conn.sendGroupV4Invite(
+                m.chat,
+                jid,
+                inviteCode,
+                inviteExp,
+                await conn.getName(m.chat),
+                'Undangan untuk bergabung ke grup WhatsApp saya',
+                jpegThumbnail
+            );
+        }
     }
-}
-handler.help = ['add', '+'].map(v => v + ' @user')
-handler.tags = ['group']
-handler.command = /^(add|\+)$/i
+};
 
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
-handler.fail = null
+handler.help = ['add', '+'].map(v => v + ' @user');
+handler.tags = ['group'];
+handler.command = /^(add|\+)$/i;
+handler.admin = true;
+handler.group = true;
+handler.botAdmin = true;
+handler.fail = null;
 
-export default handler
+export default handler;
