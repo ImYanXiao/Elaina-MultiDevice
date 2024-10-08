41aa877637a849ad468b94e734976b1e8f6d2b3f
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-10-08 23:32:41 +0700

    Nope

--- plugins/tool-tts.js
+++ plugins/tool-tts.js
@@ -1,45 +1,51 @@
-import gtts from 'node-gtts'
-import { readFileSync, unlinkSync } from 'fs'
-import { join } from 'path'
+import gtts from 'node-gtts';
+import { readFileSync, unlinkSync } from 'fs';
+import { join } from 'path';
 
-const defaultLang = 'id'
+const defaultLang = 'id';
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+  let lang = args[0];
+  let text = args.slice(1).join(' ');
 
-  let lang = args[0]
-  let text = args.slice(1).join(' ')
-  if ((args[0] || '').length !== 2) {
-    lang = defaultLang
-    text = args.join(' ')
+  if ((args[0] || '').length !== 2 || !gtts.languages().includes(lang)) {
+    lang = defaultLang;
+    text = args.join(' ');
   }
-  if (!text && m.quoted?.text) text = m.quoted.text
+  if (!text && m.quoted?.text) text = m.quoted.text;
 
-  let res
-  try { res = await tts(text, lang) }
-  catch (e) {
-    m.reply(e + '')
-    text = args.join(' ')
-    if (!text) throw `Use example ${usedPrefix}${command} en hello world`
-    res = await tts(text, defaultLang)
+  let res;
+  try {
+    res = await tts(text, lang);
+  } catch (e) {
+    m.reply(`Error: ${e.message}`);
+    text = args.join(' ');
+    if (!text) throw `Use example: ${usedPrefix}${command} en hello world`;
+    res = await tts(text, defaultLang);
   } finally {
-    if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
+    if (res) {
+      conn.sendFile(m.chat, res, 'tts.wav', null, m, true);
+      unlinkSync(res);
+    }
   }
 }
-handler.help = ['tts <lang> <teks>']
-handler.tags = ['tools']
-handler.command = /^g?tts$/i
 
-export default handler
+handler.help = ['tts <lang> <text>'];
+handler.tags = ['tools'];
+handler.command = /^g?tts$/i;
+
+export default handler;
 
 function tts(text, lang = 'id') {
-  console.log(lang, text)
+  console.log(lang, text);
   return new Promise((resolve, reject) => {
     try {
-      let tts = gtts(lang)
-      let filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav')
+      let tts = gtts(lang);
+      let filePath = join(global.__dirname, '../tmp', `${Date.now()}.wav`);
       tts.save(filePath, text, () => {
-        resolve(readFileSync(filePath))
-        unlinkSync(filePath)
-      })
-    } catch (e) { reject(e) }
-  })
+        resolve(filePath);
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
