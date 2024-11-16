467a814a4326afc60609669e7727d0a63132ee81
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-11-16 13:28:24 +0700

    Update image-wallpaper.js

--- plugins/image-wallpaper.js
+++ plugins/image-wallpaper.js
@@ -1,11 +1,11 @@
-import { wallpaper } from '@bochilteam/scraper';
+//import { wallpaper } from '@bochilteam/scraper';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `Use example ${usedPrefix}${command} Minecraft`;
   
   const maxWallpapers = 10; // ubah berapa gambar yang mau dikirim
   for (let i = 0; i < maxWallpapers; i++) {
-    const res = await wallpaper(text);
+    const res = await bochil.wallpaper(text);
     const img = res[Math.floor(Math.random() * res.length)];
     conn.sendFile(m.chat, img, 'wallpaper.jpg', `Result from *${text}*`, m);
   }
