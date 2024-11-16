d66bbec3a15b656fa0235e2d1c6e7b19c90e95b5
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2024-11-16 13:23:01 +0700

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,7 +1,7 @@
-import { mediafiredl } from '@bochilteam/scraper'
+//import { mediafiredl } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
-    let res = await mediafiredl(args[0])
+    let res = await bochil.mediafiredl(args[0])
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
     let caption = `
 *💌 Name:* ${filename}
