3ed8b2f1e4f11004112f23e8d91fa2f8b1cf6efa
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2025-03-30 16:56:16 +0700

    Tunggu Update, kalo bisa ya Bagus

--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -1,19 +1,31 @@
 import { downloadContentFromMessage } from '@adiwajshing/baileys';
 
-let handler = async (m, { conn }) => {
-  if (!m.quoted) throw 'Send/Reply Images with the caption *.readviewonve*'
-  if (m.quoted.mtype !== 'viewOnceMessageV2') throw 'Ini bukan pesan view-once.'
-  let msg = m.quoted.message
-  let type = Object.keys(msg)[0]
-  let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
-  let buffer = Buffer.from([])
-  for await (const chunk of media) {
-    buffer = Buffer.concat([buffer, chunk])
+let handler = async (m, { conn, usedPrefix, command }) => {
+  if (!m.quoted) throw `Send/Reply Images with the caption ${usedPrefix+command}`;
+
+  if (m.quoted.mtype !== 'viewOnceMessageV2' || m.quoted.mtype !== 'viewOnceMessage') {
+    throw 'Ini bukan pesan view-once.';
   }
+
+  let msg = m.quoted.message;
+
+  console.log(msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.viewOnceMessage);
+  console.log(msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.viewOnceMessageV2?.message);
+
+  let type = Object.keys(msg)[0];
+   
+  let media = await downloadContentFromMessage(msg[type], type === 'imageMessage' ? 'image' : 'video');
+
+  let buffer = Buffer.from([]);
+
+  for await (const chunk of media) {
+    buffer = Buffer.concat([buffer, chunk]);
+  }
+
   if (/video/.test(type)) {
-    return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
+    return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m);
   } else if (/image/.test(type)) {
-    return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
+    return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m);
   }
 };
 
