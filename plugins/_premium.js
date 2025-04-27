47ccad8ef8f62b9b4cf93e07c71f0530aa0479dc
RexxzynXD <150516773+RexxHayanasi@users.noreply.github.com>
2025-04-27 17:08:54 +0700

    Update _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -1,8 +1,9 @@
 export async function all(m) {
     let user = global.db.data.users[m.sender]
     if (m.chat?.endsWith('broadcast')) return;
-    if (user.premiumTime != 0 && user.premium) {
-        if (new Date() * 1 >= user.premiumTime) {
+    
+    if (user.premiumTime !== 0 && user.premium) {
+        if (Date.now() >= user.premiumTime) {
             await m.reply(`Waktu premium Kamu sudah habis!`)
             user.premiumTime = 0
             user.premium = false
