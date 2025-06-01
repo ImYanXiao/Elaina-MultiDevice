f1c5a9b3041a0b6db6e9a07e6cff7f3044e1c447
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2025-06-01 21:21:20 +0700

    Bypass nsfw nhentai ?

--- main.js
+++ main.js
@@ -8,8 +8,8 @@
   Regards from YanXiao ♡
 */
 
-// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1';
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1';
 
 import './config.js'
 
