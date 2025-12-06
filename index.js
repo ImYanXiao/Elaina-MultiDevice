3dc067414b77bf6ccd75a2556305e4f7ba84049f
RexxHayanasi Pengen S.Kom <150516773+RexxHayanasi@users.noreply.github.com>
2025-12-06 10:30:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,91 @@
-import yargs from 'yargs';
-import cfonts from 'cfonts';
-import { fileURLToPath } from 'url';
-import { join, dirname } from 'path';
-import { createRequire } from 'module';
-import { createInterface } from 'readline';
-import { setupMaster, fork } from 'cluster';
-import { watchFile, unwatchFile } from 'fs';
+console.log('🕖 Starting...')
 
-// Setup console output
-const { say } = cfonts;
-const rl = createInterface(process.stdin, process.stdout);
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const require = createRequire(__dirname);
-const { name, author } = require(join(__dirname, './package.json'));
+import { join, dirname } from 'path'
+import { createRequire } from "module"
+import { fileURLToPath } from 'url'
+import { setupMaster, fork } from 'cluster'
+import { watchFile, unwatchFile } from 'fs'
+import cfonts from 'cfonts'
+import { createInterface } from 'readline'
+import yargs from 'yargs'
+import './config.js'
 
-say('Lightweight\nWhatsApp Bot', { font: 'chrome', align: 'center', gradient: ['red', 'magenta'] });
-say(`'${name}' By @${author.name || author}`, { font: 'console', align: 'center', gradient: ['red', 'magenta'] });
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const require = createRequire(__dirname)
+const { say } = cfonts
+const rl = createInterface(process.stdin, process.stdout)
 
-console.log('🐾 Starting...'); 
+say(global.config.namebot, {
+    font: 'pallet',
+    align: 'center',
+    colors: ['white']
+})
+say(`⧻ ${global.config.namebot} by ${global.config.author}`, { 
+    font: 'console',
+    align: 'center',
+    colors: ['white']
+})
 
-var isRunning = false;
+let isRunning = false
 
-/**
- * Start a js file
- * @param {String} file `path/to/file`
- */
 function start(file) {
-  if (isRunning) return;
-  isRunning = true;
-
-  let args = [join(__dirname, file), ...process.argv.slice(2)];
-  say([process.argv[0], ...args].join(' '), { font: 'console', align: 'center', gradient: ['red', 'magenta'] });
-  
-  setupMaster({ exec: args[0], args: args.slice(1) });
-  let p = fork();
-
-  p.on('message', data => {
-    console.log('[✅RECEIVED]', data);
-    switch (data) {
-      case 'reset':
-        p.kill(); // Change here
-        isRunning = false;
-        start(file);
-        break;
-      case 'uptime':
-        p.send(process.uptime());
-        break;
-      default:
-          console.warn('[⚠️ UNRECOGNIZED MESSAGE]', data);
+    if (isRunning) return
+    isRunning = true
+    const args = [join(__dirname, file), ...process.argv.slice(2)]
+    say([process.argv[0], ...args].join(' '), {
+        font: 'console',
+        align: 'center',
+        colors: ['magenta']
+    })
+    say('⸙ MEMUAT SOURCE...', {
+        font: 'console',
+        align: 'center',
+        colors: ['blue']
+    })
+    say('⸙ MEMUAT PLUGINS...', {
+        font: 'console',
+        align: 'center',
+        colors: ['blue']
+    })
+    say('✅ DONE !', {
+        font: 'console',
+        align: 'center',
+        colors: ['green']
+    })
+    setupMaster({
+        exec: args[0],
+        args: args.slice(1),
+    })
+    const p = fork()
+    p.on('message', data => {
+        console.log('[RECEIVED]', data)
+        switch (data) {
+            case 'reset':
+                p.process.kill()
+                isRunning = false
+                start.apply(this, arguments)
+                break
+            case 'uptime':
+                p.send(process.uptime())
+                break
+        }
+    })
+    p.on('exit', (_, code) => {
+        isRunning = false
+        if (code == 'SIGKILL' || code == 'SIGABRT') return start(file)
+        console.error('[❗] Exited with code:', code)
+        if (code === 0) return
+        watchFile(args[0], () => {
+            unwatchFile(args[0])
+            start(file)
+        })
+    })
+    const opts = yargs(process.argv.slice(2)).exitProcess(false).parse()
+    if (!opts['test'] && !rl.listenerCount()) {
+        rl.on('line', line => {
+            p.emit('message', line.trim())
+        })
     }
-  });
-
-  p.on('exit', (_, code) => {
-    isRunning = false;
-    console.error('[❗] Exited with code:', code);
-    if (code !== 0) {
-      console.log('[🔄 Restarting worker due to non-zero exit code...');
-      return start(file);
-    }
-    
-    watchFile(args[0], () => {
-      unwatchFile(args[0]);
-      start(file);
-    });
-  });
-
-  let opts = yargs(process.argv.slice(2)).exitProcess(false).parse();
-  
-  if (!opts['test']) {
-    if (!rl.listenerCount()) {
-      rl.on('line', line => {
-        p.emit('message', line.trim());
-      });
-    }
-  }
 }
 
-start('main.js');
+start('main.js')
